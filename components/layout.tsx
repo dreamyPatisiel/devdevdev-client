783a7f74940ff92200b9dd00b78dfc64da07f95c
minju moon <mmj9908@gmail.com>
2024-01-03 23:02:27 +0900

    Setting : font asset 추가 및 global font  셋팅
    Co-authored-by: minyoung <alsduddl9977@gmail.com>

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,11 @@
+import { PretendardVariable } from '@/styles/fonts';
+
 export default function Layout({ children }) {
   return (
-    <>
-      <div>
-        <header className='bg-hello dark:bg-slate-800'>헤더다</header>
-        <main>{children}</main>
-        <footer>푸턷다</footer>
-      </div>
-    </>
+    <div className={PretendardVariable.className}>
+      <header className='bg-primary1 dark:bg-slate-800'>헤더다</header>
+      <main>{children}</main>
+      <footer>푸턷다</footer>
+    </div>
   );
 }
---
+++ styles/fonts.ts
@@ -0,0 +1,12 @@
+import localFont from 'next/font/local';
+
+const PretendardVariable = localFont({
+  src: [
+    {
+      path: '../public/fonts/PretendardVariable.woff2',
+      weight: '500',
+    },
+  ],
+});
+
+export { PretendardVariable };
