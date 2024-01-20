e48d7269556c9d907c617a44847eb5d535a66ac2
minyoung <alsduddl9977@gmail.com>
2024-01-20 20:31:31 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Index() {
-  return <div className='text-3xl font-bold underline'>index 나와라!</div>;
+  return <div className='text-3xl font-bold underline'>index 나와라!!</div>;
 }
