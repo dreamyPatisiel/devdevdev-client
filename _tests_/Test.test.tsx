2be221a566c9eca80048d6c3c0fc681f5d99d177
minyoung <alsduddl9977@gmail.com>
2024-01-20 19:05:08 +0900

    테스트코드 테스트

---
+++ _tests_/Test.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Index from '@/pages';
+
+describe('테스트', () => {
+  it('div 태그에 "index 나와라" 문구가 있어야 한다.', async () => {
+    const { container } = render(<Index />);
+    const div = container.querySelector('div');
+
+    expect(div?.textContent).toBe('index 나와라');
+  });
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
-export default function index() {
-  return <div className='text-3xl font-bold underline'>index 나와라 </div>;
+export default function Index() {
+  return <div className='text-3xl font-bold underline'>index 나와라</div>;
 }
