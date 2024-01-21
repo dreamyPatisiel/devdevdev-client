9bb2447a90b82147e59d21aaea10c33944f544ca
minju moon <mandelina24@gmail.com>
2024-01-21 14:47:12 +0900

    로그인 테스트코드 작성 (요청 url, 응답 헤더값 테스트)

--- _tests_/Login.test.tsx
+++ _tests_/Login.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import LoginPage from '@/pages/login/index';
-import { act } from 'react-dom/test-utils';
-import axios from 'axios';
+import { server } from './mocks/server';
 
 describe('로그인 버튼 컴포넌트', () => {
   it('로그인버튼이 렌더링 되야한다.', async () => {
@@ -31,26 +30,27 @@ describe('LoginPage 컴포넌트', () => {
   it('카카오 Redirect URL로 GET 요청을 보냈을 때 토큰값이 잘 넘어와야한다.', async () => {
     const { getByTestId } = render(<LoginPage />);
     const btnElement = getByTestId('kakaoButton');
-    // const hrefValue = linkElement.getAttribute('href');
-
-    // axios.get 메소드를 spyOn하여 모킹
-    jest.spyOn(axios, 'get').mockResolvedValue({
-      data: { token: 'yourTokenValue' },
-    });
-
     // 특정 URL로 GET 요청을 시뮬레이션
     fireEvent.click(btnElement);
 
-    const url =
-      'https://kauth.kakao.com/oauth/authorize?client_id=6479cf393f012936a0da49f7dd2f88eb&redirect_uri=http://localhost:8080/login/oauth2/code/kakao&response_type=code';
-    // 비동기 처리를 위해 waitFor 사용
-    await waitFor(() => {
-      // 여기에 헤더에 토큰이 잘 들어왔는지 확인하는 코드 추가
-      expect(axios.get).toHaveBeenCalledWith(url, {
-        headers: {
-          Authorization: 'yes', // 예상되는 토큰값에 따라 수정
-        },
-      });
+    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=6479cf393f012936a0da49f7dd2f88eb&redirect_uri=http://localhost:8080/login/oauth2/code/kakao&response_type=code`;
+
+    await waitFor(async () => {
+      const handlerForKakaoAuthUrl = server.currentHandlers.find(
+        (handler: any) => handler.info.method === 'GET',
+      );
+      const response = await handlerForKakaoAuthUrl.resolver();
+      const authorizationHeaderValue = response.headers.get('authorization');
+
+      // 터미널 창에서 확인해보기
+      console.log(server);
+      console.log('💕path: ', handlerForKakaoAuthUrl.info.path);
+      console.log('💕토큰값 :  ', authorizationHeaderValue);
+
+      // 호출하는 URL이 일치하는지 확인
+      expect(handlerForKakaoAuthUrl.info.path).toEqual(kakaoAuthUrl);
+      // mock 서버의 응답에 헤더의 토큰값이 yes인지 확인
+      expect(authorizationHeaderValue).toEqual('yes');
     });
   });
 });
