cb2330e609c3c04061c5456b099aa32218ec81da
minju moon <mandelina24@gmail.com>
2024-01-21 20:00:01 +0900

    testcode 변경

--- _tests_/mocks/handlers.ts
+++ _tests_/mocks/handlers.ts
@@ -5,8 +5,12 @@ import { baseUrlConfig, loginConfig } from '@/config';
 const URL = baseUrlConfig.serviceUrl || '';
 const END_PONIT = loginConfig.endPoint || '';
 const REDIRECT_URL = URL + END_PONIT;
+const CLIENT_ID = loginConfig.clientId;
 
-const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${loginConfig.clientId}&redirect_uri=${REDIRECT_URL}&response_type=code`;
+console.log('REDIRECT_URL: ', REDIRECT_URL);
+console.log('CLIENT_ID : ', CLIENT_ID);
+
+const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
 
 export const handlers = [
   http.get(kakaoAuthUrl, () => {
