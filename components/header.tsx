4d6a188d21ada568adb6669a72dbfabc377e0af1
minju moon <mandelina24@gmail.com>
2024-01-26 23:48:54 +0900

    Style : header컴포넌트 구현

---
+++ components/header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+import devLogo from '@/public/image/devdevdevLogo.svg';
+
+// 1440x900 ? 기준
+
+export default function Header() {
+  return (
+    <header
+      className='flex flex-row justify-between items-center'
+      style={{
+        height: '8.71vh',
+        borderBottom: '1px solid #DEE5ED',
+        padding: '0 98px',
+      }}
+    >
+      <Image src={devLogo} alt='devdevdev로고' />
+      <ul className='text-white flex flex-row gap-11'>
+        <button>픽픽픽 💖</button>
+        <button>기술블로그 🧪</button>
+        <button>내정보 🧀</button>
+        <button className='bg-primary1 text-center px-5 py-3 rounded-full'>로그인</button>
+      </ul>
+    </header>
+  );
+}
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,11 @@
 import { PretendardVariable } from '@/styles/fonts';
 import { ReactNode } from 'react';
+import Header from './header';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={PretendardVariable.className}>
-      <header className='bg-primary1 dark:bg-slate-800'>헤더다</header>
+      <Header />
       <main>{children}</main>
       <footer>푸턷다</footer>
     </div>
