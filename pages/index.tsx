e312b59ceca5f3f9ef6bcf4e9e8b952af56ba2ba
minyoung <alsduddl9977@gmail.com>
2024-01-27 15:46:53 +0900

    폰트 정의

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Index() {
-  return <div className='text-3xl font-bold underline'>index 나와라!!</div>;
+  return (
+    <div className=''>
+      <h1>DEVDEVDEV</h1>
+      <h2 className='font-bold '>ddd</h2>
+      <p className='text-st1 font-light leading-st'>hello</p>
+      <p className='c2'>hihi</p>
+    </div>
+  );
 }
