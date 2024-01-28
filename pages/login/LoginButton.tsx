ad82b4293c27bafbcaae397ff2a9673c92ec3ac0
minju moon <mandelina24@gmail.com>
2024-01-28 14:27:10 +0900

    Fix : 로그인 버튼 Link로 변경

--- pages/login/LoginButton.tsx
+++ pages/login/LoginButton.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
 
 import kakaoLogo from '@/public/image/kakao_icon.svg';
 import axios from 'axios';
@@ -7,30 +8,23 @@ import { loginConfig, baseUrlConfig } from '@/config';
 
 export default function LoginButton() {
   const URL = baseUrlConfig.serviceUrl || '';
-  const END_PONIT = loginConfig.endPoint || '';
+  const END_PONIT = loginConfig.endPoint;
   const REDIRECT_URL = URL + END_PONIT;
 
   const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${loginConfig.clientId}&redirect_uri=${REDIRECT_URL}&response_type=code`;
 
-  const handleOnClick = async () => {
-    try {
-      const response = await axios.get(kakaoAuthUrl);
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
   return (
     <>
       {/* FIXME: 추후에 디자인 나오면 css 수정하기 */}
-      <button
-        className='bg-kakaoYellow text-black text-l p-2.5 rounded-md flex items-center justify-center gap-3 min-w-48 w-full'
-        data-testid='kakaoButton'
-        onClick={handleOnClick}
-      >
-        <Image src={kakaoLogo} alt='카카오 로고' width={24} height={24} />
-        카카오 로그인
-      </button>
+      <Link href={kakaoAuthUrl}>
+        <button
+          className='bg-kakaoYellow text-black text-l p-2.5 rounded-md flex items-center justify-center gap-3 min-w-48 w-full'
+          data-testid='kakaoButton'
+        >
+          <Image src={kakaoLogo} alt='카카오 로고' width={24} height={24} />
+          카카오 로그인
+        </button>
+      </Link>
     </>
   );
 }
