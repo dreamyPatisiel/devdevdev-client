5aa8da0fe78b4840cf2ed9ff9778b0b992386f79
minju moon <mandelina24@gmail.com>
2024-01-28 14:05:05 +0900

    Fix : global 수정에 따른 컴포넌트 style변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={PretendardVariable.className}>
       <Header />
-      <main className='w-full h-100vh'>{children}</main>
+      <main className='w-full h-[100vh]'>{children}</main>
       <footer className='text-white fixed bottom-0 w-full text-center px-5'>devdevdev.co.kr</footer>
     </div>
   );
--- components/modal.tsx
+++ components/modal.tsx
@@ -4,14 +4,16 @@ import LoginButton from '@/pages/login/LoginButton';
 export default function Modal() {
   return (
     <div
-      className='border border-gray4 rounded-2xl'
-      style={{
-        height: '20.2vh',
-        width: '27.9vw',
-        padding: '3.6vh 4.6vh',
-      }}
+      className='w-[40.2rem] border border-gray4 rounded-2xl px-[4.1rem] py-[3.2rem]'
+      style={
+        {
+          // height: '20.2vh',
+          // width: '27.9vw',
+          // padding: '3.6vh 4.6vh',
+        }
+      }
     >
-      <h1 className=' text-2xl text-white text-center mb-9 '>소셜 로그인</h1>
+      <h1 className=' text-h3 text-white text-center mb-[4.3rem] '>소셜 로그인</h1>
       <LoginButton />
     </div>
   );
