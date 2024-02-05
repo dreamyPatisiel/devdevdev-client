5e56fb83283eb3b8dcbdc54734859d8884620bd5
minyoung <alsduddl9977@gmail.com>
2024-02-06 01:48:56 +0900

    observer 훅 추가

--- _tests_/mocks/data/pickData.ts
+++ _tests_/mocks/data/pickData.ts
@@ -234,36 +234,9 @@ export const pickData = {
       voteCount: '1345',
       commentCount: '324',
     },
-  ],
-};
-
-export const pickData2 = {
-  pickData: [
     {
-      id: 1,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '66',
-          picked: true,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '34',
-          picked: false,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      page: 1,
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 2,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
+      id: 14,
+      question: '14 이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
       answers: [
         {
           answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
@@ -278,202 +251,10 @@ export const pickData2 = {
       ],
       voteCount: '1345',
       commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
     },
     {
-      id: 3,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 4,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 5,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '66',
-          picked: true,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '34',
-          picked: false,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 6,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 7,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 8,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '66',
-          picked: true,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '34',
-          picked: false,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 9,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 10,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '66',
-          picked: true,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '34',
-          picked: false,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 11,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
-      answers: [
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-        {
-          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
-          percent: '',
-          picked: null,
-        },
-      ],
-      voteCount: '1345',
-      commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
-    },
-    {
-      id: 12,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
+      id: 15,
+      question: '15 이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
       answers: [
         {
           answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
@@ -488,13 +269,28 @@ export const pickData2 = {
       ],
       voteCount: '1345',
       commentCount: '324',
-      pages: {
-        nextPage: true,
-      },
     },
     {
-      id: 13,
-      question: '이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
+      id: 16,
+      question: '16 이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
+      answers: [
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
+          percent: '27',
+          picked: true,
+        },
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
+          percent: '73',
+          picked: false,
+        },
+      ],
+      voteCount: '1345',
+      commentCount: '324',
+    },
+    {
+      id: 17,
+      question: '17 이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
       answers: [
         {
           answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
@@ -509,13 +305,60 @@ export const pickData2 = {
       ],
       voteCount: '1345',
       commentCount: '324',
-      pages: {
-        nextPage: false,
-      },
+    },
+    {
+      id: 18,
+      question: '18 이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
+      answers: [
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
+          percent: '70',
+          picked: true,
+        },
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
+          percent: '30',
+          picked: false,
+        },
+      ],
+      voteCount: '1345',
+      commentCount: '324',
+    },
+    {
+      id: 19,
+      question: '19 이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
+      answers: [
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
+          percent: '70',
+          picked: true,
+        },
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
+          percent: '30',
+          picked: false,
+        },
+      ],
+      voteCount: '1345',
+      commentCount: '324',
+    },
+    {
+      id: 20,
+      question: '20 이러이러한 보안 프로세스가 포함된 개발환경에서는 어떤 프로그램이 적절할까요?',
+      answers: [
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 Toss의 방식이 적합하다',
+          percent: '70',
+          picked: true,
+        },
+        {
+          answer: '사용자가 결제를 진행 후 확인받는 프로세스에서는 KaKao 방식이 적합하다',
+          percent: '30',
+          picked: false,
+        },
+      ],
+      voteCount: '1345',
+      commentCount: '324',
     },
   ],
-  pages: {
-    totalPages: 10,
-    // nextPage: true,
-  },
 };
--- pages/pickpickpick/index.tsx
+++ pages/pickpickpick/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense, useCallback, useRef } from 'react';
 import PickContainer from './PickContainer';
 import { useGetPickData } from './api/queries';
 import { PickDataProps } from './types/pick';
@@ -7,87 +7,77 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { getPickData } from './api/pickpickpick';
 import Dropdown from '@/components/dropdown';
 import { useObserver } from './hook/useObserver';
+import Skeleton from '@/components/skeleton';
 
 export default function Index() {
   // const pickDatas = useGetPickData();
   const bottom = useRef(null);
 
-  const DynamicComponent = dynamic(() => import('@pages/pickpickpick/PickContainer'), {
-    loading: () => <p>Loading...</p>,
-  });
+  const DynamicComponent = dynamic(() => import('@pages/pickpickpick/PickContainer'));
 
   const PickComponent = React.lazy(() => import('@pages/pickpickpick/PickContainer'));
 
-  // const { data, fetchNextPage } = useInfiniteQuery({
-  //   queryKey: ['pickData'],
-  //   queryFn: getPickData,
-  //   getNextPageParam: (lastPage) => lastPage.nextPage,
-  //   initialPageParam: 9,
-  // });
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage, status, error, isFetching } =
+    useInfiniteQuery({
+      queryKey: ['pickData'],
+      queryFn: getPickData,
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, allPages, lastPageParam) => {
+        if (lastPage.data.length === 0) {
+          return undefined;
+        }
+        return lastPageParam + 1;
+      },
+    });
 
-  // console.log('pickDatas', pickDatas);
-
-  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
-    queryKey: ['pickData'],
-    queryFn: getPickData,
-    initialPageParam: 0,
-    // maxPages: 9,
-    getNextPageParam: (lastPage, allPages, lastPageParam) => {
-      if (lastPage.data.length === 0) {
-        return undefined;
+  const onIntersect = useCallback(
+    ([entry]: IntersectionObserverEntry[]) => {
+      if (!isFetching && entry.isIntersecting && hasNextPage) {
+        fetchNextPage();
       }
-      console.log('lastPageParam', lastPage.data);
-      return lastPageParam + 1;
     },
+    [fetchNextPage, hasNextPage, isFetching],
+  );
+
+  useObserver({
+    target: bottom,
+    onIntersect,
   });
-  console.log('infinitedata', data, fetchNextPage);
-
-  // const onIntersect = ([entry]: any) => entry.isIntersecting && fetchNextPage();
-
-  // // useObserver로 bottom ref와 onIntersect를 넘겨 주자.
-  // useObserver({
-  //   target: bottom,
-  //   onIntersect,
-  // });
 
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <div className='px-40 pt-24 pb-14'>
-        <div className='flex justify-between items-baseline'>
-          <h1 className='text-h2 mb-16'>픽픽픽 💖</h1>
-          <Dropdown />
-        </div>
+    // <Suspense fallback={<p>Loading...</p>}>
+    <div className='px-40 pt-24 pb-14'>
+      <div className='flex justify-between items-baseline'>
+        <h1 className='text-h2 mb-16'>픽픽픽 💖</h1>
+        <Dropdown />
+      </div>
+      {status === 'pending' ? (
+        <p>Loading...</p>
+      ) : status === 'error' ? (
+        <p>Error: {error.message}</p>
+      ) : (
         <div className='grid grid-cols-3 gap-8'>
           {data?.pages.map((group, index) => (
             <React.Fragment key={index}>
-              {console.log('group', group.data)}
               {group.data.map((data: PickDataProps) => (
-                // <p key={data.id}>{data.id}</p>
-                <PickComponent key={data.id} pickData={data} />
+                <DynamicComponent key={data.id} pickData={data} />
               ))}
             </React.Fragment>
           ))}
-          {/* <div> */}
-          <div ref={bottom} />
-          {/* {isFetchingNextPage && <p>계속 불러오는 중</p>} */}
-          <button
-            onClick={() => fetchNextPage()}
-            // disabled={!hasNextPage || isFetchingNextPage}
-          >
-            {/* {isFetchingNextPage
+        </div>
+      )}
+      <Skeleton />
+      {isFetchingNextPage && <p>Loading more...</p>}
+      <div ref={bottom} />
+    </div>
+    // </Suspense>
+  );
+}
+
+{
+  /* {isFetchingNextPage
             ? 'Loading more...'
             : hasNextPage
             ? 'Load More'
-            : 'Nothing more to load'} */}
-            더 불러오기
-          </button>
-          {/* </div> */}
-          {/* {pickDatas?.map((pickData: PickDataProps) => (
-            // <DynamicComponent key={pickData.id} />
-            <PickComponent key={pickData.id} pickData={pickData} />
-          ))} */}
-        </div>
-      </div>
-    </Suspense>
-  );
+            : 'Nothing more to load'} */
 }
