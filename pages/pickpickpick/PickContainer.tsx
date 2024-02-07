7767124cbd2d3ee28d2698c45cdb66eba3771a73
minyoung <alsduddl9977@gmail.com>
2024-02-07 21:00:32 +0900

    fix: build test error

--- pages/pickpickpick/PickContainer.tsx
+++ pages/pickpickpick/PickContainer.tsx
@@ -10,13 +10,13 @@ export default function PickContainer({ pickData }: { pickData: PickDataProps })
   return (
     <div className='rounded-[1.6rem] border-gray2 border-solid border px-10 py-12'>
       <div className='flex items-baseline gap-6'>
-        <p className='pb-11 text-gray5 p1 font-bold'>{pickData.question}</p>
+        <p className='pb-11 text-gray5 p1 font-bold'>{pickData?.question}</p>
         <Image src={right} alt='오른쪽 화살표' />
       </div>
 
       <div className='grid gap-6'>
         <>
-          {pickData.answers.map((answer, index) => (
+          {pickData?.answers.map((answer, index) => (
             <PickAnswer
               key={index}
               answer={answer.answer}
@@ -31,12 +31,12 @@ export default function PickContainer({ pickData }: { pickData: PickDataProps })
         <span className='flex items-center'>
           <Image src={fire} alt='투표 이미지' />
           <span className='c1 font-medium text-gray5 ml-2 mr-4'>투표</span>
-          <span className='c1 font-bold text-gray5'>{pickData.voteCount}</span>
+          <span className='c1 font-bold text-gray5'>{pickData?.voteCount}</span>
         </span>
         <span className='flex items-center'>
           <Image src={comment} alt='댓글 이미지' />
           <span className='c1 font-medium text-gray5 ml-2 mr-4'>댓글</span>
-          <span className='c1 font-bold text-gray5'>{pickData.commentCount}</span>
+          <span className='c1 font-bold text-gray5'>{pickData?.commentCount}</span>
         </span>
       </div>
     </div>
