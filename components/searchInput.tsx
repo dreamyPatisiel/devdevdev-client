d88e6e7d0a9e6c2ec6598a5be0db0ea39bd68e91
minju moon <mandelina24@gmail.com>
2024-02-09 22:44:15 +0900

    Feat : SearchInput 구현

---
+++ components/searchInput.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Image from 'next/image';
+import serch from '@/public/image/techblog/search.svg';
+
+const PointedText = ({ keyword, text }: { keyword: string; text: string }) => {
+  return (
+    <p className='text-p2 py-[1rem] w-full'>
+      <span className='text-point1'>{keyword}</span> <span className='text-gray4'>{text}</span>
+    </p>
+  );
+};
+
+const NoMatchingKeywords = () => {
+  return <p className='text-p2 py-[1rem] w-full text-gray4'>일치하는 키워드가 없어요</p>;
+};
+
+export default function SearchInput() {
+  return (
+    <div className='bg-gray2 rounded-[0.8rem] w-[28rem] px-[1.6rem]'>
+      <div className='flex flex-row justify-between '>
+        <input
+          placeholder='키워드 검색을 해보세요'
+          className='w-[21rem] py-[1rem] bg-gray2 text-white text-p2 focus:outline-none'
+        />
+        <button className='cursor-pointer'>
+          <Image src={serch} alt='검색아이콘'></Image>
+        </button>
+      </div>
+
+      <PointedText keyword='토스' text='프라임' />
+      <PointedText keyword='토스' text='인슈런스' />
+      <NoMatchingKeywords />
+    </div>
+  );
+}
--- pages/techblog/index.tsx
+++ pages/techblog/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TechCard from './techCard';
+import SearchInput from '@/components/searchInput';
 
 const HLine = styled.div`
   border-bottom: 1px solid var(--gray-3);
@@ -11,6 +12,7 @@ export default function index() {
   return (
     <>
       <ul className='px-[20rem]'>
+        <SearchInput />
         <TechCard />
         <HLine />
       </ul>
