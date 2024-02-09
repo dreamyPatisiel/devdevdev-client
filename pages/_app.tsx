1b9b6363fa0ad4bb27c2ee9064e0e13eec97018e
minju moon <mandelina24@gmail.com>
2024-02-09 20:58:24 +0900

    Feat : 토큰 발급시 axios Instance 생성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/layout';
+import useSetAxiosConfig from '@/api/useSetAxiosConfig';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useSetAxiosConfig();
   if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
     const MockServer = () => import('@/_tests_/mocks/Index');
     MockServer();
