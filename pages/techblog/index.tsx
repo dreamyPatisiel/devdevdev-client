e9d8e7af636b6318d1c43d5ec4462c4f5264bbed
minju moon <mandelina24@gmail.com>
2024-02-13 00:13:50 +0900

    Style : 기술블로그 전체 UI 레이아웃 작업

--- pages/techblog/index.tsx
+++ pages/techblog/index.tsx
@@ -5,14 +5,37 @@ import SearchInput from '@/components/searchInput';
 
 const HLine = styled.div`
   border-bottom: 1px solid var(--gray-3);
+  margin-bottom: 3.2rem;
   height: 3.2rem;
 `;
 
 export default function index() {
   return (
     <>
-      <ul className='px-[20rem]'>
-        <SearchInput />
+      <ul className='px-[9.8rem]'>
+        <div className='flex items-center justify-between py-[2.4rem]'>
+          <h1 className='text-st1'>기술블로그 🧪</h1>
+          <SearchInput />
+        </div>
+        <div>
+          <p className='text-p1 pb-[4.2rem]'>
+            총 <span className='text-point3'>25,425</span>건
+          </p>
+        </div>
+        <TechCard />
+        <HLine />
+        <TechCard />
+        <HLine />
+        <TechCard />
+        <HLine />
+        <TechCard />
+        <HLine />
+        <TechCard />
+        <HLine />
+        <TechCard />
+        <HLine />
+        <TechCard />
+        <HLine />
         <TechCard />
         <HLine />
       </ul>
