262f14627b0d037dfcef8ac2a15d10cde3cd4b61
minyoung <alsduddl9977@gmail.com>
2024-02-12 18:29:52 +0900

    dropdown css 수정

--- components/dropdown.tsx
+++ components/dropdown.tsx
@@ -15,20 +15,22 @@ export default function Dropdown({ dropdownMenu }: { dropdownMenu: string[] }) {
   };
 
   return (
-    <>
-      <div
-        className='rounded-[0.4rem] bg-[#292A2E] w-[14.8rem] flex justify-between items-center relative px-[1rem] py-[0.4rem] cursor-pointer'
-        onClick={handleDropdown}
+    <div
+      className='rounded-[0.4rem] bg-[#292A2E] w-[14.8rem] relative cursor-pointer'
+      onClick={handleDropdown}
+    >
+      <label
+        htmlFor='dropdown'
+        className='text-gray5 text-c1 leading-[2.4rem] cursor-pointer flex justify-between items-center px-[1rem] py-[0.4rem] '
       >
-        <label htmlFor='dropdown' className='text-gray5 text-c1 leading-[2.4rem] cursor-pointer'>
-          {selected}
-        </label>
+        {selected}
         <AngleDown alt='아래방향 화살표' />
-      </div>
+      </label>
+
       {onDropdown && (
         <ul
           id='dropdown'
-          className='text-gray4 text-c1 absolute rounded-[0.4rem] px-4 py-[0.8rem] bg-[#292A2E] top-[15.7rem] right-[10rem] w-[14.8rem] flex flex-col gap-[0.4rem]'
+          className='text-gray4 text-c1 absolute rounded-[0.4rem] px-4 py-[0.8rem] bg-[#292A2E] top-[2.5rem] right-[0] w-[14.8rem] flex flex-col gap-[0.4rem]'
         >
           {dropdownMenu.map((menu, index) => (
             <li
@@ -41,6 +43,6 @@ export default function Dropdown({ dropdownMenu }: { dropdownMenu: string[] }) {
           ))}
         </ul>
       )}
-    </>
+    </div>
   );
 }
