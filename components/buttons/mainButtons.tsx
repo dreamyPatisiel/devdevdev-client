dccfbfcf218f033eb88ec638b6d10d7491df187d
minyoung <alsduddl9977@gmail.com>
2024-02-13 18:12:50 +0900

    버튼 color 타입 지정, borderbutton 설정 추가

--- components/buttons/mainButtons.tsx
+++ components/buttons/mainButtons.tsx
@@ -1,6 +1,10 @@
 import PencilImage from '@public/image/pencil-alt.svg';
 import ImageImage from '@public/image/images.svg';
 
+type MainButtonColor = 'white' | 'primary4' | 'gray3' | 'gray5';
+type MainButtonBgColor = 'primary1' | 'primary5' | 'primary2' | 'black' | 'gray2';
+type MainButtonBoderColor = 'gray5' | 'gray3';
+
 export function MainButton({
   text,
   color,
@@ -8,8 +12,8 @@ export function MainButton({
   icon = false,
 }: {
   text: string;
-  color?: string;
-  bgcolor: string;
+  color?: MainButtonColor;
+  bgcolor: MainButtonBgColor;
   icon?: boolean;
 }) {
   return (
@@ -27,15 +31,17 @@ export function MainBorderButton({
   color,
   bgcolor,
   icon = false,
+  boderColor,
 }: {
   text: string;
-  color: string;
-  bgcolor: string;
+  color?: MainButtonColor;
+  bgcolor: MainButtonBgColor;
   icon?: boolean;
+  boderColor?: MainButtonBoderColor;
 }) {
   return (
     <button
-      className={`bg-${bgcolor} px-[2rem] py-[1.2rem] rounded-[5rem] flex items-center gap-[1rem]`}
+      className={`bg-${bgcolor} px-[2rem] py-[1.2rem] rounded-[5rem] flex items-center gap-[1rem] border-[0.1rem] border-${boderColor} border-solid`}
     >
       {icon && <ImageImage alt='사진 이미지' />}
       <span className={`p1 ${color ? `text-${color}` : 'text-white'}`}>{text}</span>
