42e74d3d9e97f6d00f64cfe7dad0011b590a9695
minyoung <alsduddl9977@gmail.com>
2024-02-16 14:47:53 +0900

    테스트 삭제

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,28 +1,12 @@
-import { Modal } from '@/components/modals/modal';
-import { useModalStore } from '@/store/modalStore';
-
 export default function Index() {
-  const { isModalOpen, openModal } = useModalStore();
-
   return (
     <>
       <div className=''>
         <h1>DEVDEVDEV</h1>
         <h2 className='font-bold '>ddd</h2>
-        <p className='text-st1 font-light leading-st' onClick={openModal}>
-          hello
-        </p>
+        <p className='text-st1 font-light leading-st'>hello</p>
         <p className='c2'>hihi</p>
       </div>
-      {isModalOpen && (
-        <Modal
-          title='모달의 제목을 작성합니다'
-          contents='모달의 상세 내용을 작성합니다. 없으면 생략이 가능합니다.'
-          size='l'
-          submitText='등록하기'
-          submitFn={() => console.log('hi')}
-        />
-      )}
     </>
   );
 }
