49805f76321361ce3867434389434d85f9e99de2
minyoung <alsduddl9977@gmail.com>
2024-02-16 14:24:10 +0900

    subModalButton, modal 컴포넌트 추가

--- components/buttons/subButton.tsx
+++ components/buttons/subButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function SubButton({
+export function SubButton({
   text,
   bgColor,
   onClick,
@@ -12,6 +12,25 @@ export default function SubButton({
   return (
     <button
       className={`bg-${bgColor} text-st2 py-[0.9rem] rounded-[0.8rem] w-[14.2rem]`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+export function SubModalButton({
+  text,
+  bgColor,
+  onClick,
+}: {
+  text: string;
+  bgColor: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <button
+      className={`bg-${bgColor} p1 px-[3.3rem] py-[0.9rem] rounded-[0.8rem] tracking-[-0.32px] font-bold`}
       onClick={onClick}
     >
       {text}
---
+++ components/modals/modalVariants.ts
@@ -0,0 +1,12 @@
+// 모달 효과
+export const modalVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.25,
+    },
+  },
+};
