af2899a41f1a32dc895f2e5e73acbf939e563c2f
minyoung <alsduddl9977@gmail.com>
2024-02-16 14:49:28 +0900

    index 코드 정리

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,12 +1,10 @@
 export default function Index() {
   return (
-    <>
-      <div className=''>
-        <h1>DEVDEVDEV</h1>
-        <h2 className='font-bold '>ddd</h2>
-        <p className='text-st1 font-light leading-st'>hello</p>
-        <p className='c2'>hihi</p>
-      </div>
-    </>
+    <div>
+      <h1>DEVDEVDEV</h1>
+      <h2 className='font-bold '>ddd</h2>
+      <p className='text-st1 font-light leading-st'>hello</p>
+      <p className='c2'>hihi</p>
+    </div>
   );
 }
