a036bf7435179434fa1b8f8f0b8c048674630dba
minyoung <alsduddl9977@gmail.com>
2024-02-16 14:24:42 +0900

    useLoginModalStore 추가

--- store/modalStore.ts
+++ store/modalStore.ts
@@ -6,6 +6,12 @@ export interface ModalStore {
   closeModal: () => void;
 }
 
+export const useLoginModalStore = create<ModalStore>((set) => ({
+  isModalOpen: false,
+  openModal: () => set({ isModalOpen: true }),
+  closeModal: () => set({ isModalOpen: false }),
+}));
+
 export const useModalStore = create<ModalStore>((set) => ({
   isModalOpen: false,
   openModal: () => set({ isModalOpen: true }),
