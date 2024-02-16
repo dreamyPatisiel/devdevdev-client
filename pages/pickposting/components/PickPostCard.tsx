742f519f5bcc285b9dae4fd2be3607f2f2e1a534
minyoung <alsduddl9977@gmail.com>
2024-02-16 11:25:02 +0900

    글자수 제한 추가

--- pages/pickposting/components/PickPostCard.tsx
+++ pages/pickposting/components/PickPostCard.tsx
@@ -1,5 +1,3 @@
-import { MainBorderButton } from '@/components/buttons/mainButtons';
-
 import dynamic from 'next/dynamic';
 
 const MarkdownEditor = dynamic(() => import('@pages/pickposting/components/MarkdownEditor'), {
@@ -13,7 +11,7 @@ export default function PickPostCard() {
         <p className='st2 font-bold mb-[1.6rem]'>선택지 중 하나를 작성해주세요</p>
         <input
           type='text'
-          className='bg-gray1 py-[1.6rem] px-[2rem] st1 text-white rounded-[1.6rem] w-[100%]'
+          className='bg-gray1 py-[1.6rem] px-[2rem] st1 text-white rounded-[1.6rem] w-[100%] border-[0.1rem] border-gray1 focus:outline-none focus:border-primary2'
           placeholder='선택지를 입력해주세요.'
         />
       </div>
@@ -21,9 +19,6 @@ export default function PickPostCard() {
       <div>
         <p className='st2 mb-[1.6rem]'>선택지에 대한 설명을 작성해주세요</p>
         <MarkdownEditor />
-      </div>
-      <div className='ml-auto'>
-        <MainBorderButton text='이미지' bgcolor='black' icon={true} boderColor='gray5' />
       </div>
     </div>
   );
