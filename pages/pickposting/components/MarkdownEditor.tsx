924a361e6d25e67f69373657e87ec41887b8853e
minyoung <alsduddl9977@gmail.com>
2024-02-19 17:38:07 +0900

    픽픽픽 이미지 버튼추가, 툴바 커스텀

--- pages/pickposting/components/MarkdownEditor.tsx
+++ pages/pickposting/components/MarkdownEditor.tsx
@@ -4,13 +4,11 @@ import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor as ToastEditor } from '@toast-ui/react-editor';
 
-type HookCallback = (url: string, text?: string) => void;
-
 export default function MarkdownEditor() {
   const editorRef = useRef<ToastEditor>(null);
   const [content, setContent] = useState(' ');
 
-  const maxLength = 50000;
+  const maxLength = 30000;
 
   const handleChangeInput = () => {
     const instance = editorRef.current?.getInstance();
@@ -23,13 +21,14 @@ export default function MarkdownEditor() {
       instance?.setMarkdown(maxContents);
     }
   };
-  //   console.log('.getHTML();', editorRef.current?.getInstance().getHTML());
 
-  const onUploadImage = async (blob: Blob | File, callback: HookCallback) => {
-    // blob은 base64 인코딩된 이미지 파일
-    // formData에 담아 서버로 보내고, 서버에서는 s3에 이미지 저장후 s3에서 url을 받아 다시 프론트로 값 전송
-    console.log('blob', blob);
-  };
+  const toolbarItems = [
+    ['heading', 'bold', 'italic', 'strike'],
+    ['ul', 'ol', 'indent', 'outdent'],
+    ['code', 'codeblock'],
+    ['hr', 'quote'],
+    ['link'],
+  ];
 
   return (
     <>
@@ -44,10 +43,8 @@ export default function MarkdownEditor() {
           placeholder='선택지에 대한 설명 혹은 의견을 작성해주세요.'
           language='ko-KR'
           onChange={handleChangeInput}
-          initialValue={' '}
-          hooks={{
-            addImageBlobHook: onUploadImage,
-          }}
+          initialValue={''}
+          toolbarItems={toolbarItems}
         />
       </div>
       <p className='text-right text-gray5 mt-[1.6rem] p2 font-light'>
--- pages/pickposting/components/PickPostCard.tsx
+++ pages/pickposting/components/PickPostCard.tsx
@@ -1,10 +1,18 @@
 import dynamic from 'next/dynamic';
 
+import { MainBorderButton } from '@/components/buttons/mainButtons';
+
 const MarkdownEditor = dynamic(() => import('@pages/pickposting/components/MarkdownEditor'), {
   ssr: false,
 });
 
 export default function PickPostCard() {
+  // const onUploadImage = async (blob: Blob | File) => {
+  //   // blob은 base64 인코딩된 이미지 파일
+  //   // formData에 담아 서버로 보내고, 서버에서는 s3에 이미지 저장후 s3에서 url을 받아 다시 프론트로 값 전송
+  //   console.log('blob', blob);
+  // };
+
   return (
     <div className='border-solid border-gray3 border-[0.1rem] rounded-[1.6rem] p-[4rem] mt-[4rem] flex flex-col gap-[3.2rem]'>
       <div>
@@ -20,6 +28,9 @@ export default function PickPostCard() {
         <p className='st2 font-bold mb-[1.6rem]'>선택지에 대한 설명을 작성해주세요</p>
         <MarkdownEditor />
       </div>
+      <span className='ml-auto'>
+        <MainBorderButton text='이미지' icon={true} bgcolor='black' boderColor='gray5' />
+      </span>
     </div>
   );
 }
