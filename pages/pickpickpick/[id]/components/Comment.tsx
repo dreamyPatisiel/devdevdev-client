76be55502f56b85bab79a863ec685e28cc480fc3
minyoung <alsduddl9977@gmail.com>
2024-02-20 01:36:17 +0900

    댓글 컴포넌트 UI 구현

---
+++ pages/pickpickpick/[id]/components/Comment.tsx
@@ -0,0 +1,31 @@
+import ThumbsupPoint from '@public/image/pickpickpick/thumbs-up-point.svg';
+import Thumbsup from '@public/image/pickpickpick/thumbs-up.svg';
+
+export default function Comment() {
+  return (
+    <div className='py-[1.6rem] border-b-[0.1rem] border-b-gray3'>
+      <div className='flex justify-between'>
+        <span>
+          <span className='c1 text-gray5 font-bold'>명탐정코난(det*******)</span>
+          <span className='c1 text-gray3 ml-[2rem]'>2023.05.11</span>
+          <span className='c1 text-gray4 ml-[0.8rem]'>수정</span>
+        </span>
+
+        <span className='flex gap-[0.8rem] items-center'>
+          <Thumbsup alt='클릭되지 않은 좋아요 아이콘' />
+          <span className='c1 text-gray5 font-bold'>1345</span>
+        </span>
+      </div>
+
+      <div className='py-[1.6rem]'>
+        <p className='p2'>
+          <span className='font-bold text-primary3 mr-[1rem]'>미래는 프론트다</span>
+          마음 울적한 날에 거리를 걸어보고, 어쩌고 저쩌고 더미 텍스트 얼마나 써야하는지 진짜
+          모르겠다 아니 네이버 웹툰은 폰트 사이즈가 13px 이더라고요. 살짝 작아보이면서도 읽히는
+          정도인 거 같아서 그런 것 같습니다. 근데 사용자들의 댓글 길이가 어느정도일지 살짝 감이
+          안오네요?
+        </p>
+      </div>
+    </div>
+  );
+}
