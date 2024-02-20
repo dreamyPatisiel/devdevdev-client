cc887a69d0c9b34c5af27ea01745e912e682fa47
minyoung <alsduddl9977@gmail.com>
2024-02-20 21:00:35 +0900

    댓글 더보기 추가

--- pages/pickpickpick/[id]/components/Comments.tsx
+++ pages/pickpickpick/[id]/components/Comments.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
 import Comment from './Comment';
+
+interface subComment {
+  id: number;
+  subComment: string;
+}
 
 export default function Comments({
   isDeleted,
@@ -12,29 +19,41 @@ export default function Comments({
     byAdmin?: boolean;
     byWriter?: boolean;
   };
-  subCommentInfo?: [
-    {
-      id: number;
-      subComment: string;
-    },
-    {
-      id: number;
-      subComment: string;
-    },
-  ];
+  subCommentInfo?: subComment[];
 }) {
+  const [moreComments, setMoreComments] = useState(false);
+
+  const handleMoreComments = () => {
+    setMoreComments(!moreComments);
+  };
+
   return (
     <div className='py-[1.6rem] border-b-[0.1rem] border-b-gray3'>
       <Comment
         comment='미래는 백엔드다   마음 울적한 날에 거리를 걸어보고, 어쩌고 저쩌고 더미 텍스트 얼마나 써야하는지 진짜 모르겠다 아니 네이버 웹툰은 폰트 사이즈가 13px 이더라고요. 살짝 작아보이면서도 읽히는 정도인 거 같아서 그런 것 같습니다. 근데 사용자들의 댓글 길이가 어느정도일지 살짝 감이 안오네요?'
         isDeleted={isDeleted}
       />
+
       {subCommentInfo &&
-        subCommentInfo.map((subComment) => (
-          <div key={subComment.id} className='py-[1.6rem]'>
-            <Comment isSubComment={true} comment={subComment.subComment} />
-          </div>
-        ))}
+        subCommentInfo.map((subComment, index) =>
+          index < 2 ? (
+            <div key={subComment.id} className='py-[1.6rem]'>
+              <Comment isSubComment={true} comment={subComment.subComment} />
+            </div>
+          ) : (
+            moreComments && (
+              <div key={subComment.id} className='py-[1.6rem]'>
+                <Comment isSubComment={true} comment={subComment.subComment} />
+              </div>
+            )
+          ),
+        )}
+
+      {subCommentInfo && subCommentInfo?.length > 2 && (
+        <button onClick={handleMoreComments} className='p2 font-bold text-gray5 ml-[2.4rem]'>
+          + 댓글 더보기
+        </button>
+      )}
     </div>
   );
 }
