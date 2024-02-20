0f094c1a44915c9e8d774f465932cf76a7d0d3e8
minyoung <alsduddl9977@gmail.com>
2024-02-20 22:56:37 +0900

    더보기 버튼 추가

---
+++ components/moreButton.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import ThreeballButton from '@public/image/pickpickpick/ellipsis-v.svg';
+
+export default function MoreButton({ moreButtonList }: { moreButtonList: string[] }) {
+  const [onMoreButton, setMoreButton] = useState(false);
+
+  return (
+    <div className='relative'>
+      <button onClick={() => setMoreButton(!onMoreButton)}>
+        <ThreeballButton alt='원 세개 버튼' />
+      </button>
+
+      {onMoreButton && (
+        <ul className='bg-gray1 border-[0.1rem] border-gray3 rounded-[0.4rem] px-[0.8rem] py-[1rem] c1 text-gray4 w-[8rem] flex flex-col gap-[0.2rem] absolute top-0 left-[2rem]'>
+          {moreButtonList.map((menuItem, index) => (
+            <li key={index} className='w-full text-left hover:text-gray5 cursor-pointer'>
+              {menuItem}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
--- pages/pickpickpick/[id]/index.page.tsx
+++ pages/pickpickpick/[id]/index.page.tsx
@@ -1,22 +1,28 @@
 import Dropdown from '@components/dropdown';
+import MoreButton from '@components/moreButton';
 
 import AnotherPick from './components/AnotherPick';
 import Comments from './components/Comments';
 import VoteCard from './components/VoteCard';
 import WritableComment from './components/WritableComment';
 
-export default function index() {
+export default function Index() {
   return (
     <div className='flex flex-col gap-[4rem] mt-[4rem] mb-[10.6rem]'>
-      <div className='border-b-[0.1rem] border-b-gray3'>
-        <h3 className='h3 font-bold p-[1rem]'>
-          토픽을 정리할 수 있는 제목입니다. 제목을 써주세요.
-        </h3>
-        <div className='p-[1rem]'>
-          <span className='p2 text-gray5 font-bold'>명탐정코난(det*******)</span>
-          <span className='p2 text-gray3 ml-[2rem] mr-[1rem]'>2023.05.11</span>
-          <span className='p2 text-gray4'>신고</span>
+      <div className='border-b-[0.1rem] border-b-gray3 flex justify-between items-baseline'>
+        <div>
+          <h3 className='h3 font-bold p-[1rem]'>
+            토픽을 정리할 수 있는 제목입니다. 제목을 써주세요.
+          </h3>
+
+          <div className='p-[1rem]'>
+            <span className='p2 text-gray5 font-bold'>명탐정코난(det*******)</span>
+            <span className='p2 text-gray3 ml-[2rem] mr-[1rem]'>2023.05.11</span>
+            <span className='p2 text-gray4'>신고</span>
+          </div>
         </div>
+
+        <MoreButton moreButtonList={['수정', '삭제']} />
       </div>
 
       <VoteCard />
