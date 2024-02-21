db68f2e27a32f8f5b4f484b339dcc88440e9ceac
minju moon <mandelina24@gmail.com>
2024-02-21 22:20:35 +0900

    Feat : 로그아웃 기능 구현

--- api/useSetAxiosConfig.tsx
+++ api/useSetAxiosConfig.tsx
@@ -1,24 +1,33 @@
 import axios from 'axios';
 
+import { useEffect } from 'react';
+
 import { baseUrlConfig } from '@/config';
+import { useLoginStatusStore } from '@/stores/loginStore';
 
 const useSetAxiosConfig = () => {
+  const { loginStatus } = useLoginStatusStore();
   const URL = baseUrlConfig.serviceUrl || '';
   axios.defaults.baseURL = URL;
   axios.defaults.withCredentials = true;
   axios.interceptors.request.use(
-    (config) => {
+    (response) => {
       const JWT_TOKEN = localStorage.getItem('accessToken');
       if (JWT_TOKEN) {
-        console.log(JWT_TOKEN, 'JWT_TOKEN');
         axios.defaults.headers.common['Authorization'] = `Bearer ${JWT_TOKEN}`;
       }
-      return config;
+      return response;
     },
     (error) => {
       return Promise.reject(error);
     },
   );
+  useEffect(() => {
+    const JWT_TOKEN = localStorage.getItem('accessToken');
+    if (JWT_TOKEN) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${JWT_TOKEN}`;
+    }
+  }, [loginStatus]); // 로그인 상태가 바뀔때도 한번 토큰값을 확인해야함
 };
 
 export default useSetAxiosConfig;
