381e1303cc521a98e86e9f4374294f11e55edc25
minju moon <mandelina24@gmail.com>
2024-03-01 20:41:39 +0900

    Fix : tooltip 컴포넌트 리팩토링 (cva,clsx,merge사용)

--- components/tooltips/tooltip.tsx
+++ components/tooltips/tooltip.tsx
@@ -1,56 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
 
-export type PositionType = 'right' | 'left' | 'top' | 'bottom';
+import { ButtonHTMLAttributes, FC } from 'react';
 
-export default function Tooltip({
-  text,
-  position,
-  bgColor,
-  textColor,
-}: {
-  text: string;
-  position: PositionType;
-  bgColor: string;
-  textColor?: string;
-}) {
-  const [tailStyle, setTailStyle] = useState('');
+import { CN } from '@/utils/mergeStyles';
 
-  useEffect(() => {
-    switch (position) {
-      case 'right':
-        setTailStyle('-right-[0.4rem] top-[0.9rem]');
-        break;
-      case 'left':
-        setTailStyle('-left-[0.4rem] top-[0.9rem]');
-        break;
-      case 'top':
-        setTailStyle('left-[50%] -top-[0.4rem]');
+const TOOLTIP_ARROW_CLASSES = ['absolute', 'w-3', 'h-3', 'transform', 'rotate-45'];
+const TOOLTIP_WRAPPER_CLASSES = [
+  'c1',
+  'px-[1.3rem]',
+  'py-[0.5rem]',
+  'rounded-[0.8rem]',
+  'font-bold',
+];
 
-        break;
-      case 'bottom':
-        setTailStyle('left-[50%] -bottom-[0.4rem]');
+export const TooltipArrowVariants = cva(TOOLTIP_ARROW_CLASSES, {
+  variants: {
+    direction: {
+      right: ['-right-[0.4rem]', 'top-[0.9rem]'],
+      left: ['-left-[0.4rem]', 'top-[0.9rem]'],
+      top: ['left-[50%]', '-top-[0.4rem]'],
+      bottom: ['left-[50%]', '-bottom-[0.4rem]'],
+    },
+    bgColor: {
+      grayTt: ['bg-gray2'],
+      greenTt: ['bg-point1'],
+    },
+  },
+});
 
-        break;
-      default:
-        break;
-    }
-  }, [position]);
+export const TooltipWrapperVariants = cva(TOOLTIP_WRAPPER_CLASSES, {
+  variants: {
+    bgColor: {
+      grayTt: ['bg-gray2', 'text-point1'],
+      greenTt: ['bg-point1', 'text-black'],
+    },
+  },
+});
+interface TooltipProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof TooltipArrowVariants>,
+    VariantProps<typeof TooltipWrapperVariants> {}
 
+const Tooltip: FC<TooltipProps> = ({ bgColor, direction, children }) => {
   return (
     <div className='relative select-none'>
-      <div
-        style={{ backgroundColor: `var(--${bgColor}` }}
-        className={`absolute  bg-gray2 w-3 h-3 transform rotate-45 ${tailStyle}`}
-      />
-      <div
-        style={{
-          color: `${textColor ? `var(--${textColor})` : 'black'}`,
-          backgroundColor: `var(--${bgColor}`,
-        }}
-        className='c1 px-[1.3rem] py-[0.5rem] rounded-[0.8rem] font-bold'
-      >
-        {text}
-      </div>
+      <div className={CN(TooltipArrowVariants({ direction, bgColor }))} />
+      <div className={CN(TooltipWrapperVariants({ bgColor }))}>{children}</div>
     </div>
   );
-}
+};
+
+export default Tooltip;
