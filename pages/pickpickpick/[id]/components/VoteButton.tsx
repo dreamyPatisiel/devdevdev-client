f783308588cbd311a1d12e222e0b2eb0c84ad078
minyoung <alsduddl9977@gmail.com>
2024-03-04 18:18:04 +0900

    getVoteResult 함수 추가

--- pages/pickpickpick/[id]/components/VoteButton.tsx
+++ pages/pickpickpick/[id]/components/VoteButton.tsx
@@ -5,6 +5,28 @@ import { useVotedStore } from '@stores/votedStore';
 export default function VoteButton({ onClick, voted }: { onClick: () => void; voted: string }) {
   const { isVoted, firstVoted, secondVoted } = useVotedStore();
 
+  const getVoteResult = () => {
+    if (!isVoted) {
+      return (
+        <>
+          <span className='p-[1rem] h3 font-bold text-gray5'>?? %</span>
+          <span className='p-[1rem] p2 font-bold text-gray4'>👈 PICK?</span>
+        </>
+      );
+    }
+
+    const picked = (voted === 'first' && firstVoted) || (voted === 'second' && secondVoted);
+    const percentageColor = picked ? 'text-primary4' : 'text-gray5';
+    const voteCountColor = picked ? 'text-primary3' : 'text-gray4';
+
+    return (
+      <>
+        <span className={`p-[1rem] h3 font-bold ${percentageColor}`}>{'50'}%</span>
+        <span className={`p-[1rem] p2 font-bold ${voteCountColor}`}>{'3,455'}표</span>
+      </>
+    );
+  };
+
   return (
     <motion.button
       whileHover={{ scale: 1.1 }}
@@ -21,24 +43,7 @@ export default function VoteButton({ onClick, voted }: { onClick: () => void; vo
         }
       `}
     >
-      {isVoted ? (
-        (voted === 'first' && firstVoted) || (voted === 'second' && secondVoted) ? (
-          <>
-            <span className='p-[1rem] h3 font-bold text-primary4'>45%</span>
-            <span className='p-[1rem] p2 font-bold text-primary3'>3,455표</span>
-          </>
-        ) : (
-          <>
-            <span className='p-[1rem] h3 font-bold text-gray5'>45%</span>
-            <span className='p-[1rem] p2 font-bold text-gray4'>3,455표</span>
-          </>
-        )
-      ) : (
-        <>
-          <span className='p-[1rem] h3 font-bold text-gray5'>?? %</span>
-          <span className='p-[1rem] p2 font-bold text-gray4'>👈 PICK?</span>
-        </>
-      )}
+      {getVoteResult()}
     </motion.button>
   );
 }
