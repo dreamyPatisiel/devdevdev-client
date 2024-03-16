de931525a55da0b09164ea32c6495a8bfd9a09d1
minju moon <mandelina24@gmail.com>
2024-03-16 11:36:46 +0900

    Fix : 필요없는 코드 삭제 , CompanyTitle 사용

--- pages/techblog/[id]/index.page.tsx
+++ pages/techblog/[id]/index.page.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
+import React from 'react';
 
 import { useRouter } from 'next/router';
 
 import { MainButton } from '@components/buttons/mainButtons';
 
 import HandRight from '@public/image/hand-right.svg';
-import ViewMoreArrow from '@public/image/techblog/arrow-alt-circle-right.svg';
-import HeartNonActive from '@public/image/techblog/heart.svg';
-import HeartActive from '@public/image/techblog/heart_active.svg';
 
-import CompanyCard from '../components/companyCard';
 import TechDetailCard from '../components/techDetailCard';
 
 const CompanyTitle = ({ title, content }: { title: string; content: string }) => {
@@ -24,28 +20,18 @@ const CompanyTitle = ({ title, content }: { title: string; content: string }) =>
 export default function Page() {
   const router = useRouter();
   console.log(router.query.id);
-  const [heart, setHeart] = useState(false);
-  const [showTooltip, setShowTooltip] = useState(false);
-  const handleHeartClick = () => {
-    setHeart((prev) => !prev);
-    setShowTooltip(true);
-  };
 
-  const heartIcon = heart ? (
-    <HeartActive className='cursor-pointer' onClick={handleHeartClick} alt='좋아요버튼' />
-  ) : (
-    <HeartNonActive className='cursor-pointer' onClick={handleHeartClick} alt='좋아요취소버튼' />
-  );
   return (
     // 기술블로그 글
     <article className='w-full h-full px-[9.8rem] py-[6.4rem]'>
       <TechDetailCard />
 
       <section className='flex items-center justify-between px-[3.2rem] py-[3.1rem] border border-gray2 rounded-[1.6rem]'>
-        <p className='st1 font-bold '>
-          <span className='text-point1 '>&#123;{'토스'}&#125;</span>는 절찬리 채용중! 확인하러
-          가볼까요?
-        </p>
+        <CompanyTitle
+          title='토스'
+          content='는 절찬리 채용중! 확인하러
+          가볼까요?'
+        />
         <MainButton text='채용정보 보러가기' variant='primary' icon={<HandRight />} />
       </section>
 
