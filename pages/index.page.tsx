521bf6567c936c6fc63e4742dab386eddbaea45b
minyoung <alsduddl9977@gmail.com>
2024-03-25 15:43:25 +0900

    index 모달 삭제

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,19 +1,10 @@
-import { useLoginModalStore } from '@stores/modalStore';
-
-import { LoginModal } from '@components/modals/modal';
-
 export default function Index() {
-  const { isModalOpen } = useLoginModalStore();
   return (
-    <>
-      <div>
-        <h1>DEVDEVDEV</h1>
-        <h2 className='font-bold '>ddd</h2>
-        <p className='text-st1 font-light leading-st'>hello</p>
-        <p className='c2'>hihi</p>
-      </div>
-
-      {isModalOpen && <LoginModal />}
-    </>
+    <div>
+      <h1>DEVDEVDEV</h1>
+      <h2 className='font-bold '>ddd</h2>
+      <p className='text-st1 font-light leading-st'>hello</p>
+      <p className='c2'>hihi</p>
+    </div>
   );
 }
