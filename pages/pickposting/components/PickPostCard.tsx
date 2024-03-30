0c6f274f70678845761b262dee2999a306899ec6
minyoung <alsduddl9977@gmail.com>
2024-03-30 20:27:08 +0900

    이미지 보여주기

--- pages/pickposting/components/PickPostCard.tsx
+++ pages/pickposting/components/PickPostCard.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react';
 
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 
 import IconPhoto from '@public/image/images.svg';
 
@@ -17,18 +18,29 @@ export default function PickPostCard() {
     fileInputRef.current?.click();
   };
 
+  const [showImages, setShowImages] = useState<string[]>([]);
   const [pickImages, setPickImages] = useState<File[]>([]);
 
   const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('e', e.target.files);
     const files = e.target.files;
     if (!files) return;
 
     const fileArray = Array.from(files);
-    console.log('fileArray', fileArray);
 
     setPickImages([...pickImages, ...fileArray]);
-    console.log('pickImages', pickImages);
+
+    fileArray.map((file) => {
+      // 이미지 화면에 띄우기
+      const reader = new FileReader();
+      // 파일을 불러오는 메서드, 종료되는 시점에 readyState는 Done(2)이 되고 onLoad 시작
+      reader.readAsDataURL(file);
+      reader.onload = (e: any) => {
+        if (reader.readyState === 2) {
+          // 파일 onLoad가 성공하면 2, 진행 중은 1, 실패는 0 반환
+          setShowImages((prevImage) => [...prevImage, e.target.result]);
+        }
+      };
+    });
   };
 
   return (
@@ -65,6 +77,12 @@ export default function PickPostCard() {
         className='hidden'
         ref={fileInputRef}
       />
+
+      <div className='flex'>
+        {showImages?.map((value, index) => (
+          <Image key={index} src={value} alt={`이미지-${index}`} width={100} height={100} />
+        ))}
+      </div>
     </div>
   );
 }
