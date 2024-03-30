6f5175b59969cd584eba3979449a48fe1a2a4101
minju moon <mandelina24@gmail.com>
2024-03-30 21:02:08 +0900

    Style - QA : h-full을 제거하여 padding안먹히던 현상 해결

--- pages/techblog/[id]/index.page.tsx
+++ pages/techblog/[id]/index.page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
 
   return (
     // 기술블로그 글
-    <article className='w-full h-full px-[9.8rem] py-[6.4rem]'>
+    <article className='px-[9.8rem] py-[6.4rem]'>
       <TechDetailCard />
 
       <section className='flex items-center justify-between mx-[10.5rem] px-[3.2rem] py-[3.1rem] border border-gray2 rounded-[1.6rem]'>
