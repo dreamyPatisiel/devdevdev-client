05f1aacb5d60b7822e5205639273fbd188cc52e2
minyoung <alsduddl9977@gmail.com>
2024-04-05 01:20:27 +0900

    픽픽픽 메인 너비, 개발고민 카드 추가

--- pages/pickpickpick/index.page.tsx
+++ pages/pickpickpick/index.page.tsx
@@ -44,6 +44,19 @@ export default function Index() {
         return (
           <>
             <div className='grid grid-cols-3 gap-8' data-testid='loaded'>
+              <div className='px-[2.4rem] py-[4.8rem]'>
+                <p className='st1 font-bold mb-[4rem]'>
+                  개발고민
+                  <br />
+                  혼자 끙끙 앓지말고,
+                  <br />
+                  함께 나눠요!
+                </p>
+                <p className='p1 font-light'>🥺 스타트업 로그인 방식은 어떤게 좋을까?</p>
+                <p className='p1 font-light'>😓 크롤링 기능은 어떤 개발툴이 적절할까?</p>
+                <p className='p1 font-light'>😝 데이터 관련해서는 여기가 더 잘하지!</p>
+              </div>
+
               {pickData?.pages.map((group, index) => (
                 <React.Fragment key={index}>
                   {group?.data.content.map((data: PickDataProps) => (
@@ -66,7 +79,7 @@ export default function Index() {
   };
 
   return (
-    <div className='px-40 pt-24 pb-14'>
+    <div className='px-[20.3rem] pt-24 pb-14'>
       <div className='flex justify-between items-baseline'>
         <h1 className='text-h2 mb-16 text-white' data-testid='pickheart'>
           픽픽픽 💖
