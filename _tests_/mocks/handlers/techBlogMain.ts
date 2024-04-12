3442886c53ce8a67a96b98db31732fdbef055e80
MinjuMoon <mandelina24@gmail.com>
2024-04-12 16:07:53 +0900

    Feat : 기술블로그 data 가공 코드

--- _tests_/mocks/handlers/techBlogMain.ts
+++ _tests_/mocks/handlers/techBlogMain.ts
@@ -2,15 +2,26 @@ import { HttpResponse, http } from 'msw';
 
 import { TECH_BLOG_DATA } from './../data/techBlogData';
 
-export const pickpicpickHandler = http.get(`/pickData`, ({ request }) => {
+export const techBlogHandler = http.get(`/devdevdev/api/v1/articles`, ({ request }) => {
   const searchParams = new URL(request.url).searchParams;
   const size = Number(searchParams.get('size'));
+  const elasticId = searchParams.get('elasticId');
   const pickSort = Number(searchParams.get('pickSort'));
-  const elasticId = Number(searchParams.get('elasticId'));
 
-  // TODO: 데이터 size,sort등에 따라 가공해서 줘야함..어떻게?
+  const getElasticIdIndex = () => {
+    if (!elasticId) return 0;
+
+    const elasticIdIdx = TECH_BLOG_DATA.data.content.findIndex((el) => el.elasticId === elasticId);
+
+    return elasticIdIdx;
+  };
+
   return HttpResponse.json({
     resultType: TECH_BLOG_DATA.resultType,
-    data: TECH_BLOG_DATA.data,
+    data: {
+      content: [TECH_BLOG_DATA.data.content.slice(getElasticIdIndex(), getElasticIdIndex() + size)],
+      totalElements: TECH_BLOG_DATA.data.totalElements,
+      last: TECH_BLOG_DATA.data.last,
+    },
   });
 });
