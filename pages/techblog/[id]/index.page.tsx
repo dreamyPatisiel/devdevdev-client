a2c02df346448e2efa020fa65279368d7d9c3fe2
MinjuMoon <mandelina24@gmail.com>
2024-04-26 10:05:51 +0900

    Fix : 상태별 컴포넌트 렌더링 분기처리 (코드통일)

--- pages/techblog/[id]/index.page.tsx
+++ pages/techblog/[id]/index.page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
   const router = useRouter();
   const techArticleId = router.query.id as string;
 
-  const { data, isError, error, isSuccess, isLoading } = useQuery({
+  const { data, isError, error, isSuccess, isLoading, status } = useQuery({
     queryKey: ['techDetail', techArticleId],
     queryFn: () => {
       return getDetailTechBlog(techArticleId);
@@ -34,60 +34,57 @@ export default function Page() {
   });
 
   const getStatusComponent = () => {
-    if (isLoading) {
-      return (
-        <div className='w-full h-full flex items-center justify-center'>
-          <DevDevDevLoading />
-        </div>
-      );
-    }
-
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
-
     if (!techArticleId) {
       return <></>;
     }
 
-    if (isError) {
-      return <div>Failed to fetch data.</div>;
-    }
-
-    if (isSuccess) {
-      const { company } = data;
-      return (
-        <article className='px-[20.4rem] py-[6.4rem]'>
-          <>
-            <TechDetailCard {...data} />
-            <section className='flex items-center justify-between px-[3.2rem] py-[3.1rem] border border-gray2 rounded-[1.6rem]'>
-              <CompanyTitle
-                title={company?.name}
-                content='는 절찬리 채용중! 확인하러
-          가볼까요?'
-              />
-              <MainButton text='채용정보 보러가기' variant='primary' icon={<HandRight />} />
-            </section>
-
-            {/* ------------------------------------2차-------------------------------------- */}
-            {/* 기업공고 & 댓글 */}
-            {/* <section className='border border-solid border-gray2 rounded-[1.6rem] px-[3.2rem] py-[4.2rem]  mt-[3.2rem] mb-[9.6rem]'>
-        <div className='flex flex-row items-center justify-between mb-[3.4rem]'>
-        <CompanyTitle title='토스' content='는 절찬리 채용중! 관심기업으로 등록하세요' />
-          <MainButton text='기업 구독' color='white' bgcolor='primary1' icon={<PlusIcon />} />
+    switch (status) {
+      case 'pending':
+        return (
+          <div className='w-full h-full flex items-center justify-center'>
+            <DevDevDevLoading />
           </div>
+        );
 
-          <ul className='grid grid-cols-2 grid-rows-2 gap-[2rem]'>
-          <CompanyCard Img={<TossLogo />} />
-          <CompanyCard Img={<TossLogo />} />
-          <CompanyCard Img={<TossLogo />} />
-          <CompanyCard Img={<TossLogo />} />
-          <ViewMoreArrow />
-          </ul>
-        </section> */}
-          </>
-        </article>
-      );
+      case 'error':
+        return <p>Error: {error?.message}</p>;
+
+      case 'success':
+        const { company } = data;
+        return (
+          <article className='px-[20.4rem] py-[6.4rem]'>
+            <>
+              <TechDetailCard {...data} />
+              <section className='flex items-center justify-between px-[3.2rem] py-[3.1rem] border border-gray2 rounded-[1.6rem]'>
+                <CompanyTitle
+                  title={company?.name}
+                  content='는 절찬리 채용중! 확인하러
+              가볼까요?'
+                />
+                <MainButton text='채용정보 보러가기' variant='primary' icon={<HandRight />} />
+              </section>
+
+              {/* ------------------------------------2차-------------------------------------- */}
+              {/* 기업공고 & 댓글 */}
+              {/* <section className='border border-solid border-gray2 rounded-[1.6rem] px-[3.2rem] py-[4.2rem]  mt-[3.2rem] mb-[9.6rem]'>
+            <div className='flex flex-row items-center justify-between mb-[3.4rem]'>
+            <CompanyTitle title='토스' content='는 절찬리 채용중! 관심기업으로 등록하세요' />
+              <MainButton text='기업 구독' color='white' bgcolor='primary1' icon={<PlusIcon />} />
+              </div>
+    
+              <ul className='grid grid-cols-2 grid-rows-2 gap-[2rem]'>
+              <CompanyCard Img={<TossLogo />} />
+              <CompanyCard Img={<TossLogo />} />
+              <CompanyCard Img={<TossLogo />} />
+              <CompanyCard Img={<TossLogo />} />
+              <ViewMoreArrow />
+              </ul>
+            </section> */}
+            </>
+          </article>
+        );
+      default:
+        return <></>;
     }
   };
 
