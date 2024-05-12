5eca20a214a4d7299911d0c0656da94450de940f
minju moon <mandelina24@gmail.com>
2024-05-12 15:05:48 +0900

    Remove : Svgr삭제

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
   pageExtensions: ['page.tsx'],
 };
 
