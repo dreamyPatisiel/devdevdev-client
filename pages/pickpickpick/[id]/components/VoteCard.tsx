f61ae3e4a1789e2133ef42a09f1dfa6651cbbf7f
minyoung <alsduddl9977@gmail.com>
2024-05-18 20:11:41 +0900

    feat: 마크다운 Viewer 적용, img 추가

--- pages/pickpickpick/[id]/components/VoteCard.tsx
+++ pages/pickpickpick/[id]/components/VoteCard.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 
 import { EllipsisGradientText } from '@components/EllipsisGradientText';
 
 import AngleDownPoint from '@public/image/pickpickpick/angle-down-point.svg';
 import AngleUpPoint from '@public/image/pickpickpick/angle-up-point.svg';
+
+import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 
 import { pickOptionData } from '../types/pickDetailData';
 import VoteButton from './VoteButton';
@@ -25,11 +29,13 @@ export default function VoteCard({
     setFullContents(!isFullContents);
   };
 
+  const Viewer = dynamic(() => import('@toast-ui/react-editor').then((mod) => mod.Viewer), {
+    ssr: false,
+  });
+
   return (
-    <div
-      className={`flex gap-[4rem] px-[4rem] py-[1.6rem] ${!isFullContents && 'max-h-[28.7rem]'}`}
-    >
-      <div className='px-[4rem] py-[1.6rem] rounded-[1.6rem] border border-gray3 flex flex-col gap-[2.4rem] w-full'>
+    <div className={`flex gap-[4rem] p-[4rem] pb-[1.6rem] ${!isFullContents && 'min-h-[28.7rem]'}`}>
+      <div className='px-[4rem] py-[1.6rem] rounded-[1.6rem] border border-gray3 flex flex-col gap-[2.4rem] w-full overflow-hidden'>
         <p className='pt-[2.4rem] pb-[3.2rem] text-st1 leading-[2.8rem] font-semibold border-b-[0.1rem] border-b-gray1'>
           {pickDetailOptionData?.title}
         </p>
@@ -40,11 +46,23 @@ export default function VoteCard({
           endPercent='100%'
           className={`p1 ${!isFullContents && 'ellipsis'}`}
         >
-          {pickDetailOptionData?.content}
+          <Viewer initialValue={pickDetailOptionData?.content} theme='dark' />
+
+          {pickDetailOptionData?.pickDetailOptionImages.length !== 0 && (
+            <p className='p2 font-light text-gray5 pt-[7.2rem] pb-[2.4rem]'>첨부 이미지</p>
+          )}
+
+          {pickDetailOptionData?.pickDetailOptionImages?.map((optionImage) => (
+            <img
+              src={optionImage.imageUrl}
+              alt={`픽픽픽 옵션 이미지-${optionImage.id}`}
+              key={optionImage.id}
+            />
+          ))}
         </EllipsisGradientText>
 
         <button
-          className={`p2 font-bold text-point1 flex items-center gap-[0.8rem] justify-center `}
+          className={`p2 font-bold text-point1 flex items-center gap-[0.8rem] justify-center`}
           onClick={handleFullContents}
         >
           내용 전체보기
--- pages/pickpickpick/[id]/types/pickDetailData.ts
+++ pages/pickpickpick/[id]/types/pickDetailData.ts
@@ -10,6 +10,11 @@ export interface PickDetailData {
   };
 }
 
+interface PickDetailOptionImage {
+  id: number; //131,
+  imageUrl: string; //"https://devdevdev-storage.s3.ap-northeast-2.amazonaws.com/test/pickpickpick/47f0e20f-48f4-4ff5-826a-94a4229c58cb-Ex-kIt8UUAAWy3n.jpg"
+}
+
 export interface pickOptionData {
   id: number; //216,
   title: string; //"ㅇㅈ",
@@ -17,14 +22,5 @@ export interface pickOptionData {
   percent: number; //0,
   content: string; //"무조건 마싯지 몬소리",
   voteTotalCount: number; //0,
-  pickDetailOptionImages: [
-    {
-      id: number; //131,
-      imageUrl: string; //"https://devdevdev-storage.s3.ap-northeast-2.amazonaws.com/test/pickpickpick/47f0e20f-48f4-4ff5-826a-94a4229c58cb-Ex-kIt8UUAAWy3n.jpg"
-    },
-    {
-      id: number; //132,
-      imageUrl: string; //"https://devdevdev-storage.s3.ap-northeast-2.amazonaws.com/test/pickpickpick/f4578e7f-e792-4d9f-bd6c-b8da75504f53-IMG_0274.jpeg"
-    },
-  ];
+  pickDetailOptionImages: PickDetailOptionImage[];
 }
