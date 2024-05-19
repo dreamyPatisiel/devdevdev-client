9887cb7e6327d5a56a4e45551a634902b1d93751
minju moon <mandelina24@gmail.com>
2024-05-19 20:53:17 +0900

    Refactor : techDetailSubComponent 파일에 techDetailCard에 사용되는 컴포넌트 분리

--- pages/techblog/components/techDetailCard.tsx
+++ pages/techblog/components/techDetailCard.tsx
@@ -1,56 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-import Image from 'next/image';
 import Link from 'next/link';
 
-import { EllipsisGradientText } from '@components/common/EllipsisGradientText';
 import SearchInput from '@components/common/searchInput';
 import Tooltip from '@components/common/tooltips/tooltip';
 
 import TechHeaderImg from '@public/image/techblog/TechHeaderImg.png';
-import RightArrow from '@public/image/techblog/angle-right-point1.svg';
 
 import { TechCardProps } from '../types/techBlogType';
 import BookmarkIcon from './bookmarkIcon';
-
-const TechDetailInfo = ({
-  company,
-  author,
-  date,
-}: {
-  company: string;
-  author: string;
-  date: string;
-}) => {
-  return (
-    <div className='p1 flex border-white gap-[1.6rem] select-none'>
-      <div>{company}</div>
-      <span className='text-gray4'>|</span>
-      <div>by. {author || company}</div>
-      <span className='text-gray4'>|</span>
-      <time dateTime={date}>{date}</time>
-    </div>
-  );
-};
-
-const TechMainContent = ({ content }: { content: string }) => {
-  return (
-    <EllipsisGradientText startPercent='60%' endPercent='100%' className='p1 py-[1.7rem]'>
-      {content}
-    </EllipsisGradientText>
-  );
-};
-
-const ArticleViewBtn = ({ techArticleUrl }: { techArticleUrl: string }) => {
-  return (
-    <button className='w-full flex justify-center items-center st1 text-point1 pt-[6.4rem] pb-[4.8rem] mb-[4.8rem] font-bold'>
-      <Link href={techArticleUrl} target='_blank'>
-        <p className='mr-[1.6rem]'>아티클 전체 보기</p>
-      </Link>
-      <Image src={RightArrow} alt='오른쪽화살표' className='text-point1' />
-    </button>
-  );
-};
+import { ArticleViewBtn, TechDetailInfo, TechMainContent } from './techDetailCardSubComponent';
 
 export default function TechDetailCard(techDetailProps: TechCardProps) {
   const {
