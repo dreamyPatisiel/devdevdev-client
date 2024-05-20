31f6765d423c68db27260c78fe876bd4f42fc0b3
minyoung <alsduddl9977@gmail.com>
2024-05-21 02:39:50 +0900

    feat: 위로올려! 버튼 구현

---
+++ components/common/GoToTopButton.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+import GoToTop from '@public/image/goToTopIcon.svg';
+
+export default function GoToTopButton() {
+  return (
+    <div className='fixed right-0 bottom-0'>
+      <Image
+        src={GoToTop}
+        alt='페이지 상단 이동 버튼'
+        onClick={() => window.scrollTo(0, 0)}
+        className='cursor-pointer'
+      />
+    </div>
+  );
+}
