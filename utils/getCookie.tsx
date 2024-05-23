ef8e6162ca332354f79dfa856b314cd2d4d2d0b0
minyoung <alsduddl9977@gmail.com>
2024-05-23 22:27:04 +0900

    fix: getGA 요청 최대 횟수 제한 설정

--- utils/getCookie.tsx
+++ utils/getCookie.tsx
@@ -1,3 +1,5 @@
+import { MAX_RETRIES, RETRY_INTERVAL } from '@/constants/getCookieConstants';
+
 /** 쿠키의 key값을 입력하면 value값을 리턴해주는 함수 */
 export function getCookie(key: string) {
   const cookies = document.cookie.split(';');
@@ -20,17 +22,24 @@ export const checkLogin = () => {
   if (loginSuccess) {
     return loginSuccess;
   } else {
-    setTimeout(checkLogin, 1000);
+    setTimeout(checkLogin, RETRY_INTERVAL);
   }
 };
 
-export const getGA = () => {
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/** 쿠키에서 GA를 가져와서 리턴해주는 함수*/
+export const getGA = async () => {
   const GA = getCookie('_ga');
 
-  if (GA) {
-    return GA;
-  } else {
-    setTimeout(getGA, 1000);
+  for (let attempt = 0; attempt < MAX_RETRIES; attempt++) {
+    if (GA) {
+      return GA;
+    }
+
+    await wait(RETRY_INTERVAL); // 시간지연을 위함
   }
+
+  console.error('Failed to get GA cookie after maximum retries');
+  return null; // 적절한 실패 처리
 };
-1;
