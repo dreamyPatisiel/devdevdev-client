a214b4a629d5a2b3d0a5880d82651ac05941bcad
minju moon <mandelina24@gmail.com>
2024-05-26 17:04:20 +0900

    Feat : 메인페이지 픽픽픽,기술블로그 스켈레톤 처리

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 
 import Image from 'next/image';
-import Link from 'next/link';
-
-import { useDropdownStore } from '@stores/dropdownStore';
 
 import PickTitle from '@components/common/title/ArrowTitle';
-import MainCardComponent from '@components/features/main/MainCardComponent';
-import StatisticsItem from '@components/features/pickpickpick/StatisticsItem';
-import TechBlogImg from '@components/features/techblog/techBlogImg';
+import DynamicPickComponent from '@components/features/main/dynamicPickComponent';
+import DynamicTechBlogComponent from '@components/features/main/dynamicTechBlogComponent';
+import MainCardComponent from '@components/features/main/mainCard/MainCardComponent';
 
-import Comment from '@public/image/comment-dots.svg';
 import DevLogo from '@public/image/devdevdevLogo.svg';
-import Fire from '@public/image/fire-alt.svg';
-
-import { useInfinitePickData } from './pickpickpick/api/useInfinitePickData';
-import PickAnswer from './pickpickpick/components/PickAnswer';
-import { PickDataProps } from './pickpickpick/types/pick';
-import { useInfiniteTechBlogData } from './techblog/api/useInfiniteTechBlog';
-import { ArticleViewBtn } from './techblog/components/techDetailCardSubComponent';
-import { TechContent, TechInfo } from './techblog/components/techSubComponent';
-import { TechCardProps } from './techblog/types/techBlogType';
 
 export const MainPageLogo = () => {
   return (
@@ -34,16 +21,10 @@ export const MainPageLogo = () => {
 export default function Index() {
   const PICK_PATH = '/pickpickpick';
   const TECH_PATH = '/techblog';
-  const { sortOption } = useDropdownStore();
-
-  const { pickData } = useInfinitePickData(sortOption);
-
-  const { techBlogData } = useInfiniteTechBlogData(sortOption);
 
   return (
     <>
       <div className='w-full h-full px-[20.3rem] py-[6.4rem]'>
-        {/* 영역1 */}
         <MainPageLogo />
 
         <div className='grid grid-row' style={{ gridTemplateRows: '1fr 1fr' }}>
@@ -61,36 +42,7 @@ export default function Index() {
                 iconText='바로가기'
                 routeURL={PICK_PATH}
               />
-              <div className='overflow-y-scroll  max-h-[47rem]'>
-                {pickData?.pages.map((group, index) => (
-                  <div key={index}>
-                    {group?.data.content.map((data: PickDataProps) => (
-                      <Link href={`${PICK_PATH}/${data.id}`} key={data.id}>
-                        <div className='border border-gray1 rounded-3xl px-[2.4rem] py-7 mb-[1.6rem]'>
-                          <PickTitle title={data.title} version='mainPagePickTitle' />
-                          <ul className='grid gap-[0.9rem]'>
-                            {data?.pickOptions.map((option) => (
-                              <PickAnswer
-                                key={option.id}
-                                {...option}
-                                isVoted={data.isVoted}
-                                className='px-8 py-[1.2rem] rounded-xl'
-                              />
-                            ))}
-                          </ul>
-                          <StatisticsItem
-                            icon={Fire}
-                            alt='투표 이미지'
-                            text='투표'
-                            count={data.voteTotalCount}
-                            className='mt-[1.6rem]'
-                          />
-                        </div>
-                      </Link>
-                    ))}
-                  </div>
-                ))}
-              </div>
+              <DynamicPickComponent />
             </div>
           </section>
 
@@ -100,8 +52,7 @@ export default function Index() {
               gridTemplateColumns: '1fr 1.53fr',
             }}
           >
-            <MainCardComponent path='/techblog' />
-
+            <MainCardComponent path={TECH_PATH} />
             <div>
               <PickTitle
                 title='따끈따끈! 최신 아티클'
@@ -109,51 +60,7 @@ export default function Index() {
                 iconText='바로가기'
                 routeURL={TECH_PATH}
               />
-              <div className='overflow-y-scroll max-h-[47rem]'>
-                {techBlogData?.pages?.map((group, index) => (
-                  <React.Fragment key={index}>
-                    {group.data.content.map((data: TechCardProps) => (
-                      <div
-                        key={data.id}
-                        className='grid grid-flow-col border-white gap-[3.2rem] text-white py-[3.2rem] border-b border-b-gray1 border-solid select-none '
-                      >
-                        <div>
-                          <TechBlogImg
-                            id={data.id}
-                            thumbnailUrl={data.thumbnailUrl}
-                            width={'w-[12rem]'}
-                            height={'h-[8rem]'}
-                            rounded='rounded-[0.8rem]'
-                          />
-
-                          <ArticleViewBtn
-                            techArticleUrl={data.techArticleUrl}
-                            fontSize='c1'
-                            textIconGap={'mr-[0.8rem]'}
-                            paddingY='pt-[1.6rem]'
-                          />
-                        </div>
-                        <div>
-                          <div className='flex items-center justify-between border-white'>
-                            <Link href={`/techblog/${data.id}`}>
-                              <p className='font-bold st2 py-[0.7rem]'>{data.title}</p>
-                            </Link>
-                          </div>
-                          <TechInfo
-                            author={data.author}
-                            date={data.regDate}
-                            company={data.company?.name}
-                            companyId={data?.id}
-                          />
-                          <Link href={`/techblog/${data.id}`}>
-                            <TechContent content={data.contents} maxLines={4} className='mr-4' />
-                          </Link>
-                        </div>
-                      </div>
-                    ))}
-                  </React.Fragment>
-                ))}
-              </div>
+              {DynamicTechBlogComponent()}
             </div>
           </section>
         </div>
