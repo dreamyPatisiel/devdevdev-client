95bcdd1f86335df093c0240d15bdaa571411d10f
minyoung <alsduddl9977@gmail.com>
2024-05-29 18:06:03 +0900

    feat: deletepick 성공, 에러처리 추가 작업

--- pages/pickpickpick/[id]/apiHooks/useDeletePick.ts
+++ pages/pickpickpick/[id]/apiHooks/useDeletePick.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
+import { useRouter } from 'next/router';
+
 import { useMutation } from '@tanstack/react-query';
 
 import { useToastVisibleStore } from '@stores/toastVisibleStore';
+
+import { UNDEFINED_ERROR_MESSAGE } from '@/constants/errorMessageConstants';
+import { ErrorRespone } from '@/types/errorResponse';
 
 const deletePick = async (id: string) => {
   const res = await axios.delete(`/devdevdev/api/v1/picks/${id}`);
@@ -12,10 +17,22 @@ const deletePick = async (id: string) => {
 
 export const useDeletePick = () => {
   const { setToastVisible } = useToastVisibleStore();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: deletePick,
-    onSuccess: () => setToastVisible('성공'),
-    onError: () => setToastVisible('실패'),
+    onSuccess: () => {
+      setToastVisible('투표를 성공적으로 삭제했어요.');
+      router.push('/pickpickpick');
+    },
+    onError: (error: ErrorRespone) => {
+      const errorMessage = error.response.data.message;
+
+      if (errorMessage == null) {
+        return setToastVisible(UNDEFINED_ERROR_MESSAGE);
+      }
+
+      return setToastVisible(errorMessage);
+    },
   });
 };
