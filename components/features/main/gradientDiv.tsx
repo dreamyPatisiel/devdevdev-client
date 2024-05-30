30ef64e5251d180d3f992768537bd69838933df5
minju moon <mandelina24@gmail.com>
2024-05-31 00:56:55 +0900

    Fix : 그리디언트 컴포넌트 pointer-events-none 삭제

--- components/features/main/gradientDiv.tsx
+++ components/features/main/gradientDiv.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function GradientDiv() {
   return (
     <div
-      className='absolute bottom-0 w-full h-full pointer-events-none'
+      className='absolute bottom-0 w-full h-full'
       style={{
         background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 85%, rgba(0, 0, 0, 1) 100%)`,
       }}
