53e497e288e12b5576c31cf60be1c6658675c7cc
minju moon <mandelina24@gmail.com>
2024-06-01 20:56:32 +0900

    Fix :  사용안하는 속성 제거

--- pages/myinfo/bookmark/index.page.tsx
+++ pages/myinfo/bookmark/index.page.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 import { Dropdown } from '@components/common/dropdown';
 import DynamicTechBlogComponent from '@components/features/main/dynamicTechBlogComponent';
