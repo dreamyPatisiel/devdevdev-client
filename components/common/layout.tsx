a4e2316d50ec80a01479d6df3eaf9ec7c2631afe
MinjuMoon <mandelina24@gmail.com>
2024-06-03 14:27:14 +0900

    Feat : 뎁구리 이미지 추가 , 모바일 인식 훅 추가

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 
 import { useRouter } from 'next/router';
 
+import useIsMobile from '@hooks/useIsMobile';
+
 import { PretendardVariable } from '@/styles/fonts';
 
 import GoToTopButton from './GoToTopButton';
@@ -13,22 +15,30 @@ export default function Layout({ children }: { children: ReactNode }) {
   const router = useRouter();
   const { pathname } = router;
 
+  const isMobile = useIsMobile();
+
   if (pathname === '/loginloading') {
     return <>{children}</>;
   }
 
   return (
-    <div
-      className={`${PretendardVariable.className}  grid grid-rows-[8.5rem,1fr,5vh] h-screen text-white`}
-    >
-      <Header />
-      <main className='w-full'>
-        <Toast />
-        <AuthModal />
-        {children}
-        <GoToTopButton />
-      </main>
-      <footer className='flex justify-center items-center px-5'>devdevdev.co.kr</footer>
-    </div>
+    <>
+      {isMobile ? (
+        <div>모바일이다</div>
+      ) : (
+        <div
+          className={`${PretendardVariable.className}  grid grid-rows-[8.5rem,1fr,5vh] h-screen text-white`}
+        >
+          <Header />
+          <main className='w-full'>
+            <Toast />
+            <AuthModal />
+            {children}
+            <GoToTopButton />
+          </main>
+          <footer className='flex justify-center items-center px-5'>devdevdev.co.kr</footer>
+        </div>
+      )}
+    </>
   );
 }
