76a438b12a917abcd64d6524b73d109e4350d3f1
minju moon <mandelina24@gmail.com>
2024-06-06 23:34:01 +0900

    Feat :  북마크에서 삭제시 토스트 띄워주기

--- components/features/main/dynamicTechBlogComponent.tsx
+++ components/features/main/dynamicTechBlogComponent.tsx
@@ -14,6 +14,7 @@ import { TechContent, TechInfo } from '@pages/techblog/components/techSubCompone
 import { TechCardProps } from '@pages/techblog/types/techBlogType';
 
 import { useDropdownStore } from '@stores/dropdownStore';
+import { useToastVisibleStore } from '@stores/toastVisibleStore';
 
 import { useObserver } from '@hooks/useObserver';
 
@@ -36,9 +37,9 @@ export default function DynamicTechBlogComponent({
   bottomDiv?: React.MutableRefObject<null>;
   dataType: 'main' | 'myinfo';
 }) {
-  const { sortOption } = useDropdownStore();
   const queryClient = useQueryClient();
-
+  const { sortOption } = useDropdownStore();
+  const { setToastVisible } = useToastVisibleStore();
   const useConditionalInfiniteHook = (dataType: 'main' | 'myinfo') => {
     const techBlogHook = useInfiniteTechBlogData('LATEST');
     const myInfoBookmarkHook = useInfiniteMyInfoBookmark(sortOption as MyinfoBookmarkDropdownProps);
@@ -69,6 +70,8 @@ export default function DynamicTechBlogComponent({
     id: number;
     isBookmarkActive: boolean;
   }) => {
+    setToastVisible('북마크에서 삭제했어요');
+
     bookmarkMutation(
       {
         techArticleId: id,
