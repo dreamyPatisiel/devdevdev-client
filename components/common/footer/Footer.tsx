49d4916743152e773d677ccae7e38a5c88cbcd79
minju moon <mandelina24@gmail.com>
2024-06-08 00:35:02 +0900

    Fix : 이메일 프로토콜 설정

--- components/common/footer/Footer.tsx
+++ components/common/footer/Footer.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { DEVDEVDEV_INSTAGRAM, DEVDEVDEV_NOTION, DEVDEVDEV_YOUTUBE } from '@/constants/FooterLink';
+import {
+  DEVDEVDEV_EMAIL,
+  DEVDEVDEV_INSTAGRAM,
+  DEVDEVDEV_NOTION,
+  DEVDEVDEV_YOUTUBE,
+} from '@/constants/FooterLink';
 import envelope from '@/public/image/footer/envelope.svg';
 import instagram from '@/public/image/footer/instagram.svg';
 import youtube from '@/public/image/footer/youtube.svg';
@@ -11,26 +16,29 @@ import youtube from '@/public/image/footer/youtube.svg';
 export default function Footer() {
   return (
     <footer className='flex justify-between items-center px-[20.3rem]'>
-      <nav>
-        <ul className='flex'>
-          <li className='mr-6'>꿈빛파티시엘</li>
-          <li className='flex justify-center items-center gap-3  text-gray4'>
-            <Image src={envelope} alt='메일 아이콘' />
-            <p>dreamy5patisiel@gmail.com</p>
-          </li>
-          <div className='ml-[2.4rem] flex items-center gap-5'>
-            <Link href={DEVDEVDEV_NOTION} target='_blank'>
-              <Image src={instagram} alt='인스타그램 아이콘' />
-            </Link>
-            <Link href={DEVDEVDEV_YOUTUBE} target='_blank'>
-              <Image src={youtube} alt='유투브 아이콘' />
-            </Link>
-            <Link href={DEVDEVDEV_INSTAGRAM} className=' text-gray3' target='_blank'>
-              개인정보 처리방침
-            </Link>
-          </div>
-        </ul>
+      <nav className='flex'>
+        <p className='mr-6 text-gray4'>꿈빛파티시엘</p>
+        <a
+          href={`mailto:${DEVDEVDEV_EMAIL}`}
+          className='flex justify-center items-center gap-3 text-gray4'
+        >
+          <Image src={envelope} alt='메일 아이콘' />
+          <span>{DEVDEVDEV_EMAIL}</span>
+        </a>
+
+        <div className='ml-[2.4rem] flex items-center gap-5'>
+          <Link href={DEVDEVDEV_NOTION} target='_blank'>
+            <Image src={instagram} alt='인스타그램 아이콘' />
+          </Link>
+          <Link href={DEVDEVDEV_YOUTUBE} target='_blank'>
+            <Image src={youtube} alt='유투브 아이콘' />
+          </Link>
+          <Link href={DEVDEVDEV_INSTAGRAM} className=' text-gray3' target='_blank'>
+            개인정보 처리방침
+          </Link>
+        </div>
       </nav>
+
       <p className='c1 text-gray4'>Copyright &copy; 2024 꿈빛파티시엘 All rights reserved.</p>
     </footer>
   );
