fd550c14ea2450ddf0dd61f85a581d1649f84d6e
minju moon <mandelina24@gmail.com>
2024-06-08 00:22:12 +0900

    Feat : 푸터 제작

--- components/common/footer/Footer.tsx
+++ components/common/footer/Footer.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
+import { DEVDEVDEV_INSTAGRAM, DEVDEVDEV_NOTION, DEVDEVDEV_YOUTUBE } from '@/constants/FooterLink';
 import envelope from '@/public/image/footer/envelope.svg';
 import instagram from '@/public/image/footer/instagram.svg';
 import youtube from '@/public/image/footer/youtube.svg';
 
 export default function Footer() {
   return (
-    <footer className='flex justify-center items-center'>
+    <footer className='flex justify-between items-center px-[20.3rem]'>
       <nav>
         <ul className='flex'>
           <li className='mr-6'>꿈빛파티시엘</li>
-          <li className='flex justify-center items-center'>
+          <li className='flex justify-center items-center gap-3  text-gray4'>
             <Image src={envelope} alt='메일 아이콘' />
             <p>dreamy5patisiel@gmail.com</p>
           </li>
-          <li>
-            <Image src={instagram} alt='인스타그램 아이콘' />
-          </li>
-          <li>
-            <Image src={youtube} alt='유투브 아이콘' />
-          </li>
-          <li>개인정보 처리방침</li>
+          <div className='ml-[2.4rem] flex items-center gap-5'>
+            <Link href={DEVDEVDEV_NOTION} target='_blank'>
+              <Image src={instagram} alt='인스타그램 아이콘' />
+            </Link>
+            <Link href={DEVDEVDEV_YOUTUBE} target='_blank'>
+              <Image src={youtube} alt='유투브 아이콘' />
+            </Link>
+            <Link href={DEVDEVDEV_INSTAGRAM} className=' text-gray3' target='_blank'>
+              개인정보 처리방침
+            </Link>
+          </div>
         </ul>
       </nav>
-      <p className='c1 text-gray4'>&copy; Copyright ⓒ 2024 꿈빛파티시엘 All rights reserved.</p>
+      <p className='c1 text-gray4'>Copyright &copy; 2024 꿈빛파티시엘 All rights reserved.</p>
     </footer>
   );
 }
---
+++ constants/FooterLink.ts
@@ -0,0 +1,3 @@
+export const DEVDEVDEV_NOTION = 'https://www.notion.so/cc6a4a90d41846fb8faea02bf5b3b6c2?pvs=4';
+export const DEVDEVDEV_YOUTUBE = 'https://www.youtube.com/@dreamy5patisiel';
+export const DEVDEVDEV_INSTAGRAM = 'https://www.instagram.com/devdevdev_official/';
