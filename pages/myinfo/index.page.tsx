9c9e1e52689a67b6baf2f6faa5f6887ac38d624d
minju moon <mandelina24@gmail.com>
2024-06-08 17:21:57 +0900

    Feat : 헤더 , 내정보에 닉네임 & 이메일 렌더

--- pages/myinfo/index.page.tsx
+++ pages/myinfo/index.page.tsx
@@ -1,18 +1,42 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 
 import Link from 'next/link';
 
+import { UserInfoType } from '@/types/userInfoType';
+
 export default function Index({ children }: { children: ReactNode }) {
+  const [userInfoObj, setUserInfoObj] = useState<UserInfoType>({
+    accessToken: '',
+    nickname: '',
+    email: '',
+  });
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem('userInfo');
+    if (userInfo) {
+      const userInfoObj: UserInfoType = JSON.parse(userInfo);
+      const USER_EMAIL = userInfoObj.email;
+      const USER_NICKNAME = userInfoObj.nickname;
+      const USER_ACCESS_TOKEN = userInfoObj.accessToken;
+
+      setUserInfoObj({
+        email: USER_EMAIL,
+        nickname: USER_NICKNAME,
+        accessToken: USER_ACCESS_TOKEN,
+      });
+    }
+  }, []);
+
   return (
     <div
       className='px-[20.3rem] py-[6.4rem] grid grid-flow-col gap-[4.8rem]'
       style={{ gridTemplateColumns: '22% 78%' }}
     >
       <section className='w-full'>
-        <p className='st1 text-bold mb-[1.6rem]'>
-          <span className='text-point1'>게으른 댑댑이</span>님
+        <p className='st1 font-bold mb-[1.6rem]'>
+          <span className='text-point1'>{userInfoObj.nickname}</span>님
         </p>
-        <p className='p2 text-gray4'> dreaming_turlte@gmail.com </p>
+        <p className='p2 text-gray4'>{userInfoObj.email}</p>
         <ul className='flex flex-col p1 text-gray4 mt-16'>
           <Link href='/myinfo/mypost' className='p-7 hover:text-gray5'>
             내가 썼어요
