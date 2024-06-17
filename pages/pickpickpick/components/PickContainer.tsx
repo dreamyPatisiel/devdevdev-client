e3afc99ba23e869a26f78e5ba6fcb816475b9926
minyoung <alsduddl9977@gmail.com>
2024-06-18 04:32:17 +0900

    fix: 픽 컨테이너 전체 높이 수정

--- pages/pickpickpick/components/PickContainer.tsx
+++ pages/pickpickpick/components/PickContainer.tsx
@@ -50,7 +50,7 @@ export default function PickContainer({
   };
 
   return (
-    <div className='rounded-[1.6rem] border-gray2 border-solid border h-[43.5rem]'>
+    <div className='rounded-[1.6rem] border-gray2 border-solid border h-[42.3rem]'>
       <div
         className={`bg-gray1 px-[2.4rem] py-[2.8rem] mb-[3.2rem] rounded-t-[1.6rem] h-[10.8rem] ${disabledStyle}`}
       >
@@ -68,7 +68,7 @@ export default function PickContainer({
               key={option.id}
               {...option}
               isVoted={pickData.isVoted}
-              className='h-[9.5rem]'
+              className='h-[9rem]'
             />
           ))}
         </ul>
