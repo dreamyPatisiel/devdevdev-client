d3cfc2f5a08192dff8394b6c2ecb699021dc3bf6
minju moon <mandelina24@gmail.com>
2024-06-23 01:01:51 +0900

    Feat : 메타태그 컴포넌트 , 메타 데이터 정의

--- components/meta/MetaHead.tsx
+++ components/meta/MetaHead.tsx
@@ -1,16 +1,20 @@
 import Head from 'next/head';
 
-import 뎁뎁뎁로고 from '@public/image/devdevdevLogo.svg';
+import 뎁구리_og from '@public/image/뎁구리/뎁구리_og.png';
+
+import { META, SITE_URL } from '@/constants/metaData';
 
 const MetaHead = ({
   title,
   description,
   url,
+  keyword,
   image,
 }: {
   title?: string;
   description?: string;
   url?: string;
+  keyword?: string[];
   image?: string;
 }) => {
   return (
@@ -18,12 +22,17 @@ const MetaHead = ({
       <title>{title || 'devdevdev'}</title>
       <meta name='description' content={description || '힘들고 막힐 때는 댑댑댑'} />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name='keywords' content={keyword?.join(',') || META.MAIN.keyword.join(',')} />
       <meta property='og:title' content={title || 'devdevdev'} />
       <meta property='og:type' content='website' />
       {/* TODO: 최종 url은 변경 필요 */}
-      <meta property='og:url' content={url || 'https://dev.devdevdev.co.kr/'} />
-      <meta property='og:image' content={image || 뎁뎁뎁로고} />
-      <meta property='og:article:author' content='누구나누리' />
+      <meta property='og:url' content={url || SITE_URL} />
+      <meta property='og:image' content={image || 뎁구리_og.src} />
+
+      {/* 트위터용 */}
+      <meta name='twitter:title' content={title || 'devdevdev'} />
+      <meta name='twitter:description' content='힘들고 막힐 때는 댑댑댑' />
+      <meta name='twitter:image' content={image || 뎁구리_og.src} />
     </Head>
   );
 };
