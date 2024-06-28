5e39faea5ef957e939d4533f68e7c531f3d58b14
minju Moon <mandelina24@gmail.com>
2024-06-28 10:41:47 +0900

    Fix : 중복 스타일 리팩토링

--- pages/myinfo/components/NoMyInfoData.tsx
+++ pages/myinfo/components/NoMyInfoData.tsx
@@ -11,19 +11,22 @@ export default function NoMyInfoData({ type }: { type: 'pickpickpick' | 'techblo
   const router = useRouter();
   const btnText = type === 'pickpickpick' ? '픽픽픽 작성하기' : '기술블로그로 이동하기 ';
 
+  const TITLE_STYLE = 'st2 font-bold';
+  const SUBTITLE_STYLE = 'p1 text-gray4 mb-[2.4rem]';
+
   return (
-    <div className='flex flex-col items-center justify-center gap-[2.4rem]'>
+    <div className='flex flex-col items-center justify-center gap-[2rem]'>
       <p className='text-[5.6rem] mb-[2.4rem] mt-[7.2rem]'>😳</p>
 
       {type === 'pickpickpick' ? (
         <>
-          <p className='st2 font-bold'>아직 작성한 픽픽픽이 없어요!</p>
-          <p className='p1 text-gray4'>픽픽픽을 작성하러 가볼까요?</p>
+          <p className={TITLE_STYLE}>아직 작성한 픽픽픽이 없어요!</p>
+          <p className={SUBTITLE_STYLE}>픽픽픽을 작성하러 가볼까요?</p>
         </>
       ) : (
         <>
-          <p className='st2 font-bold'>북마크 하신 기술블로그 게시물이 없어요!</p>
-          <p className='p1 text-gray4'>따끈따끈한 게시물들 구경하러 가볼까요?</p>
+          <p className={TITLE_STYLE}>북마크 하신 기술블로그 게시물이 없어요!</p>
+          <p className={SUBTITLE_STYLE}>따끈따끈한 게시물들 구경하러 가볼까요?</p>
         </>
       )}
 
