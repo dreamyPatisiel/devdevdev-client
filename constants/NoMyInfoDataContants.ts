f5b218e47e13fb33a9a6b238be3294e9ed9d68cd
minju moon <mandelina24@gmail.com>
2024-06-30 10:52:18 +0900

    Fix : 내정보 없을때 컴포넌트 상수값 분리, 공통컴포넌트 처리

---
+++ constants/NoMyInfoDataContants.ts
@@ -0,0 +1,10 @@
+export const NO_MYINFO_DATA = {
+  PICKPICKPICK: {
+    TITLE: '아직 작성한 픽픽픽이 없어요!',
+    SUBTITLE: '픽픽픽을 작성하러 가볼까요?',
+  },
+  BOOKMARK: {
+    TITLE: '북마크 하신 기술블로그 게시물이 없어요!',
+    SUBTITLE: '따끈따끈한 게시물들 구경하러 가볼까요?',
+  },
+};
--- pages/myinfo/components/NoMyInfoData.tsx
+++ pages/myinfo/components/NoMyInfoData.tsx
@@ -7,29 +7,29 @@ import { MainButton } from '@components/common/buttons/mainButtons';
 
 import ArrowLeft from '@public/image/arrow-left-2.svg';
 
-export default function NoMyInfoData({ type }: { type: 'pickpickpick' | 'techblog' }) {
-  const router = useRouter();
-  const btnText = type === 'pickpickpick' ? '픽픽픽 작성하기' : '기술블로그로 이동하기 ';
+import { NO_MYINFO_DATA } from '@/constants/NoMyInfoDataContants';
 
+const TitleContent = ({ title, subTitle }: { title: string; subTitle: string }) => {
   const TITLE_STYLE = 'st2 font-bold';
   const SUBTITLE_STYLE = 'p1 text-gray4 mb-[2.4rem]';
 
   return (
+    <>
+      <p className={TITLE_STYLE}>{title}</p>
+      <p className={SUBTITLE_STYLE}>{subTitle}</p>
+    </>
+  );
+};
+
+export default function NoMyInfoData({ type }: { type: 'pickpickpick' | 'techblog' }) {
+  const router = useRouter();
+  const btnText = type === 'pickpickpick' ? '픽픽픽 작성하기' : '기술블로그로 이동하기 ';
+  const message = type === 'pickpickpick' ? NO_MYINFO_DATA.PICKPICKPICK : NO_MYINFO_DATA.BOOKMARK;
+
+  return (
     <div className='flex flex-col items-center justify-center gap-[2rem]'>
       <p className='text-[5.6rem] mb-[2.4rem] mt-[7.2rem]'>😳</p>
-
-      {type === 'pickpickpick' ? (
-        <>
-          <p className={TITLE_STYLE}>아직 작성한 픽픽픽이 없어요!</p>
-          <p className={SUBTITLE_STYLE}>픽픽픽을 작성하러 가볼까요?</p>
-        </>
-      ) : (
-        <>
-          <p className={TITLE_STYLE}>북마크 하신 기술블로그 게시물이 없어요!</p>
-          <p className={SUBTITLE_STYLE}>따끈따끈한 게시물들 구경하러 가볼까요?</p>
-        </>
-      )}
-
+      <TitleContent title={message.TITLE} subTitle={message.SUBTITLE} />
       <MainButton
         text={btnText}
         variant='primary'
