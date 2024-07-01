eff73c56cfae774dca46bd6084a6a084e80a87b1
MinYoung Kim <84004367+minyoung22222@users.noreply.github.com>
2024-07-02 00:23:32 +0900

    Update DevGuriErrorTxtConstants.ts

--- constants/DevGuriErrorTxtConstants.ts
+++ constants/DevGuriErrorTxtConstants.ts
@@ -3,11 +3,11 @@ export const DEVGURI_ERR_TEXT = {
     BUTTON_TEXT: '링크 복사하기',
     MAIN_TITLE: '화면 너비를 키워주세요',
     SUB_TITLE1: '아쉽게도 지원하지 않는 해상도에요.',
-    SUB_TITLE2: '최대한 빠르게 지원하도록 댑구리가 힘내고 있어요',
+    SUB_TITLE2: '최대한 빠르게 지원하도록 댑구리가 힘내고 있어요.',
   },
   NETWORK_ERR: {
     BUTTON_TEXT: '새로고침',
-    MAIN_TITLE: '잠시 후 다시 시도해주세요',
+    MAIN_TITLE: '잠시 후 다시 시도해주세요.',
     SUB_TITLE1: '요청사항을 처리하는데 실패했어요.',
   },
 };
