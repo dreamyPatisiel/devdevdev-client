1688af637088cf6e50b27cef55eec79963a4eaa6
minju moon <mandelina24@gmail.com>
2024-07-15 22:45:20 +0900

    Fix : startTransition를 이용하여 검색어 자동완성 UX 개선중

--- components/common/searchInput.tsx
+++ components/common/searchInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState, startTransition } from 'react';
 
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -9,15 +9,6 @@ import { useCompanyIdStore, useSearchKeywordStore } from '@stores/techBlogStore'
 import { useToastVisibleStore } from '@stores/toastVisibleStore';
 
 import Search from '@public/image/techblog/search.svg';
-
-const PointedText = ({ keyword, text }: { keyword: string; text: string }) => {
-  return (
-    <p className='text-p2 py-[1rem] w-full'>
-      <span className='text-point1'>{keyword}</span>
-      <span className='text-gray4'>{text}</span>
-    </p>
-  );
-};
 
 const NoMatchingKeywords = () => {
   return <p className='text-p2 py-[1rem] w-full text-gray4'>일치하는 키워드가 없어요</p>;
@@ -33,6 +24,8 @@ export default function SearchInput() {
 
   const [keyword, setKeyword] = useState('');
   const [debouncedKeyword, setDebouncedKeyword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const forbiddenCharsPattern = /[!^()-+/[\]{}:]/;
 
@@ -40,8 +33,16 @@ export default function SearchInput() {
 
   useEffect(() => {
     const debounceTimeout = setTimeout(() => {
-      setDebouncedKeyword(keyword);
-    }, 1000);
+      startTransition(() => {
+        setIsVisible(true);
+        setIsLoading(true);
+        setDebouncedKeyword(keyword);
+      });
+      return () => {
+        clearTimeout(debounceTimeout);
+        setIsLoading(false);
+      };
+    }, 100);
 
     return () => {
       clearTimeout(debounceTimeout);
@@ -53,6 +54,12 @@ export default function SearchInput() {
       setKeyword('');
     }
   }, [searchKeyword]);
+
+  useEffect(() => {
+    if (status === 'success' || status === 'error') {
+      setIsLoading(false);
+    }
+  }, [status]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
@@ -84,10 +91,34 @@ export default function SearchInput() {
     }
   };
 
-  const handleKeywordChage = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleKeywordChange = (e: ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
   };
 
+  const PointedText = ({
+    keyword,
+    text,
+    suggestion,
+  }: {
+    keyword: string;
+    text: string;
+    suggestion: string;
+  }) => {
+    const handleKeywordClick = () => {
+      console.log('keyword', keyword);
+      setCompanyId(undefined);
+      setKeyword(suggestion); // input의 text
+      setSearchKeyword(suggestion);
+      setIsLoading(false);
+      setIsVisible(false);
+    };
+    return (
+      <p className='text-p2 py-[1rem] w-full' onClick={handleKeywordClick}>
+        <span className='text-point1'>{keyword}</span>
+        <span className='text-gray4'>{text}</span>
+      </p>
+    );
+  };
   return (
     <div className='bg-gray2 rounded-[0.8rem] w-[28rem] px-[1.6rem]'>
       <div className='relative flex flex-row justify-between '>
@@ -95,27 +126,34 @@ export default function SearchInput() {
           placeholder='키워드 검색을 해보세요'
           className='w-[21rem] py-[0.8rem] bg-gray2 text-white p2 focus:outline-none'
           value={keyword}
-          onChange={handleKeywordChage}
+          onChange={handleKeywordChange}
           onKeyDown={handleKeyDown}
         />
         <button className='cursor-pointer' onClick={handleClickSearchBtn}>
           <Image src={Search} alt='검색아이콘' />
         </button>
       </div>
-      <div
-        className='fixed bg-gray2 w-[28rem] px-[1.6rem] rounded-[0.8rem]'
-        style={{ zIndex: 1000 }}
-      >
-        {status === 'success' && data.length > 0 ? (
-          data.map((suggestion: string, index: number) => {
-            const regex = new RegExp(keyword, 'g');
-            const text = suggestion.replace(regex, '').trim();
-            return <PointedText key={index} keyword={keyword} text={text} />;
-          })
-        ) : (
-          <>{keyword && <NoMatchingKeywords />}</>
-        )}
-      </div>
+      {isVisible && (
+        <div
+          className='fixed bg-gray2 w-[28rem] px-[1.6rem] rounded-[0.8rem]'
+          style={{ zIndex: 1000 }}
+        >
+          {isLoading ? (
+            <p className='text-p2 py-[1rem] w-full text-gray4'>로딩 중...</p>
+          ) : status === 'success' && data.length > 0 ? (
+            data.map((suggestion: string, index: number) => {
+              const regex = new RegExp(keyword, 'i'); // 대소문자 구분 없이 검색
+              const match = suggestion.match(regex);
+              const text = match ? suggestion.replace(regex, '').trim() : suggestion;
+              return (
+                <PointedText key={index} keyword={keyword} text={text} suggestion={suggestion} />
+              );
+            })
+          ) : (
+            <>{keyword && <NoMatchingKeywords />}</>
+          )}
+        </div>
+      )}
     </div>
   );
 }
