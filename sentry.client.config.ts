ad74b5fa05ed6672bc53b56367058ef502ae248f
minyoung <alsduddl9977@gmail.com>
2024-07-30 12:11:19 +0900

    fix: 디버그 모드 환경에 따라 활성화 설정

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,5 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
+const SENTRY_DEBUG = process.env.NEXT_PUBLIC_SENTRY_DEBUG === 'true';
+
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN, // 프로젝트의 DSN을 설정하여 Sentry 서버에 이벤트를 전송
   environment: process.env.NODE_ENV, //환경을 설정하여 에러가 발생한 환경을 구분
@@ -7,7 +9,7 @@ Sentry.init({
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0, // 모든 트랜잭션을 추적하도록 샘플링 비율을 1.0으로 설정
-  debug: true, // 디버그 로그를 활성화
+  debug: SENTRY_DEBUG, // 디버그 로그를 활성화 (production에서는 비활성화)
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
