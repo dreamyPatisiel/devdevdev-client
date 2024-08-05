b54385e53310dfbbffb7010a4031ba69b6014e80
minju moon <mandelina24@gmail.com>
2024-08-06 00:07:58 +0900

    Style : 기술블로그 상단 컴포넌트들 모바일용 레이아웃 셋팅 , 모바일 페이지 주석처리

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -44,31 +44,31 @@ export default function Layout({ children }: { children: ReactNode }) {
 
   return (
     <>
-      {isMobile ? (
+      {/* {isMobile ? (
         <>
           <MobileHeader />
           <Toast />
           <DevGuriError type='mobile' pathname={pathname} />
         </>
-      ) : (
-        <div
-          ref={scrollContainerRef}
-          className={`${PretendardVariable.className} overflow-x-auto box-border grid grid-rows-[8.5rem,1fr,5vh] h-screen text-white`}
-        >
-          <Header />
-          <AuthModal />
-          <QueryErrorBoundary>
-            <div className='flex justify-center w-full'>
-              <main className='w-full min-w-[1440px] max-w-[1920px]'>
-                <Toast />
-                {children}
-                {pathname !== '/' && <GoToTopButton scrollContainerRef={scrollContainerRef} />}
-              </main>
-            </div>
-            <Footer />
-          </QueryErrorBoundary>
-        </div>
-      )}
+      ) : ( */}
+      <div
+        ref={scrollContainerRef}
+        className={`${PretendardVariable.className} overflow-x-auto box-border grid grid-rows-[8.5rem,1fr,5vh] h-screen text-white`}
+      >
+        <Header />
+        <AuthModal />
+        <QueryErrorBoundary>
+          <div className='flex justify-center w-full'>
+            <main className='w-full min-w-[1440px] max-w-[1920px]'>
+              <Toast />
+              {children}
+              {pathname !== '/' && <GoToTopButton scrollContainerRef={scrollContainerRef} />}
+            </main>
+          </div>
+          <Footer />
+        </QueryErrorBoundary>
+      </div>
+      {/* )} */}
     </>
   );
 }
