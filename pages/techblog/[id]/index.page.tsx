0f59ca57681c1301e5d609e74d323c1255b9b7f8
minju Moon <mandelina24@gmail.com>
2024-08-18 10:22:49 +0900

    Fix : Carrer -> Career 오타 수정

--- pages/techblog/[id]/index.page.tsx
+++ pages/techblog/[id]/index.page.tsx
@@ -58,14 +58,14 @@ export default function Page() {
       case 'success':
         if (!CurDetailTechBlogData) return;
         const { company } = CurDetailTechBlogData;
-        const TechCarrerBaseStyle = 'flex py-[3.1rem] border border-gray2 rounded-[1.6rem]';
-        const TechCarrerMobileStyle = `flex-col gap-9 px-[2.4rem] mb-[2.7rem]`;
-        const TechCarrerDesktopStyle = `flex-row items-center justify-between px-[3.2rem]`;
+        const TechCareerBaseStyle = 'flex py-[3.1rem] border border-gray2 rounded-[1.6rem]';
+        const TechCareerMobileStyle = `flex-col gap-9 px-[2.4rem] mb-[2.7rem]`;
+        const TechCareerDesktopStyle = `flex-row items-center justify-between px-[3.2rem]`;
         return (
           <article className={isMobile ? 'px-[1.6rem] py-[4rem]' : 'px-[20.4rem] py-[6.4rem]'}>
             <TechDetailCard techDetailProps={CurDetailTechBlogData} techArticleId={techArticleId} />
             <section
-              className={`${TechCarrerBaseStyle} ${isMobile ? TechCarrerMobileStyle : TechCarrerDesktopStyle}`}
+              className={`${TechCareerBaseStyle} ${isMobile ? TechCareerMobileStyle : TechCareerDesktopStyle}`}
             >
               <CompanyTitle
                 title={company.name}
