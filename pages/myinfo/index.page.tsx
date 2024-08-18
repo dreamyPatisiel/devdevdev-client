3a4dc91a3c2c3cd69ba4add71498d4a5f31d0d21
minyoung <alsduddl9977@gmail.com>
2024-08-18 17:07:50 +0900

    feat: 내정보 헤더 모바일 UI 적용

--- pages/myinfo/index.page.tsx
+++ pages/myinfo/index.page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router';
 
 import { useUserInfoStore } from '@stores/userInfoStore';
 
+import useIsMobile from '@hooks/useIsMobile';
+
 import { NO_USER_NAME } from '@/constants/UserInfoConstants';
 import { ROUTES } from '@/constants/routes';
 
@@ -19,12 +21,13 @@ export default function MyInfo({ children }: { children: ReactNode }) {
   const currentPath = router.pathname;
 
   const { userInfo } = useUserInfoStore();
+  const isMobile = useIsMobile();
 
   const ACTIVE_CLASS = 'bg-gray1 rounded-xl text-white font-bold';
 
   return (
     <div
-      className='px-[20.3rem] py-[6.4rem] grid grid-flow-col gap-[4.8rem]'
+      className={`${isMobile ? 'px-[1.6rem]' : 'grid grid-flow-col px-[20.3rem] py-[6.4rem] gap-[4.8rem]'}`}
       style={{ gridTemplateColumns: '21% 79%' }}
     >
       <section className='w-full'>
@@ -32,12 +35,14 @@ export default function MyInfo({ children }: { children: ReactNode }) {
           <span className='text-point1'>{userInfo.nickname || NO_USER_NAME}</span>님
         </p>
         <p className='p2 text-gray4'>{userInfo.email}</p>
-        <ul className='flex flex-col p1 text-gray4 mt-16'>
+        <ul
+          className={`flex p1 text-gray4 mt-16 ${isMobile ? ' justify-between mb-[3.2rem]' : 'flex-col '}`}
+        >
           {MYINFO_LINKS.map((link, index) => (
             <Link
               key={index}
               href={link.href}
-              className={`p-7 hover:text-white ${currentPath === link.href ? ACTIVE_CLASS : ''}`}
+              className={` hover:text-white ${currentPath === link.href ? ACTIVE_CLASS : ''} ${isMobile ? 'p-[1.8rem] w-[11.9rem] h-[5.1rem] flex justify-center items-center' : 'p-7'}`}
             >
               {link.label}
             </Link>
