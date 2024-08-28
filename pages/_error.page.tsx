34f98c8d9143622fe1c67f8bbf493ebb0978a957
minyoung <alsduddl9977@gmail.com>
2024-08-29 00:39:22 +0900

    feat: 모바일 버전 404, 에러 이미지 적용

--- pages/_error.page.tsx
+++ pages/_error.page.tsx
@@ -3,10 +3,13 @@ import { useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
+import useIsMobile from '@hooks/useIsMobile';
+
 import { MainButton } from '@components/common/buttons/mainButtons';
 
 import RetryIcon from '@public/assets/ReplayIcon';
 import ArrowLeft from '@public/image/arrow-left-2.svg';
+import MobileErrorImage from '@public/image/error-mobile.svg';
 import ErrorImage from '@public/image/error.svg';
 
 import { PAGE_ERROR_MESSAGE } from '@/constants/errorMessageConstants';
@@ -16,6 +19,7 @@ import * as Sentry from '@sentry/nextjs';
 
 export default function ErrorPage({ resetErrorBoundary }: { resetErrorBoundary: () => void }) {
   const router = useRouter();
+  const isMobile = useIsMobile();
 
   const handleRetryClick = () => {
     resetErrorBoundary();
@@ -29,7 +33,11 @@ export default function ErrorPage({ resetErrorBoundary }: { resetErrorBoundary: 
 
   return (
     <div className='flex flex-col items-center mt-[11.8rem]'>
-      <Image src={ErrorImage} alt='에러 이미지' />
+      {isMobile ? (
+        <Image src={MobileErrorImage} alt='에러 이미지' />
+      ) : (
+        <Image src={ErrorImage} alt='에러 이미지' />
+      )}
       <p className='st2 font-bold mt-[4.629rem] mb-[3.2rem]'>{PAGE_ERROR_MESSAGE}</p>
 
       <MainButton
