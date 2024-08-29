f68dfbea542e13f2a661a6873fa98d08a4fa6a01
minyoung <alsduddl9977@gmail.com>
2024-08-29 22:40:27 +0900

    refactor: dropdownOptionToKorean util 함수로 분리

--- components/common/dropdowns/mobileDropdown.tsx
+++ components/common/dropdowns/mobileDropdown.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import Image from 'next/image';
 
+import { dropdownOptionToKorean } from '@utils/dropdownOptionToKorean';
+
 import { DropdownOptionProps, useDropdownStore } from '@stores/dropdownStore';
 
 import AngleDown from '@public/image/dropdown-angle-down.svg';
@@ -37,25 +39,6 @@ export default function MobileDropdown({
       break;
   }
 
-  const mapToKorean = (englishOption: DropdownOptionProps) => {
-    switch (englishOption) {
-      case 'LATEST':
-        return '최신순';
-      case 'POPULAR':
-        return '인기순';
-      case 'MOST_VIEWED':
-        return '조회순';
-      case 'MOST_COMMENTED':
-        return '댓글 많은 순';
-      case 'BOOKMARKED':
-        return '등록순';
-      case 'HIGHEST_SCORE':
-        return '정확도순';
-      default:
-        return '';
-    }
-  };
-
   const handleOptionSelected = (value: DropdownOptionProps) => () => {
     setSort(value);
     setShowBottom(false);
@@ -67,7 +50,7 @@ export default function MobileDropdown({
         className='rounded-[10rem] px-[1.4rem] py-[0.8rem] bg-gray1 text-gray4 flex gap-[0.8rem] c1 font-bold cursor-pointer'
         onClick={() => setShowBottom(true)}
       >
-        {mapToKorean(sortOption)}
+        {dropdownOptionToKorean(sortOption)}
         <Image src={AngleDown} alt='아래방향 화살표' />
       </div>
 
@@ -81,7 +64,7 @@ export default function MobileDropdown({
                 onClick={handleOptionSelected(option as DropdownOptionProps)}
                 className={`px-[2.4rem] py-[1.2rem] st2 text-gray4 cursor-pointer ${sortOption === option ? 'text-point1 bg-gray2 rounded-[0.8rem]' : 'hover:text-gray5'}`}
               >
-                {mapToKorean(option as DropdownOptionProps)}
+                {dropdownOptionToKorean(option as DropdownOptionProps)}
               </li>
             ))}
           </ul>
