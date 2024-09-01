17f94765d4354746d00294523ab0a718cf2c9dac
minyoung <alsduddl9977@gmail.com>
2024-09-01 14:18:26 +0900

    feat: 모달 모바일 구현

--- components/common/buttons/subButtons.tsx
+++ components/common/buttons/subButtons.tsx
@@ -1,10 +1,14 @@
 import { cn } from '@utils/mergeStyle';
 
+import useIsMobile from '@hooks/useIsMobile';
+
 import { Spinner } from '@chakra-ui/spinner';
 
 import { LogoutButtonProps, ModalButtonProps, SubButtonProps } from './types/subButtons';
 import {
+  desktopModalButtonBaseStyle,
   LogoutButtonVariants,
+  mobileModalButtonBaseStyle,
   ModalButtonVariants,
   SubButtonVariants,
 } from './variants/subButtons';
@@ -22,10 +26,13 @@ export function SubButton({ text, variant, disabled, onClick, className }: SubBu
 }
 
 export function ModalButton({ text, variant, disabled, onClick, isPending }: ModalButtonProps) {
+  const isMobile = useIsMobile();
+
   return (
     <button
       className={cn(
         ModalButtonVariants({ variant }),
+        isMobile ? mobileModalButtonBaseStyle : desktopModalButtonBaseStyle,
         'flex items-center gap-[1rem]',
         `${isPending && 'pl-[1.9rem] bg-primary5'}`,
       )}
