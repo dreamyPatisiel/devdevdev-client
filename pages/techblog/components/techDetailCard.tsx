42883dbdb6b71fd84313a91a4806d31d55e1bb2e
minju Moon <mandelina24@gmail.com>
2024-09-08 17:40:33 +0900

    Fix : 기술블로그 상단 간격 삭제

--- pages/techblog/components/techDetailCard.tsx
+++ pages/techblog/components/techDetailCard.tsx
@@ -69,9 +69,7 @@ export default function TechDetailCard({
 
   return (
     <section className={`${isMobile ? 'mb-[5.6rem]' : 'mb-[9.6rem]'}`}>
-      <div
-        className={`flex items-center justify-between  ${isMobile ? 'mb-[2.4rem]' : 'mb-[4.8rem]'}`}
-      >
+      <div className={`flex items-center justify-between  ${isMobile ? '' : 'mb-[4.8rem]'}`}>
         {!isMobile && (
           <>
             <Link
