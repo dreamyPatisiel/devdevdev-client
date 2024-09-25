1633d69fda9098c6b0c9cc00f2ab6f5ca06c39d0
minyoung <alsduddl9977@gmail.com>
2024-09-26 02:49:32 +0900

    feat: formatISOtoDate 함수 구현, 적용

--- components/common/comments/CommentHeader.tsx
+++ components/common/comments/CommentHeader.tsx
@@ -1,3 +1,5 @@
+import { formatISOtoDate } from '@utils/formatDate';
+
 import MoreButton from '../moreButton';
 import { StatusTag } from '../tags';
 
@@ -15,8 +17,8 @@ export default function CommentHeader({
   isDeleted,
   author,
   maskedEmail,
-  createdAt = '2024.01.01',
-  isCommentAuthor = false,
+  createdAt,
+  isCommentAuthor,
 }: CommentHeaderProps) {
   const moreButtonList = isCommentAuthor ? ['수정', '삭제'] : ['신고'];
 
@@ -25,7 +27,7 @@ export default function CommentHeader({
       <span className='flex items-center'>
         <span className='c1 text-gray5 font-bold'>{`${author}(${maskedEmail})`}</span>
         {isPickAuthor ? <StatusTag text='작성자' bgColor='point1' /> : null}
-        <span className='c1 text-gray3 ml-[2rem]'>{createdAt}</span>
+        <span className='c1 text-gray3 ml-[2rem]'>{formatISOtoDate(createdAt || '')}</span>
       </span>
 
       {isDeleted ? null : <MoreButton moreButtonList={moreButtonList} type='small' />}
--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -3,3 +3,10 @@ export const formatDate = (dateString: string) => {
   const [year, month, day] = dateString.split('-');
   return `${year}.${month}.${day}`;
 };
+
+/** 서버에서 주는 YYYY-MM-DD HH:mm:ss 형식을 YYYY.MM.DD로 바꾸는 함수 */
+export const formatISOtoDate = (dateString: string) => {
+  const date = dateString.split(' ')[0];
+
+  return formatDate(date);
+};
