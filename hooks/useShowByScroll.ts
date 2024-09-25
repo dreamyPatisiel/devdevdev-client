42d5ec4353e9337e5f0749078093ed47c969167f
minyoung <alsduddl9977@gmail.com>
2024-09-25 16:31:29 +0900

    fix: useShowByScroll 디바운싱 제거

--- hooks/useShowByScroll.ts
+++ hooks/useShowByScroll.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import useScrollPosition from './useScrollController';
 import { useScrollDirection } from './useScrollDirection';
@@ -8,21 +8,18 @@ export default function useShowByScroll() {
   const scrollDirection = useScrollDirection();
   const { position } = useScrollPosition();
 
-  const timerId = useRef<ReturnType<typeof setTimeout> | null>(null);
-
-  const debounce = (func: Function, sec: number) => {
-    if (timerId.current) clearTimeout(timerId.current);
-    timerId.current = setTimeout(() => func(), sec);
-  };
-
   useEffect(() => {
     if (position === 'top') {
       return setShowBottom(true);
     }
 
-    if (scrollDirection === 'up') debounce(() => setShowBottom(true), 500);
+    if (position === 'bottom') {
+      return setShowBottom(false);
+    }
+
+    if (scrollDirection === 'up') setShowBottom(true);
     if (scrollDirection === 'down') setShowBottom(false);
-  }, [scrollDirection]);
+  }, [scrollDirection, position]);
 
   return { showBottom };
 }
