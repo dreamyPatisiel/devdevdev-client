a2d5fe1777c5df7fdd3b53e929a3948caaf06e4f
minyoung <alsduddl9977@gmail.com>
2024-09-26 23:17:13 +0900

    fix: isDeleted 수정, 예시 값 추가

--- pages/pickpickpick/[id]/components/Comment.tsx
+++ pages/pickpickpick/[id]/components/Comment.tsx
@@ -54,7 +54,7 @@ export default function Comment({
             />
           )}
 
-      <CommentContents comment={comment} isDeleted={false} />
+      <CommentContents comment={comment} isDeleted={isDeleted} />
     </div>
   );
 }
