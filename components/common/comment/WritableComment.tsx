7344355f5a9cf05a6f8cb91c1e87a6fc5e02a12f
MinjuMoon <mmj9908@greeneffect.co.kr>
2024-10-21 19:56:35 +0900

    Fix : 멘션시 parentCommentAuthor 가 없어도 @가 나오는 현상 제거

--- components/common/comment/WritableComment.tsx
+++ components/common/comment/WritableComment.tsx
@@ -75,7 +75,7 @@ export default function WritableComment({
           suppressContentEditableWarning={true}
           className='p2 text-[#BD79FF] pointer-events-none ml-0'
         >
-          @{parentCommentAuthor}{' '}
+          {parentCommentAuthor ? `@${parentCommentAuthor}` : ''}{' '}
         </span>
         <span
           contentEditable='true'
