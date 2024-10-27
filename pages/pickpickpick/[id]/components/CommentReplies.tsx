19a5c86725a10c1eb00dd83925e765cfc373a65f
minyoung <alsduddl9977@gmail.com>
2024-10-27 16:43:39 +0900

    feat: 댓글 전체 보기 기능 구현

--- pages/pickpickpick/[id]/components/CommentReplies.tsx
+++ pages/pickpickpick/[id]/components/CommentReplies.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Comment from './Comment';
 import { SubCommentsProps } from './CommentSet';
 
@@ -7,34 +9,46 @@ interface CommentRepliesProps {
 }
 
 export default function CommentReplies({ replies, pickId }: CommentRepliesProps) {
+  const [showAllComments, setShowAllComments] = useState(false);
+  const CAN_SHOW_COMMENT_COUNT = 3;
+  // 추후에 Count 변경하기
+
+  const handleShowAllComments = () => {
+    setShowAllComments(true);
+  };
   return (
     <>
       {replies
-        // ?.slice(0, 5)
+        ?.slice(0, CAN_SHOW_COMMENT_COUNT)
         ?.map((subComment) => (
           <Comment
             key={subComment.pickCommentId}
             isSubComment={true}
-            createdAt={subComment.createdAt}
-            isCommentOfPickAuthor={subComment.isCommentOfPickAuthor}
-            author={subComment.author}
-            maskedEmail={subComment.maskedEmail}
-            comment={subComment.contents}
-            isCommentAuthor={subComment.isCommentAuthor}
-            isDeleted={subComment.isDeleted}
-            isModified={subComment.isModified}
             votedPickOption={null}
             votedPickOptionTitle={null}
             pickId={pickId}
-            pickOriginParentCommentId={subComment.pickOriginParentCommentId}
-            pickParentCommentId={subComment.pickParentCommentId}
-            pickParentCommentAuthor={subComment.pickParentCommentAuthor}
-            pickCommentId={subComment.pickCommentId}
             type={'reply'}
-            isRecommended={subComment.isRecommended}
-            recommendTotalCount={subComment.recommendTotalCount}
+            {...subComment}
           />
         ))}
+      {showAllComments
+        ? replies
+            ?.slice(CAN_SHOW_COMMENT_COUNT, replies.length)
+            .map((subComment) => (
+              <Comment
+                key={subComment.pickCommentId}
+                isSubComment={true}
+                votedPickOption={null}
+                votedPickOptionTitle={null}
+                pickId={pickId}
+                type={'reply'}
+                {...subComment}
+              />
+            ))
+        : replies &&
+          replies.length > CAN_SHOW_COMMENT_COUNT && (
+            <button onClick={handleShowAllComments}>댓글 전체 보기 +</button>
+          )}
     </>
   );
 }
