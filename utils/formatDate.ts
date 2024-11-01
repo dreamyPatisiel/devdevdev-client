b29ad52945d6e6c1254c6313423922fe07a2d8f7
MinjuMoon <mmj9908@greeneffect.co.kr>
2024-11-01 16:48:50 +0900

    Fix : 댓글작성 시간 보이도록 format함수 수정

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -4,9 +4,11 @@ export const formatDate = (dateString: string) => {
   return `${year}.${month}.${day}`;
 };
 
-/** 서버에서 주는 YYYY-MM-DD HH:mm:ss 형식을 YYYY.MM.DD로 바꾸는 함수 */
+/** 서버에서 주는 YYYY-MM-DD HH:mm:ss 형식을 YYYY.MM.DD HH:mm로 바꾸는 함수 */
 export const formatISOtoDate = (dateString: string) => {
-  const date = dateString.split(' ')[0];
-
-  return formatDate(date);
+  const [date, time] = dateString.split(' ');
+  const formattedDate = date.replace(/-/g, '.');
+  const formattedTime = time.slice(0, 5); // HH:mm:ss에서 HH:mm만 가져오기
+  console.log(date, time);
+  return `${formattedDate} ${formattedTime}`;
 };
