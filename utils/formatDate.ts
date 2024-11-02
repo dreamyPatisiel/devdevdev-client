a425d4e1629f3ce6919330cfbbd8d670e3aeac8a
minju Moon <mandelina24@gmail.com>
2024-11-02 18:01:04 +0900

    Fix : 사용하지 않는 콘솔 제거

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -9,6 +9,5 @@ export const formatISOtoDate = (dateString: string) => {
   const [date, time] = dateString.split(' ');
   const formattedDate = date.replace(/-/g, '.');
   const formattedTime = time.slice(0, 5); // HH:mm:ss에서 HH:mm만 가져오기
-  console.log(date, time);
   return `${formattedDate} ${formattedTime}`;
 };
