5fd496f0c3624e7a8e3b491c9c56f78e95a39c02
minju Moon <mandelina24@gmail.com>
2024-11-03 14:38:21 +0900

    Feat : 모바일 스켈레톤 구현

--- components/common/skeleton/commentSkeleton.tsx
+++ components/common/skeleton/commentSkeleton.tsx
@@ -1,6 +1,6 @@
-import BorderRoundButton, { LikeButton, ReplyButton } from '../comment/borderRoundButton';
+import BorderRoundButton, { LikeButton } from '../comment/borderRoundButton';
 
-// 댓글 스켈레톤
+// pc버전 댓글 스켈레톤
 export const CommentSkeleton = () => {
   return (
     <div className='pb-[1.6rem]'>
@@ -30,6 +30,37 @@ export const CommentSkeletonList = ({ itemsInRows }: CommentSkeletonListProps) =
     <>
       {Array.from({ length: itemsInRows }, (_, index) => (
         <CommentSkeleton key={index} />
+      ))}
+    </>
+  );
+};
+
+// 모바일버전  댓글 스켈레톤
+export const MobileCommentSkeleton = () => {
+  return (
+    <div className='pb-[1.6rem]'>
+      <div className='flex flex-col py-[1.6rem] gap-[1.6rem]'>
+        <div className='w-[17.5rem] h-[2.4rem] rounded-[1.2rem] bg-[#29292E] relative skeleton-item'></div>
+        <div className='w-[6.9rem] h-[2.4rem] rounded-[1.2rem] bg-[#29292E] relative skeleton-item'></div>
+        <div className='w-full h-[16.8rem] rounded-[1.2rem] bg-[#29292E] mb-[0.8rem] relative skeleton-item overflow-hidden'></div>
+      </div>
+      <div className='flex gap-[8px]'>
+        <BorderRoundButton isActived={false} text='답글' onClick={() => {}} />
+        <LikeButton isLiked={false} likeCount={0} />
+      </div>
+    </div>
+  );
+};
+
+interface MobileCommentSkeletonListProps {
+  itemsInRows: number;
+}
+
+export const MobileCommentSkeletonList = ({ itemsInRows }: MobileCommentSkeletonListProps) => {
+  return (
+    <>
+      {Array.from({ length: itemsInRows }, (_, index) => (
+        <MobileCommentSkeleton key={index} />
       ))}
     </>
   );
