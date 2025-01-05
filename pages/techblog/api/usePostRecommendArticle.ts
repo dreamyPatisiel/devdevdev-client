c769afba3942b7eb187a5c7b4e5390581f68af2e
mandelina <mandelina24@gmail.com>
2025-01-05 21:53:12 +0900

    Feat : 기술블로그 추천 기능 개발

---
+++ pages/techblog/api/usePostRecommendArticle.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { getGA } from '@utils/getCookie';
+
+import { useToastVisibleStore } from '@stores/toastVisibleStore';
+
+import { ErrorRespone } from '@/types/errorResponse';
+import { SuccessResponse } from '@/types/successResponse';
+
+import { TechRecommendArticleStatus } from '../types/techBlogType';
+
+export const postRecommendArticle = async ({ techArticleId }: { techArticleId: string }) => {
+  const GA = await getGA();
+
+  const res = await axios.post<SuccessResponse<TechRecommendArticleStatus>>(
+    `/devdevdev/api/v1/articles/${techArticleId}/recommend`,
+    {},
+    {
+      headers: { 'Anonymous-Member-Id': GA },
+    },
+  );
+  return res.data;
+};
+
+export const usePostRecommendArticle = (techArticleId: string) => {
+  const queryClient = useQueryClient();
+  const { setToastVisible } = useToastVisibleStore();
+
+  return useMutation({
+    mutationFn: postRecommendArticle,
+    onError: (error: ErrorRespone) => {
+      const errorMessage = error.response.data.message;
+      setToastVisible(errorMessage, 'error');
+    },
+    onSuccess: async ({ data }) => {
+      console.log(data, 'data');
+
+      await queryClient.invalidateQueries({ queryKey: ['techDetail', techArticleId] });
+      if (data.status) {
+        setToastVisible('추천했어요!', 'success');
+      } else {
+        setToastVisible('취소했어요', 'success');
+      }
+    },
+  });
+};
