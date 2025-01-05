27e74b0d6db423de0b772cc9b72c802fa6b7e93f
mandelina <mandelina24@gmail.com>
2025-01-05 20:00:39 +0900

    Feat : 메인버튼 v2 구현 (업데이트 된 버튼)

---
+++ components/common/buttons/mainButtonsV2.tsx
@@ -0,0 +1,34 @@
+import { MainButtonV2Props } from './types/mainButtonsV2';
+import { MainButtonV2Variants } from './variants/mainButtonsV2';
+
+export function MainButtonV2({
+  text,
+  icon,
+  iconPosition = 'right',
+  disabled,
+  onClick,
+  type = 'button',
+  className,
+  color,
+  size,
+  radius,
+  line,
+  ...rest
+}: MainButtonV2Props) {
+  return (
+    <button
+      className={MainButtonV2Variants({ color, line, size, radius })}
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+      {...rest}
+    >
+      {/* 아이콘 왼쪽 정렬 */}
+      {iconPosition === 'left' && icon}
+      {/* 텍스트 */}
+      <span className='p1 font-bold'>{text}</span>
+      {/* 아이콘 오른쪽 정렬 */}
+      {iconPosition === 'right' && icon}
+    </button>
+  );
+}
