fc65ccbea12191a6bd8052dc61e0eb3540a1a779
minyoung <alsduddl9977@gmail.com>
2025-01-12 15:55:23 +0900

    fix: useCheckAndScrollToComment try-catch문으로 수정

--- hooks/useCheckAndScrollToComment.ts
+++ hooks/useCheckAndScrollToComment.ts
@@ -14,28 +14,36 @@ export const useCheckAndScrollToComment = ({
   fetchNextPage,
 }: CheckAndScrollToCommentProps) => {
   const { setToastVisible } = useToastVisibleStore();
+  const MAX_RETRIES = 10;
 
   useEffect(() => {
     const checkAndScrollToComment = async () => {
       if (!commentId || !hasNextPage) return;
 
-      let retryCount = 0;
-      const MAX_RETRIES = 10;
-      let commentElement = document.getElementById(`comment-${commentId}`);
+      try {
+        let retryCount = 0;
+        let commentElement = document.getElementById(`comment-${commentId}`);
 
-      while (!commentElement && hasNextPage && retryCount < MAX_RETRIES) {
-        retryCount++;
-        await fetchNextPage();
-        await new Promise((resolve) => setTimeout(resolve, 100));
-        commentElement = document.getElementById(`comment-${commentId}`);
+        while (!commentElement && hasNextPage && retryCount < MAX_RETRIES) {
+          retryCount++;
+          await fetchNextPage();
+          await new Promise((resolve) => setTimeout(resolve, 100));
+          commentElement = document.getElementById(`comment-${commentId}`);
+        }
+
+        if (commentElement) {
+          commentElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+          return setToastVisible({ message: `댓글을 찾았어요! 🥳` });
+        }
+
+        return setToastVisible({ message: `댓글을 찾을 수 없어요 🥲` });
+      } catch (error) {
+        console.error(error);
+        return setToastVisible({
+          message: `댓글을 찾는 도중 에러가 발생했어요 😢`,
+          type: 'error',
+        });
       }
-
-      if (commentElement) {
-        commentElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
-        return setToastVisible({ message: `댓글을 찾았어요! 🥳` });
-      }
-
-      return setToastVisible({ message: `댓글을 찾을 수 없어요 🥲` });
     };
 
     checkAndScrollToComment();
