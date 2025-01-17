1d08064060a473c6bb861cb5fa2972ced5bfc9f6
minyoung <alsduddl9977@gmail.com>
2025-01-17 21:06:59 +0900

    fix: getMaskedEmail 로직 수정

--- utils/getUserInfo.tsx
+++ utils/getUserInfo.tsx
@@ -13,5 +13,12 @@ export default function getUserInfoFromLocalStorage(): UserInfoType | null {
 }
 
 export const getMaskedEmail = (email: string) => {
-  return email.substring(0, 3).concat('*'.repeat(12));
+  const UNMASKED_COUNT = 3;
+
+  const id = email.slice(0, email.indexOf('@'));
+  const unmaskedId = id.slice(0, UNMASKED_COUNT);
+  const maskedCount = Math.max(0, id.length - UNMASKED_COUNT);
+  const maskedId = '*'.repeat(maskedCount);
+
+  return unmaskedId + maskedId;
 };
