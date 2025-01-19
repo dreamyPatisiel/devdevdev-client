de365c115d0e19d00df54b1d570496ac34b952d2
mandelina <mandelina24@gmail.com>
2025-01-19 17:45:30 +0900

    Fix  : try문 아래에 setTimeout으로 지연 주기 (못찾는 현상까지 해결하기 위함)

--- hooks/useCheckAndScrollToComment.ts
+++ hooks/useCheckAndScrollToComment.ts
@@ -28,27 +28,27 @@ export const useCheckAndScrollToComment = ({
 
       if (status === 'success') {
         try {
-          let retryCount = 0;
-          let commentElement = document.getElementById(`comment-${commentId}`);
+          setTimeout(async () => {
+            let retryCount = 0;
+            let commentElement = document.getElementById(`comment-${commentId}`);
 
-          while (!commentElement && hasNextPage && retryCount < MAX_RETRIES) {
-            retryCount++;
-            await fetchNextPage();
-            await new Promise((resolve) => setTimeout(resolve, 100));
-            commentElement = document.getElementById(`comment-${commentId}`);
-          }
+            while (!commentElement && hasNextPage && retryCount < MAX_RETRIES) {
+              retryCount++;
+              await fetchNextPage();
+              await new Promise((resolve) => setTimeout(resolve, 100));
+              commentElement = document.getElementById(`comment-${commentId}`);
+            }
 
-          if (commentElement) {
-            setTimeout(() => {
+            if (commentElement) {
               commentElement?.scrollIntoView({ behavior: 'smooth', block: 'center' });
-            }, 100);
 
-            setToastVisible({ message: `댓글을 찾았어요! 🥳` });
-          }
+              setToastVisible({ message: `댓글을 찾았어요! 🥳` });
+            }
 
-          if (!commentElement) {
-            setToastVisible({ message: `댓글을 찾을 수 없어요 🥲` });
-          }
+            if (!commentElement) {
+              setToastVisible({ message: `댓글을 찾을 수 없어요 🥲` });
+            }
+          }, 100);
         } catch (error) {
           console.error(error);
           return setToastVisible({
