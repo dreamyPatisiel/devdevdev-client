b47da36a37423f31fd102a55262f11f530dad88c
minyoung <alsduddl9977@gmail.com>
2025-01-20 16:46:42 +0900

    fix: 구글 애드센스 lazyOnload 추가

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -160,6 +160,7 @@ export default function Document() {
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
           crossOrigin='anonymous'
+          strategy='lazyOnload'
         />
         <meta
           name='google-adsense-account'
