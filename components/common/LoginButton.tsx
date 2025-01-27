762921b079814bc5fe49808da7a21dc9ee4f77f1
mandelina <mandelina24@gmail.com>
2025-01-27 22:23:37 +0900

    Fix : 로그인 실패처리 & checklogin함수 수정

--- components/common/LoginButton.tsx
+++ components/common/LoginButton.tsx
@@ -29,10 +29,14 @@ export default function LoginButton() {
   const END_PONIT = loginConfig.endPoint;
   const REDIRECT_URL = URL + END_PONIT;
 
+  let retryCount = 0;
+  const MAX_RETRY_COUNT = 300; //2초 x 300회 =  10분
+  let intervalId: NodeJS.Timeout | null = null;
+
   const handleOpenModal = () => {
     const newWindow = window.open(REDIRECT_URL, '_blank', 'width=400,height=550');
 
-    if (newWindow) {
+    if (newWindow && !intervalId) {
       // 새탭의 위치를 중앙으로 조정
       const screenWidth = window.screen.width;
       const screenHeight = window.screen.height;
@@ -40,40 +44,54 @@ export default function LoginButton() {
       const top = (screenHeight - 550) / 2;
       newWindow.moveTo(left, top);
 
-      const intervalId = setInterval(async () => {
+      intervalId = setInterval(async () => {
+        if (newWindow.closed) {
+          clearInterval(intervalId!);
+          intervalId = null;
+          return;
+        }
+
         const loginStatus = await checkLogin();
         console.log('loginStatus', loginStatus);
-        if (loginStatus) {
-          // checking일땐 계속 폴링상태 유지
+        // checking일땐 계속 폴링상태 유지
 
-          if (loginStatus === 'active') {
-            clearInterval(intervalId); // 폴링 중지
-            newWindow.close();
-            const accessToken = getCookie('DEVDEVDEV_ACCESS_TOKEN') as string;
-            const email = getCookie('DEVDEVDEV_MEMBER_EMAIL') as string;
-            const nickname = getCookie('DEVDEVDEV_MEMBER_NICKNAME') as string;
-            const isAdmin = getCookie('DEVDEVDEV_MEMBER_IS_ADMIN') as string;
-
-            const userInfo = {
-              accessToken: accessToken,
-              email: email,
-              nickname: decodeURIComponent(nickname).replace(/\+/g, ' '),
-              isAdmin: isAdmin === 'true',
-            };
-
-            // store에 저장하는 로직
-            setUserInfo(userInfo);
-            setLoginStatus();
-            // 기술블로그 추천 여부를 초기화 하기 위한 로직
-            queryClient.invalidateQueries({ queryKey: ['techDetail'] });
-
-            router.reload();
-          }
-        } else {
-          console.log('로그인 실패');
+        if (loginStatus === 'active') {
+          clearInterval(intervalId!);
+          intervalId = null;
           newWindow.close();
-          setToastVisible({ message: '로그인에 실패했어요. 다시 시도해주세요.', type: 'error' });
+          const accessToken = getCookie('DEVDEVDEV_ACCESS_TOKEN') as string;
+          const email = getCookie('DEVDEVDEV_MEMBER_EMAIL') as string;
+          const nickname = getCookie('DEVDEVDEV_MEMBER_NICKNAME') as string;
+          const isAdmin = getCookie('DEVDEVDEV_MEMBER_IS_ADMIN') as string;
+
+          const userInfo = {
+            accessToken: accessToken,
+            email: email,
+            nickname: decodeURIComponent(nickname).replace(/\+/g, ' '),
+            isAdmin: isAdmin === 'true',
+          };
+
+          // store에 저장하는 로직
+          setUserInfo(userInfo);
+          setLoginStatus();
+          // 기술블로그 추천 여부를 초기화 하기 위한 로직
+          queryClient.invalidateQueries({ queryKey: ['techDetail'] });
+
+          router.reload();
         }
+        // checking 상태일때 최대 시도 횟수 초과시 로그인 실패 토스트 띄우기
+        else {
+          if (retryCount >= MAX_RETRY_COUNT) {
+            console.error('로그인 실패');
+            clearInterval(intervalId!);
+            intervalId = null;
+            newWindow.close();
+            setToastVisible({ message: '로그인에 실패했어요. 다시 시도해주세요.', type: 'error' });
+            closeLoginModal();
+            return;
+          }
+        }
+        retryCount += 1;
         closeLoginModal();
       }, 2 * 1000);
     }
