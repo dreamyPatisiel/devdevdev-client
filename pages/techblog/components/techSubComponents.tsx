5ac111dc26b80c88258517eb96affbe07070bab5
mandelina <mandelina24@gmail.com>
2025-02-16 21:07:09 +0900

    Fix  : 기존 컴포넌트 리팩토링 & 스타일링 누락 추가(아이콘 있을때 스타일)

--- pages/techblog/components/techSubComponents.tsx
+++ pages/techblog/components/techSubComponents.tsx
@@ -7,18 +7,16 @@ import { useToastVisibleStore } from '@stores/toastVisibleStore';
 import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
 
 export const ImgWrapper = ({
-  width,
-  height,
+  className,
   children,
 }: {
-  width: string;
-  height: string;
+  className: string;
   children: React.ReactNode;
 }) => {
   return (
     <div
       className={cn(
-        `overflow-hidden bg-gray700 rounded-[2rem] flex justify-center items-center ${width} ${height}`,
+        `overflow-hidden bg-gray700 rounded-[2rem] flex justify-center items-center ${className}`,
       )}
     >
       {children}
@@ -44,7 +42,7 @@ export const TechCardWrapper = ({ children }: { children: React.ReactNode }) => 
 export const TechTitle = ({ title, width }: { title: string; width: string }) => {
   const { isMobile } = useMediaQueryContext();
   const baseStyle = 'flex flex-row items-center gap-8 font-bold st2 text-white border-white ';
-  const mobileStyle = 'max-w-[80vw] pt-[2.4rem] pb-[1.2rem]';
+  const mobileStyle = 'max-w-[TechContent] pt-[2.4rem] pb-[1.2rem]';
   const desktopStyle = 'py-[0.7rem]';
   return (
     <div className={`${baseStyle} ${isMobile ? mobileStyle : desktopStyle}`}>
@@ -66,7 +64,7 @@ export const TechContent = ({
   const baseStyle = `w-full p2 text-gray100 truncate-multiline`;
 
   return (
-    <div className={(cn(isMobile ? 'max-w-[100vw]' : 'max-w-[80vw]'), className)}>
+    <section className={(cn(isMobile ? 'max-w-[100vw]' : 'max-w-[80vw]'), className)}>
       <p
         className={baseStyle}
         style={{
@@ -75,7 +73,7 @@ export const TechContent = ({
       >
         {content}
       </p>
-    </div>
+    </section>
   );
 };
 
