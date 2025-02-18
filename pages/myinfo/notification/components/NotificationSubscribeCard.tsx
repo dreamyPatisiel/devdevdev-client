73750f9a4f782ad9b6a3f14570921b9e0e86a28c
minyoung <alsduddl9977@gmail.com>
2025-02-18 20:53:59 +0900

    feat: NotificationSubscribeCard 모바일 UI 구현

--- pages/myinfo/notification/components/NotificationSubscribeCard.tsx
+++ pages/myinfo/notification/components/NotificationSubscribeCard.tsx
@@ -8,8 +8,10 @@ import { TechContent, TechInfo } from '@pages/techblog/components/techSubCompone
 import TechBlogImg from '@components/features/techblog/techBlogImg';
 
 import { ROUTES } from '@/constants/routes';
+import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
 
 export default function NotificationSubscribeCard() {
+  const { isMobile } = useMediaQueryContext();
   const data = {
     id: 1,
     thumbnailUrl: 'https://img.animalplanet.co.kr/news/2020/02/10/700/1b2b4q83cbc25b47g551.jpg',
@@ -26,7 +28,7 @@ export default function NotificationSubscribeCard() {
   return (
     <div
       key={data.id}
-      className='flex gap-[1.6rem] flex-col rounded-Radius16 px-[3.2rem] py-[2.4rem] border border-gray500'
+      className={`flex gap-[1.6rem] flex-col rounded-Radius16 border border-gray500 py-[2.4rem] ${isMobile ? 'px-[1.6rem]' : 'px-[3.2rem]'}`}
     >
       <div className='rounded-[10rem] p-[0.4rem] bg-gray800 text-center'>
         <p className='p2'>
@@ -34,20 +36,26 @@ export default function NotificationSubscribeCard() {
         </p>
       </div>
 
-      <div className='flex gap-[3.2rem]'>
+      <div className={`flex ${isMobile ? 'flex-col gap-[2.4rem]' : 'gap-[3.2rem]'}`}>
         <div className='flex flex-col gap-[1.6rem]'>
-          <TechBlogImg id={data.id} thumbnailUrl={data.thumbnailUrl} size='small' />
-
-          <ArticleViewTextButton
-            techArticleUrl={data.techArticleUrl}
-            fontSize='p2'
-            textIconGap='mr-[0.45rem]'
-            paddingY='0'
-            iconSize='w-[7px] h-[12px]'
+          <TechBlogImg
+            id={data.id}
+            thumbnailUrl={data.thumbnailUrl}
+            size={isMobile ? 'mobile' : 'small'}
           />
+
+          {!isMobile && (
+            <ArticleViewTextButton
+              techArticleUrl={data.techArticleUrl}
+              fontSize='p2'
+              textIconGap='mr-[0.45rem]'
+              paddingY='0'
+              iconSize='w-[7px] h-[12px]'
+            />
+          )}
         </div>
 
-        <div className='flex flex-col gap-[0.8rem]'>
+        <div className={`flex flex-col ${isMobile ? 'gap-[0.4rem]' : 'gap-[0.8rem]'}`}>
           <div className='flex items-center justify-between border-white'>
             <Link href={`${ROUTES.TECH_BLOG}`}>
               <p className='font-bold st2'>{data.title}</p>
@@ -66,6 +74,16 @@ export default function NotificationSubscribeCard() {
             <TechContent content={data.contents} maxLines={2} />
           </Link>
         </div>
+
+        {isMobile && (
+          <ArticleViewTextButton
+            techArticleUrl={data.techArticleUrl}
+            fontSize='p2'
+            textIconGap='mr-[0.45rem]'
+            paddingY='0'
+            iconSize='w-[7px] h-[12px]'
+          />
+        )}
       </div>
     </div>
   );
