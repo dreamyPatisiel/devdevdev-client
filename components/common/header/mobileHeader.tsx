601c490010e6c0e52a8ad0c066c484c8f9187740
mandelina <mandelina24@gmail.com>
2025-02-24 21:55:27 +0900

    Feat : 모바일 헤더 스타일링 업데이트 & 알림 컴포넌트 추가

--- components/common/header/mobileHeader.tsx
+++ components/common/header/mobileHeader.tsx
@@ -13,12 +13,13 @@ import { useUserInfoStore } from '@stores/userInfoStore';
 import useHandleLinkClick from '@hooks/useHandleNavClick';
 
 import DevLogo from '@public/image/devdevdevLogo.svg';
-import logoutIcon from '@public/image/right-from-bracket.svg';
-import loginIcon from '@public/image/right-to-bracket.svg';
 
 import { MENU_LISTS } from '@/constants/NavListConstants';
 import { NO_USER_NAME } from '@/constants/UserInfoConstants';
 import { ROUTES } from '@/constants/routes';
+
+import AlertBellNav from '../alertBell/AlertBellNav';
+import { MainButtonV2 } from '../buttons/mainButtonsV2';
 
 export default function MobileHeader() {
   const router = useRouter();
@@ -41,28 +42,6 @@ export default function MobileHeader() {
     }
   }, [userInfo, setLoginStatus, setLogoutStatus]);
 
-  const loginStatusButton = (loginStatus: 'login' | 'logout' | 'loading' | 'account-delete') => {
-    const statusName =
-      loginStatus === 'login' ? (
-        <span className='text-secondary400'>{userInfo.nickname || NO_USER_NAME}님</span>
-      ) : (
-        <span>로그인</span>
-      );
-
-    const icon = loginStatus === 'login' ? logoutIcon : loginIcon;
-
-    return (
-      <button
-        onClick={openLoginModal}
-        type='button'
-        className='p1 text-gray100 font-bold flex items-center gap-[1rem]'
-      >
-        {statusName}
-        <Image src={icon} alt={`${statusName}아이콘`} width={16} height={13} />
-      </button>
-    );
-  };
-
   return (
     <header className='h-[9rem]'>
       <div className='flex flex-col bg-gray600 border-b border-b-gray200 fixed w-full z-40'>
@@ -70,9 +49,26 @@ export default function MobileHeader() {
           <Link href={MAIN} aria-label='메인'>
             <Image src={DevLogo} alt='DEVDEVDEV 로고' width={64} height={23} />
           </Link>
-          {/* <Image src={HeaderBar} alt='바 로고' /> */}
-
-          {loginStatusButton(loginStatus)}
+          <div className='flex gap-[1.6rem]'>
+            {loginStatus === 'login' && (
+              <div className='flex items-center gap-3 p1'>
+                <p className='text-secondary400'>
+                  {userInfo.nickname || NO_USER_NAME}
+                  <span className='text-white'>님</span>
+                </p>
+                <AlertBellNav />
+              </div>
+            )}
+            <MainButtonV2
+              onClick={openLoginModal}
+              text={loginStatus === 'login' ? '로그아웃' : '로그인'}
+              type='button'
+              color='primary'
+              line={false}
+              radius='rounded'
+              size='xSmall'
+            />
+          </div>
         </div>
 
         <nav className='px-[1.6rem] py-[0.9rem] p2 font-bold'>
