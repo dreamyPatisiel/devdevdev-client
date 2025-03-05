9669133d6a6a83107cca1506bd8cf7224a1e97e8
mandelina <mandelina24@gmail.com>
2025-03-05 23:53:32 +0900

    Feat : 알림 리스트 컴포넌트 구현 (1차)

--- components/common/alertBell/AlertBellNav.tsx
+++ components/common/alertBell/AlertBellNav.tsx
@@ -2,6 +2,10 @@ import { cn } from '@utils/mergeStyle';
 
 import AlertBellIcon from '@public/assets/AlertBellIcon';
 
+import AlertList from './AlertList';
+
+// import { useSSE } from '../../hooks/useSSE';
+
 export default function AlertBellNav({
   alertCount,
   onClick,
@@ -11,12 +15,27 @@ export default function AlertBellNav({
   onClick?: () => void;
   className?: string;
 }) {
+  // const notifications = useSSE('/api/notifications');
+
+  const notifications = [
+    { id: 1, companyName: '토스', message: '에서 새로운 글이 올라왔어요!', time: 5 },
+    { id: 2, companyName: '쿠팡', message: '에서 새로운 글이 올라왔어요!', time: 10 },
+    { id: 3, companyName: 'AWS', message: '에서 새로운 글이 올라왔어요!', time: 15 },
+    { id: 4, companyName: '우아한형제들', message: '에서 새로운 글이 올라왔어요!', time: 20 },
+    { id: 5, companyName: '토스', message: '에서 새로운 글이 올라왔어요!', time: 25 },
+  ];
+
   return (
-    <div className={cn(`flex flex-row items-center gap-[0.3rem] ${className}`)} onClick={onClick}>
-      <AlertBellIcon color='gray200' />
-      <span className='h-[1.6rem] ml-[0.3rem] py-[0.1rem] px-[0.49rem] rounded-RadiusRounded bg-primary500 c2'>
-        {alertCount || 0}
-      </span>
+    <div className='relative'>
+      <div className={cn(`flex flex-row items-center gap-[0.3rem] ${className}`)} onClick={onClick}>
+        <AlertBellIcon color='gray200' />
+        <span className='h-[1.6rem] ml-[0.3rem] py-[0.1rem] px-[0.49rem] rounded-RadiusRounded bg-primary500 c2'>
+          {alertCount || 0}
+        </span>
+      </div>
+      <div className='absolute top-full mt-[2rem] right-0'>
+        <AlertList notifications={notifications} />
+      </div>
     </div>
   );
 }
---
+++ components/common/alertBell/AlertList.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import AngleRightIcon from '@public/assets/AngleRightIcon';
+
+interface Notification {
+  id: number;
+  message: string;
+  companyName: string;
+  time: number;
+}
+
+interface NotificationListProps {
+  notifications: Notification[];
+}
+
+export default function AlertList({ notifications }: NotificationListProps) {
+  return (
+    <div className='text-white rounded-Radius16 border border-gray500'>
+      <div className='bg-gray600 flex justify-between items-center px-[1.2rem] pt-[1.6rem] pb-[0.8rem]'>
+        <p className='c1 text-gray200'>
+          알림 <span className='text-secondary300'>{notifications.length}</span>
+        </p>
+        <button className='c1 text-secondary300'>모두 읽음</button>
+      </div>
+      {notifications.length > 0 ? (
+        notifications.map((notification) => (
+          // ArrowLeftgreen 아이콘 컬러값 변경해서 사용해야함
+          <div
+            key={notification.id}
+            className='bg-gray800 flex justify-between items-center gap-[1rem] px-[1.2rem] py-[0.8rem] border-b border-gray500 last:border-b-0'
+          >
+            <p className='text-gray100 truncate'>
+              <span className='text-secondary300'>{notification.companyName}</span>
+              {notification.message}
+            </p>
+            <span className='c2 text-gray300'>{notification.time}분전</span>
+          </div>
+        ))
+      ) : (
+        <p className='py-[2.4rem] text-center text-gray700'>확인할 알림이 없어요</p>
+      )}
+      <button className='w-full flex flex-row justify-center items-center gap-[1rem] bg-gray600 px-[1.2rem] pt-[0.8rem] pb-[1.6rem] rounded-b-Radius16'>
+        <p className=' text-gray200 hover:text-gray-200'>알림 전체보기</p>
+        <AngleRightIcon color={`var(--gray200)`} />
+      </button>
+    </div>
+  );
+}
