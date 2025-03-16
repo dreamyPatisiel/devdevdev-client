6f2a165dca514870ffe509beed2541da9edcb332
minyoung <alsduddl9977@gmail.com>
2025-03-16 18:38:26 +0900

    feat: 모달 구현 & 픽픽픽 글 등록 모달 반영

--- pages/pickposting/constants/pickPostConstants.ts
+++ pages/pickposting/constants/pickPostConstants.ts
@@ -1,3 +1,4 @@
 export const MAX_IMAGE_COUNT = 3;
 export const PICK_SUCCESS_MESSAGE = '픽픽픽 작성을 완료했어요!';
 export const PICK_UPDATE_MESSAGE = '투표를 성공적으로 수정했어요!';
+export const PICK_ERROR_MESSAGE = '픽픽픽 등록에 실패했어요. 다시 시도해주세요.';
--- stores/modalStore.ts
+++ stores/modalStore.ts
@@ -1,5 +1,39 @@
 import { create } from 'zustand';
 
+import { ReactElement } from 'react';
+
+interface ModalInfo {
+  id: string;
+  title: string;
+  contents: ReactElement | string;
+  submitText: string;
+  cancelText: string;
+  submitFunction: () => void;
+  cancelFunction: () => void;
+  size?: 's' | 'm' | 'l';
+}
+interface ModalInfos {
+  modalInfos: ModalInfo[];
+  isPending?: boolean;
+  setIsPending?: (isPending: boolean) => void;
+  //TODO: 추후에 disabled 추가
+}
+
+interface ModalActions {
+  pushModal: (modalInfo: ModalInfo) => void;
+  popModal: () => void;
+  clearModals: () => void;
+}
+
+export const useModalStore = create<ModalInfos & ModalActions>((set) => ({
+  modalInfos: [],
+  isPending: undefined,
+  setIsPending: (isPending) => set({ isPending }),
+  pushModal: (modalInfo) => set((state) => ({ modalInfos: [...state.modalInfos, modalInfo] })),
+  popModal: () => set((state) => ({ modalInfos: state.modalInfos.slice(0, -1) })), // FIXME: 추후에 id값 비교해서 같은 것만 빼기
+  clearModals: () => set({ modalInfos: [] }),
+}));
+
 // login모달 store
 export interface LoginModalStore {
   isLoginModalOpen: boolean;
@@ -17,36 +51,36 @@ export const useLoginModalStore = create<LoginModalStore>((set) => ({
 }));
 
 // 모달 store
-export interface ModalStore {
-  isModalOpen: boolean;
-  openModal: () => void;
-  closeModal: () => void;
-  modalType: string;
-  setModalType: (type: string) => void;
-  title: string;
-  contents: string;
-  setTitle: (title: string) => void;
-  setContents: (text: string) => void;
-  modalSubmitFn: () => void;
-  setModalSubmitFn: (submitFn: () => void) => void;
-  isPending: boolean;
-  setIsPending: () => void;
-  setIsNotPending: () => void;
-}
+// export interface ModalStore {
+//   isModalOpen: boolean;
+//   openModal: () => void;
+//   closeModal: () => void;
+//   modalType: string;
+//   setModalType: (type: string) => void;
+//   title: string;
+//   contents: string;
+//   setTitle: (title: string) => void;
+//   setContents: (text: string) => void;
+//   modalSubmitFn: () => void;
+//   setModalSubmitFn: (submitFn: () => void) => void;
+//   isPending: boolean;
+//   setIsPending: () => void;
+//   setIsNotPending: () => void;
+// }
 
-export const useModalStore = create<ModalStore>((set) => ({
-  isModalOpen: false,
-  openModal: () => set({ isModalOpen: true }),
-  closeModal: () => set({ isModalOpen: false, modalType: '' }),
-  modalType: '',
-  setModalType: (type) => set({ modalType: type }),
-  title: '',
-  contents: '',
-  setTitle: (text) => set({ title: text }),
-  setContents: (text) => set({ contents: text }),
-  modalSubmitFn: () => {},
-  setModalSubmitFn: (submitFn) => set({ modalSubmitFn: submitFn }),
-  isPending: false,
-  setIsPending: () => set({ isPending: true }),
-  setIsNotPending: () => set({ isPending: false }),
-}));
+// export const useModalStore = create<ModalStore>((set) => ({
+//   isModalOpen: false,
+//   openModal: () => set({ isModalOpen: true }),
+//   closeModal: () => set({ isModalOpen: false, modalType: '' }),
+//   modalType: '',
+//   setModalType: (type) => set({ modalType: type }),
+//   title: '',
+//   contents: '',
+//   setTitle: (text) => set({ title: text }),
+//   setContents: (text) => set({ contents: text }),
+//   modalSubmitFn: () => {},
+//   setModalSubmitFn: (submitFn) => set({ modalSubmitFn: submitFn }),
+//   isPending: false,
+//   setIsPending: () => set({ isPending: true }),
+//   setIsNotPending: () => set({ isPending: false }),
+// }));
