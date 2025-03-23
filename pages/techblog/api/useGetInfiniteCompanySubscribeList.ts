ec3a859dc9d5b9078ebe89bc5a012945d61aead7
mandelina <mandelina24@gmail.com>
2025-03-23 17:19:56 +0900

    Feat  : 구독가능한 기업조회 get hook생성

---
+++ pages/techblog/api/useGetInfiniteCompanySubscribeList.ts
@@ -0,0 +1,84 @@
+import axios from 'axios';
+
+import { useCallback } from 'react';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { COMPANY_SUBSCRIBE_VIEW_SIZE } from '../constants/techBlogConstants';
+
+// 필요한 상수 및 타입을 정의합니다.
+
+interface GetCompanySubscribeProps {
+  companyId?: string;
+  size?: number;
+}
+
+export const getCompanySubscribeList = async ({ companyId, size }: GetCompanySubscribeProps) => {
+  const queryParams = {
+    size: size ? size : COMPANY_SUBSCRIBE_VIEW_SIZE,
+    ...(companyId && { companyId }),
+  };
+
+  const res = await axios.get('/devdevdev/api/v1/subscriptions/companies?', {
+    params: {
+      ...queryParams,
+    },
+  });
+  return res.data;
+};
+
+export const useInfiniteCompanySubscribeList = (companyId?: string, size?: number) => {
+  const {
+    data: companySubscribeList,
+    fetchNextPage, // 다음 페이지의 데이터를 가져옴
+    isFetchingNextPage, // 현재 다음 페이지를 가져오는지 여부
+    hasNextPage, // 다음 페이지가 있는지
+    status, // 쿼리의 상태
+    error,
+    isFetching, // 데이터를 가지고 오는지 여부
+  } = useInfiniteQuery({
+    queryKey: ['companySubscribeList', companyId, size],
+    // 데이터를 요청하는데 사용하는 함수
+    queryFn: ({ pageParam }) => {
+      let companyId = '';
+
+      if (pageParam) {
+        const parsedParam = JSON.parse(pageParam);
+        companyId = parsedParam.companyId;
+      }
+
+      return getCompanySubscribeList({
+        companyId: companyId,
+        size: size,
+      });
+    },
+    initialPageParam: '',
+    getNextPageParam: (lastPage) => {
+      if (lastPage?.data.last) {
+        return undefined;
+      }
+      const companyId = lastPage.data.content[COMPANY_SUBSCRIBE_VIEW_SIZE - 1]?.companyId;
+      return JSON.stringify({ companyId: companyId });
+    },
+  });
+
+  const onIntersect = useCallback(
+    ([entry]: IntersectionObserverEntry[]) => {
+      if (!isFetching && entry.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage, isFetching],
+  );
+
+  return {
+    companySubscribeList,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    status,
+    error,
+    isFetching,
+    onIntersect,
+  };
+};
