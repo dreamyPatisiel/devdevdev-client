1fec08d5a25ad875002bb2e1433750656a615c01
minyoung <alsduddl9977@gmail.com>
2025-04-12 17:02:07 +0900

    fix: popModal 시  disabled: false, showDropdown: false 처리

--- stores/modalStore.ts
+++ stores/modalStore.ts
@@ -39,13 +39,10 @@ export const useModalStore = create<ModalInfos & ModalActions>((set) => ({
       const { refreshSelectedBlameData } = useSelectedStore.getState();
       const { refreshBlameReason } = useBlameReasonStore.getState();
 
-      if (refreshSelectedBlameData) {
-        refreshSelectedBlameData();
-      }
+      refreshSelectedBlameData();
+      refreshBlameReason();
 
-      if (refreshBlameReason) {
-        refreshBlameReason();
-      }
+      set({ disabled: false, showDropdown: false });
 
       return { modalInfos: state.modalInfos.slice(0, -1) };
     }), // FIXME: 추후에 id값 비교해서 같은 것만 빼기
@@ -74,38 +71,3 @@ export const useLoginModalStore = create<LoginModalStore>((set) => ({
   description: '',
   setDescription: (description) => set({ description: description }),
 }));
-
-// 모달 store
-// export interface ModalStore {
-//   isModalOpen: boolean;
-//   openModal: () => void;
-//   closeModal: () => void;
-//   modalType: string;
-//   setModalType: (type: string) => void;
-//   title: string;
-//   contents: string;
-//   setTitle: (title: string) => void;
-//   setContents: (text: string) => void;
-//   modalSubmitFn: () => void;
-//   setModalSubmitFn: (submitFn: () => void) => void;
-//   isPending: boolean;
-//   setIsPending: () => void;
-//   setIsNotPending: () => void;
-// }
-
-// export const useModalStore = create<ModalStore>((set) => ({
-//   isModalOpen: false,
-//   openModal: () => set({ isModalOpen: true }),
-//   closeModal: () => set({ isModalOpen: false, modalType: '' }),
-//   modalType: '',
-//   setModalType: (type) => set({ modalType: type }),
-//   title: '',
-//   contents: '',
-//   setTitle: (text) => set({ title: text }),
-//   setContents: (text) => set({ contents: text }),
-//   modalSubmitFn: () => {},
-//   setModalSubmitFn: (submitFn) => set({ modalSubmitFn: submitFn }),
-//   isPending: false,
-//   setIsPending: () => set({ isPending: true }),
-//   setIsNotPending: () => set({ isPending: false }),
-// }));
