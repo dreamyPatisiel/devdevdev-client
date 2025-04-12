b4aa056a7ad9e1fe24e599540ded26f18000b4b5
minyoung <alsduddl9977@gmail.com>
2025-04-12 17:29:53 +0900

    refactor: PICK_VOTE_MODAL -> PICK_VOTE_REGISTER_MODAL 로 네이밍 수정

--- components/common/modals/modalConfig/pickVote.ts
+++ components/common/modals/modalConfig/pickVote.ts
@@ -1,5 +1,4 @@
-export const PICK_VOTE_MODAL = {
-  // FIXME: PICK_VOTE_MODAL을 PICK_VOTE_REGISTER_MODAL로 변경
+export const PICK_VOTE_REGISTER_MODAL로 = {
   id: crypto.randomUUID(),
   title: '투표를 등록할까요?',
   contents: '타인을 비방하거나 광고가 포함된 게시물은 관리자에 의해 삭제될 수 있어요.',
