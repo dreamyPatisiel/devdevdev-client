6cf3c6973da46f7ff00d72f0d711b3fd71acf455
minyoung <alsduddl9977@gmail.com>
2025-04-16 22:22:16 +0900

    feat: 구독한 기업 리스트 API 연동

--- pages/myinfo/notifications/subscribe/index.page.tsx
+++ pages/myinfo/notifications/subscribe/index.page.tsx
@@ -1,61 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import MyInfo from '@pages/myinfo/index.page';
+import { MySubscription } from '@pages/myinfo/types/responseData';
+
+import { useObserver } from '@hooks/useObserver';
 
 import logoImage from '@public/image/devdevdevLogo.svg';
 
 import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
 
 import NotificationNav from '../components/NotificationNav';
+import { useGetMySubscriptions } from './apiHooks/useGetMySubscriptions';
 import SubscribeCard from './components/SubscribeCard';
 
 export default function Subscribe() {
-  const { isMobile } = useMediaQueryContext();
+  const bottom = useRef(null);
 
-  const subscribeData = [
-    {
-      id: 1,
-      logoImage: logoImage,
-      company: '댑구리',
-      isSubscribe: true,
-    },
-    {
-      id: 2,
-      logoImage: logoImage,
-      company: '댑구리',
-      isSubscribe: true,
-    },
-    {
-      id: 3,
-      logoImage: logoImage,
-      company: '댑구리',
-      isSubscribe: true,
-    },
-    {
-      id: 4,
-      logoImage: logoImage,
-      company: '댑구리',
-      isSubscribe: false,
-    },
-    {
-      id: 5,
-      logoImage: logoImage,
-      company: '댑구리',
-      isSubscribe: true,
-    },
-    {
-      id: 6,
-      logoImage: logoImage,
-      company: '댑구리',
-      isSubscribe: false,
-    },
-    {
-      id: 7,
-      logoImage: logoImage,
-      company: '댑구리',
-      isSubscribe: true,
-    },
-  ];
+  const { isMobile } = useMediaQueryContext();
+  const { mySubscriptionsData, isFetchingNextPage, hasNextPage, status, onIntersect } =
+    useGetMySubscriptions();
+
+  useObserver({
+    target: bottom,
+    onIntersect,
+  });
+
+  if (status === 'pending') {
+    return (
+      <MyInfo>
+        <NotificationNav />
+        <section
+          className={`flex flex-wrap gap-x-[1.6rem] gap-y-[2.4rem] ${isMobile ? 'mb-[8rem]' : ''}`}
+        >
+          {Array.from({ length: 8 }, (_, index) => (
+            <SubscribeCard
+              key={index}
+              logoImage={logoImage}
+              company='로딩 중...'
+              isSubscribe={false}
+            />
+          ))}
+          {/* TODO: 스켈레톤으로 수정 */}
+        </section>
+      </MyInfo>
+    );
+  }
 
   return (
     <MyInfo>
@@ -63,14 +52,31 @@ export default function Subscribe() {
       <section
         className={`flex flex-wrap gap-x-[1.6rem] gap-y-[2.4rem] ${isMobile ? 'mb-[8rem]' : ''}`}
       >
-        {subscribeData.map((subscribeItem) => (
-          <SubscribeCard
-            key={subscribeItem.id}
-            logoImage={subscribeItem.logoImage}
-            company={subscribeItem.company}
-            isSubscribe={subscribeItem.isSubscribe}
-          />
+        {mySubscriptionsData?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page?.data?.content.map((subscribeItem: MySubscription) => (
+              <SubscribeCard
+                key={subscribeItem.companyId}
+                logoImage={subscribeItem.companyImageUrl}
+                company={subscribeItem.companyName}
+                isSubscribe={subscribeItem.isSubscribed}
+              />
+            ))}
+          </React.Fragment>
         ))}
+
+        {isFetchingNextPage && hasNextPage && (
+          <div className='mt-[2rem]'>
+            {isMobile ? (
+              <SubscribeCard logoImage={logoImage} company='로딩 중...' isSubscribe={false} />
+            ) : (
+              <SubscribeCard logoImage={logoImage} company='로딩 중...' isSubscribe={false} />
+            )}
+            {/* TODO: 스켈레톤으로 수정 */}
+          </div>
+        )}
+
+        <div ref={bottom} />
       </section>
     </MyInfo>
   );
