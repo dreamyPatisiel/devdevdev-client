e9bb89d0cd0f5253755210d55952b650b17eb480
minyoung <alsduddl9977@gmail.com>
2025-04-16 23:01:32 +0900

    feat: 구독한 기업 - 아티클 보기 기능 추가

--- pages/myinfo/notifications/subscribe/components/SubscribeCard.tsx
+++ pages/myinfo/notifications/subscribe/components/SubscribeCard.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
+import { useRouter } from 'next/router';
+
+import { useCompanyIdStore } from '@stores/techBlogStore';
+
 import { MainButtonV2 } from '@components/common/buttons/mainButtonsV2';
 
+import { ROUTES } from '@/constants/routes';
 import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
 
 export default function SubscribeCard({
   logoImage,
   company,
   isSubscribe,
+  id,
 }: {
   logoImage: string;
   company: string;
   isSubscribe: boolean;
+  id: number;
 }) {
   const { isMobile } = useMediaQueryContext();
+
+  const { setCompanyId } = useCompanyIdStore();
+
+  const router = useRouter();
 
   return (
     <div
@@ -28,7 +39,17 @@ export default function SubscribeCard({
       <div className='rounded-b-Radius16 border border-gray600 border-t-0 p-[1.6rem] flex flex-col gap-[1.6rem] justify-center text-center'>
         <strong className='st2 font-bold'>{company}</strong>
         <div className='flex flex-col gap-[0.8rem]'>
-          <MainButtonV2 text='아티클 보기' color='gray' size='small' radius='square' line={false} />
+          <MainButtonV2
+            text='아티클 보기'
+            color='gray'
+            size='small'
+            radius='square'
+            line={false}
+            onClick={async () => {
+              await setCompanyId(id);
+              await router.push(ROUTES.TECH_BLOG);
+            }}
+          />
           <MainButtonV2
             text={isSubscribe ? '구독 중' : '구독하기'}
             color='primary'
