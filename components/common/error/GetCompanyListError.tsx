2ed9caace2b7a991ae374db057dfac512b5f848e
mandelina <mandelina24@gmail.com>
2025-04-20 14:45:08 +0900

    Fix : 기술블로그 기업 섹션 에러컴포넌트 폰트 스타일링 수정

--- components/common/error/GetCompanyListError.tsx
+++ components/common/error/GetCompanyListError.tsx
@@ -26,11 +26,10 @@ export default function GetCompanyListError({
       className={`grid ${isMobile ? 'gap-[2.4rem]' : 'grid-flow-col justify-between items-center'} bg-gray800 p-[2.4rem] rounded-Radius16`}
     >
       <div>
-        <p className='st2 mb-[1rem]'>잠시 후 다시 시도해주세요</p>
-        <p className='p2 text-gray200'>기업 목록을 불러오는데 문제가 발생했어요!</p>
+        <p className='st2 font-bold mb-[1rem]'>{DEVGURI_ERR_TEXT.NETWORK_ERR.MAIN_TITLE}</p>
+        <p className='p2 font-medium text-gray200'>기업 목록을 불러오는데 문제가 발생했어요!</p>
       </div>
       <MainButtonV2
-        className=''
         color='primary'
         line={false}
         radius='square'
