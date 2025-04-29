0edf913a82720db67b4adc0356f233d71756fb86
MinjuMoon <mmj9908@greeneffect.co.kr>
2025-04-29 13:36:04 +0900

    Feat : 알림 Link달기 , 단건읽기 api연동 및 알림 툴팁 max-w 설정

--- components/common/alertBell/AlertList.tsx
+++ components/common/alertBell/AlertList.tsx
@@ -1,46 +1,66 @@
 import React from 'react';
 
+import Link from 'next/link';
+
+import { usePatchAlertRead } from '@pages/main/api/usePatchAlertRead';
+import { AlertType } from '@pages/main/types/AlertType';
+
 import { useAlertStore } from '@stores/AlertStore';
+import { useFullPopupVisibleStore } from '@stores/mobile/fullPopupStore';
 
 import ArrowRight9x20 from '@components/svgs/arrowRight9x20';
 
+import { ROUTES } from '@/constants/routes';
 import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
-import { AlertType } from '@pages/main/types/AlertType';
-
-
 
 export default function AlertList({ alert }: { alert: AlertType }) {
   const { id, type, title, createdAt, isRead, companyName, techArticleId } = alert;
   const { isBellDisabled } = useAlertStore();
   const { isMobile } = useMediaQueryContext();
+  const { closeFullPopup } = useFullPopupVisibleStore();
+
+  const { mutate: patchAlertRead } = usePatchAlertRead();
+
+  const isDisabled = isBellDisabled || isRead;
+
+  const handleAlertListClick = () => {
+    if (isMobile) {
+      closeFullPopup();
+    }
+    if (type === 'SUBSCRIPTION') {
+      patchAlertRead(id);
+    }
+  };
 
   return (
-    <li
-      key={id}
-      className={`p2 ${isMobile ? 'px-[2.4rem] py-[1.6rem]' : 'px-[1.2rem] py-[0.8rem]'} bg-gray800 border-b border-gray500 last:border-b-0 cursor-pointer`}
-    >
-      <div className='flex justify-between items-center gap-[1rem]'>
-        <p
-          className={`max-w-[32rem] text-gray100 truncate font-medium ${isBellDisabled ? 'opacity-50' : ''}`}
-        >
-          <span className='text-secondary300 font-bold'>{companyName}</span>
-          {/* TODO: 댓글 문구는 확정후 수정 필요 (임시) */}
-          {type === 'SUBSCRIPTION' ? '에서 새로운 글이 올라왔어요!' : '에서 댓글이 달렸어요!'}
-        </p>
-        <div className='flex flex-row items-center gap-[1rem]  whitespace-nowrap'>
-          <span className={`max-w-[5.5rem] c2 text-gray300 ${isBellDisabled ? 'opacity-50' : ''}`}>
-            {/* TODO: 서버 필드값 수정후 수정 필요 */}
-            {/* {createdAt} */}
-            23시간 전
-          </span>
-          <ArrowRight9x20 />
-        </div>
-      </div>
-      <p
-        className={`text-gray300 whitespace-nowrap overflow-hidden text-ellipsis ${isBellDisabled ? 'opacity-50' : ''}`}
+    <Link href={`${ROUTES.TECH_BLOG}/${techArticleId}`} onClick={handleAlertListClick}>
+      <li
+        key={id}
+        className={`p2 ${isMobile ? 'px-[2.4rem] py-[1.6rem]' : 'px-[1.2rem] py-[0.8rem]'} bg-gray800 border-b border-gray500 last:border-b-0 cursor-pointer`}
       >
-        {title}
-      </p>
-    </li>
+        <div className='flex justify-between items-center gap-[1rem]'>
+          <p
+            className={`max-w-[32rem] text-gray100 truncate font-medium ${isDisabled ? 'opacity-50' : ''}`}
+          >
+            <span className='text-secondary300 font-bold'>{companyName}</span>
+            {/* TODO: 댓글 문구는 확정후 수정 필요 (임시) */}
+            {type === 'SUBSCRIPTION' ? '에서 새로운 글이 올라왔어요!' : '에서 댓글이 달렸어요!'}
+          </p>
+          <div className='flex flex-row items-center gap-[1rem]  whitespace-nowrap'>
+            <span className={`max-w-[5.5rem] c2 text-gray300 ${isDisabled ? 'opacity-50' : ''}`}>
+              {/* TODO: 서버 필드값 수정후 수정 필요 */}
+              {/* {createdAt} */}
+              23시간 전
+            </span>
+            <ArrowRight9x20 />
+          </div>
+        </div>
+        <p
+          className={`text-gray300 whitespace-nowrap overflow-hidden text-ellipsis ${isDisabled ? 'opacity-50' : ''}`}
+        >
+          {title}
+        </p>
+      </li>
+    </Link>
   );
 }
--- pages/main/api/usePatchAlertReadAll.ts
+++ pages/main/api/usePatchAlertReadAll.ts
@@ -3,31 +3,29 @@ import axios from 'axios';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { useToastVisibleStore } from '@stores/toastVisibleStore';
-
 import { ALERT_READALL } from '@/constants/apiConstants';
 import { ErrorRespone } from '@/types/errorResponse';
 import { SuccessResponse } from '@/types/successResponse';
+
+import * as Sentry from '@sentry/nextjs';
 
 export const patchAlertAllRead = async () => {
   const res = await axios.patch<SuccessResponse<boolean>>(ALERT_READALL, null);
   return res.data;
 };
 
-// TODO: 연동후 수정필요
 export const usePatchAlertReadAll = () => {
   const queryClient = useQueryClient();
-  const { setToastVisible } = useToastVisibleStore();
 
   return useMutation({
     mutationFn: patchAlertAllRead,
     onSuccess: async () => {
-      //   await queryClient.invalidateQueries({ queryKey: ['notifications'] });
+      await queryClient.invalidateQueries({ queryKey: ['getAlertLists'] });
     },
     onError: (error: ErrorRespone) => {
-      // 처리를 해야하나 ? 읽기는 내부 데이터를 위한 용도인데..
-      //   const errorMessage = error.response?.data.message || '알림 읽기 처리 중 오류가 발생했습니다.';
-      //   setToastVisible({ message: errorMessage, type: 'error' });
+      if (process.env.NODE_ENV === 'production') {
+        Sentry.captureException(error);
+      }
     },
   });
 };
