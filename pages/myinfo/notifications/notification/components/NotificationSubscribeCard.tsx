79cc134545d337d32e713fd211f2737576a7d05a
minyoung.kim <minyoung.kim@wonderwall.kr>
2025-05-04 17:30:45 +0900

    feat: 단건 읽기 API 연동

--- pages/myinfo/notifications/notification/components/NotificationSubscribeCard.tsx
+++ pages/myinfo/notifications/notification/components/NotificationSubscribeCard.tsx
@@ -9,6 +9,7 @@ import TechBlogImg from '@components/features/techblog/techBlogImg';
 import { ROUTES } from '@/constants/routes';
 import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
 
+import { usePatchNotificationsRead } from '../apiHooks/usePatchNotificationsRead';
 import { MyInfoArticleViewTextButton } from './ArticleViewTextButton';
 
 export interface NotificationSubscribeCardProps {
@@ -43,54 +44,56 @@ export interface NotificationSubscribeCardProps {
 
 export default function NotificationSubscribeCard(props: NotificationSubscribeCardProps) {
   const { isMobile } = useMediaQueryContext();
+  const { mutate: patchNotificationsReadMutate } = usePatchNotificationsRead();
 
-  const { isRead, techArticle } = props;
+  const { notificationId, isRead, techArticle } = props;
   const { id, thumbnailUrl, techArticleUrl, title, contents, company, regDate, author } =
     techArticle;
 
   return (
-    <div
-      key={id}
-      className={`flex gap-[1.6rem] flex-col rounded-Radius16 border border-gray500 py-[2.4rem] 
+    <Link href={`${ROUTES.TECH_BLOG}/${id}`}>
+      <div
+        key={id}
+        className={`flex gap-[1.6rem] flex-col rounded-Radius16 border border-gray500 py-[2.4rem] 
         ${isMobile ? 'px-[1.6rem]' : 'px-[3.2rem]'}
         ${isRead ? 'opacity-50' : ''}
       `}
-    >
-      <div className='rounded-[10rem] p-[0.4rem] bg-gray800 text-center'>
-        <p className='p2'>
-          <strong className='font-bold'>{company.name}</strong>의 새 아티클이에요!
-        </p>
-      </div>
-
-      <div className={`flex ${isMobile ? 'flex-col gap-[2.4rem]' : 'gap-[3.2rem]'}`}>
-        <div className='flex flex-col gap-[1.6rem] items-center'>
-          <TechBlogImg id={id} thumbnailUrl={thumbnailUrl} size={isMobile ? 'mobile' : 'small'} />
-
-          {!isMobile && <MyInfoArticleViewTextButton techArticleUrl={techArticleUrl} />}
+        onClick={() => {
+          patchNotificationsReadMutate(notificationId);
+        }}
+      >
+        <div className='rounded-[10rem] p-[0.4rem] bg-gray800 text-center'>
+          <p className='p2'>
+            <strong className='font-bold'>{company.name}</strong>의 새 아티클이에요!
+          </p>
         </div>
 
-        <div className={`flex flex-col ${isMobile ? 'gap-[0.4rem]' : 'gap-[0.8rem]'}`}>
-          <div className='flex items-center justify-between border-white'>
-            <Link href={`${ROUTES.TECH_BLOG}`}>
-              <p className='font-bold st2'>{title}</p>
-            </Link>
+        <div className={`flex ${isMobile ? 'flex-col gap-[2.4rem]' : 'gap-[3.2rem]'}`}>
+          <div className='flex flex-col gap-[1.6rem] items-center'>
+            <TechBlogImg id={id} thumbnailUrl={thumbnailUrl} size={isMobile ? 'mobile' : 'small'} />
+
+            {!isMobile && <MyInfoArticleViewTextButton techArticleUrl={techArticleUrl} />}
           </div>
 
-          <TechInfo
-            type='main'
-            author={author}
-            date={regDate}
-            company={company.name}
-            companyId={company.id}
-          />
+          <div className={`flex flex-col ${isMobile ? 'gap-[0.4rem]' : 'gap-[0.8rem]'}`}>
+            <div className='flex items-center justify-between border-white'>
+              <p className='font-bold st2'>{title}</p>
+            </div>
 
-          <Link href={`${ROUTES.TECH_BLOG}`}>
+            <TechInfo
+              type='main'
+              author={author}
+              date={regDate}
+              company={company.name}
+              companyId={company.id}
+            />
+
             <TechContent content={contents} maxLines={isMobile ? 3 : 2} />
-          </Link>
-        </div>
+          </div>
 
-        {isMobile && <MyInfoArticleViewTextButton techArticleUrl={techArticleUrl} />}
+          {isMobile && <MyInfoArticleViewTextButton techArticleUrl={techArticleUrl} />}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
