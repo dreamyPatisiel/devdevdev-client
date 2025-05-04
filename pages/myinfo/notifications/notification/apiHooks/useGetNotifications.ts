d87b3a57160632ee46c868e22bda6d943b885712
minyoung.kim <minyoung.kim@wonderwall.kr>
2025-05-04 17:11:09 +0900

    feat: getNotificationsPage API 연동

---
+++ pages/myinfo/notifications/notification/apiHooks/useGetNotifications.ts
@@ -0,0 +1,82 @@
+import axios from 'axios';
+
+import { useCallback } from 'react';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import {
+  GET_MY_SUBSCRIPTION_SIZE_MOBILE,
+  GET_MY_SUBSCRIPTION_SIZE_WEB,
+} from '@pages/myinfo/constants/apiParams';
+
+import { NOTIFICATIONS_PAGE } from '@/constants/apiConstants';
+import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
+
+const getNotificationsPage = async ({
+  size,
+  notificationId,
+}: {
+  size: number;
+  notificationId: number;
+}) => {
+  const queryParmas = {
+    size,
+    notificationId,
+  };
+  const res = await axios.get(`${NOTIFICATIONS_PAGE}`, {
+    params: {
+      ...queryParmas,
+    },
+  });
+
+  return res?.data;
+};
+
+export const useGetNotificationsPage = () => {
+  const { isMobile } = useMediaQueryContext();
+
+  const size = isMobile ? GET_MY_SUBSCRIPTION_SIZE_MOBILE : GET_MY_SUBSCRIPTION_SIZE_WEB;
+
+  const {
+    data: notificationsPageData,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    status,
+    error,
+    isFetching,
+  } = useInfiniteQuery({
+    queryKey: ['notificationsPage', size],
+    queryFn: ({ pageParam }) => {
+      return getNotificationsPage({ size, notificationId: pageParam });
+    },
+    initialPageParam: Number.MAX_SAFE_INTEGER,
+    getNextPageParam: (lastPage) => {
+      if (lastPage?.data.last) {
+        return undefined;
+      }
+      const lastNotificationId = lastPage?.data.content[size - 1]?.notificationId;
+      return lastNotificationId ?? undefined;
+    },
+  });
+
+  const onIntersect = useCallback(
+    ([entry]: IntersectionObserverEntry[]) => {
+      if (!isFetching && entry.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage, isFetching],
+  );
+
+  return {
+    notificationsPageData,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    status,
+    error,
+    isFetching,
+    onIntersect,
+  };
+};
--- pages/myinfo/notifications/notification/index.page.tsx
+++ pages/myinfo/notifications/notification/index.page.tsx
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { Fragment, useRef, useState } from 'react';
+import React from 'react';
 
 import {
   NotificationFilterListProps,
   NotificationFilterStatus,
 } from '@pages/myinfo/types/myInfoFilter';
 
+import { useObserver } from '@hooks/useObserver';
+
 import MyInfoFilterButtons from '../../components/MyInfoFilterButtons';
 import MyInfo from '../../index.page';
 import NotificationNav from '../components/NotificationNav';
-import NotificationSubscribeCard from './components/NotificationSubscribeCard';
+import { useGetNotificationsPage } from './apiHooks/useGetNotifications';
+import NotificationSubscribeCard, {
+  NotificationSubscribeCardProps,
+} from './components/NotificationSubscribeCard';
 
 export default function Notification() {
   const [notificationFilterStatus, setNotificationFilterStatus] =
     useState<NotificationFilterStatus>('SUBSCRIBE');
+
+  const bottom = useRef(null);
+
+  const { notificationsPageData, isFetchingNextPage, hasNextPage, status, onIntersect } =
+    useGetNotificationsPage();
+  console.log('notificationsPageData', notificationsPageData);
+
+  useObserver({
+    target: bottom,
+    onIntersect,
+  });
 
   const notificationFilterList: NotificationFilterListProps[] = [
     // {
@@ -28,13 +45,22 @@ export default function Notification() {
     {
       filterStatus: 'SUBSCRIBE',
       filterName: '구독 업데이트',
-      filterTotal: 2,
+      filterTotal: notificationsPageData?.pages[0]?.data?.totalElements,
     },
   ];
 
   const handleNotificationFilterClick = (filterStatus: NotificationFilterStatus) => {
     setNotificationFilterStatus(filterStatus);
   };
+
+  if (status === 'pending') {
+    return (
+      <MyInfo>
+        <NotificationNav />
+        <section className={``}>로딩중...</section>
+      </MyInfo>
+    );
+  }
 
   return (
     <MyInfo>
@@ -45,8 +71,20 @@ export default function Notification() {
         handleFilterClick={handleNotificationFilterClick}
       />
       <section className='flex flex-col gap-[2.4rem] pb-[12rem]'>
-        <NotificationSubscribeCard />
-        <NotificationSubscribeCard />
+        {notificationsPageData?.pages.map((page, index) => (
+          <Fragment key={index}>
+            {page?.data?.content.map((notificationPageItem: NotificationSubscribeCardProps) => (
+              <NotificationSubscribeCard
+                key={notificationPageItem.notificationId}
+                {...notificationPageItem}
+              />
+            ))}
+          </Fragment>
+        ))}
+
+        {isFetchingNextPage && hasNextPage && <div className='mt-[2rem]'>로딩중...</div>}
+
+        <div ref={bottom} />
       </section>
     </MyInfo>
   );
