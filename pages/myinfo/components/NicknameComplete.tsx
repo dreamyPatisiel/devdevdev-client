0163bd68d982683fca0e9daeb79816a349f1dbd5
minyoung.kim <minyoung.kim@wonderwall.kr>
2025-07-13 16:25:09 +0900

    feat: 닉네임 확정 멘트 추가

--- pages/myinfo/components/NicknameComplete.tsx
+++ pages/myinfo/components/NicknameComplete.tsx
@@ -6,15 +6,24 @@ import { useNicknameStore } from '@stores/nicknameStore';
 
 import starBackground from '@public/image/myInfo/star_background.png';
 
+import { useMediaQueryContext } from '@/contexts/MediaQueryContext';
+
 export default function NicknameComplete() {
+  const { isMobile } = useMediaQueryContext();
+
   const { nickname } = useNicknameStore();
 
   return (
-    <div className='relative w-full h-[13.75rem]'>
-      <Image src={starBackground} alt='닉네임 배경' fill className='object-cover' />
-      <p className='st2 font-bold absolute inset-0 text-gray600 flex items-center justify-center'>
-        {nickname}
+    <>
+      <div className='relative w-full h-[13.75rem]'>
+        <Image src={starBackground} alt='닉네임 배경' fill className='object-cover' />
+        <p className='st2 font-bold absolute inset-0 text-gray600 flex items-center justify-center'>
+          {nickname}
+        </p>
+      </div>
+      <p className={`text-gray200 whitespace-pre-wrap mt-[3.2rem] ${isMobile ? 'p2' : 'p1'}`}>
+        정말 멋진 닉네임이에요
       </p>
-    </div>
+    </>
   );
 }
