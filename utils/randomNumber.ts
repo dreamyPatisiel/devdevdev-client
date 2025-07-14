1bc1a2bde32f945e756741b191db7b3fd89ba292
minyoung.kim <minyoung.kim@wonderwall.kr>
2025-07-14 21:50:35 +0900

    fix: getRandomIndex 함수 수정

--- utils/randomNumber.ts
+++ utils/randomNumber.ts
@@ -1,6 +1,6 @@
-export const getRandomNumberOfThree = () => {
+export const getRandomIndex = (max: number) => {
   const randomNum = Math.random();
-  const randomIndex = Math.floor(Math.floor(randomNum * 10) % 3);
+  const randomIndex = Math.floor(Math.floor(randomNum * 10) % max);
 
   return randomIndex;
 };
