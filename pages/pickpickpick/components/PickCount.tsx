890a1ca39008321fc55ad264af0bd3745492be08
mandelina <mandelina24@gmail.com>
2025-11-02 17:54:36 +0900

    Fix : PickCount 컴포넌트의 불필요한 주석 제거

--- pages/pickpickpick/components/PickCount.tsx
+++ pages/pickpickpick/components/PickCount.tsx
@@ -8,8 +8,7 @@ interface PickCountProps {
 export const PickCount = ({ count = 1, className = '' }: PickCountProps) => {
   return (
     <p className={`p1 text-gray50 ${className}`}>
-      {/* TODO: 추후 데이터 연동 */}총 <span className='text-secondary500 font-bold'>{count}</span>
-      건
+      총 <span className='text-secondary500 font-bold'>{count}</span>건
     </p>
   );
 };
