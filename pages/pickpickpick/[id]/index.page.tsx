73f1569cf6af950d545581c5441afa998ccea2d2
minyoung.kim <minyoung.kim@wonderwall.kr>
2025-11-02 17:01:13 +0900

    fix: pickDetailPage에 v2 적용

--- pages/pickpickpick/[id]/index.page.tsx
+++ pages/pickpickpick/[id]/index.page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import DevLoadingComponent from '@pages/loading/index.page';
@@ -17,7 +17,11 @@ import {
   PICK_VOTE_MODIFIED_MODAL,
 } from '@components/common/modals/modalConfig/pickVote';
 import MoreButton from '@components/common/moreButton';
+import StatisticsItem from '@components/features/pickpickpick/StatisticsItem';
 import type { MetaHeadProps } from '@components/meta/MetaHead';
+
+import GrayComment from '@public/image/comment-dots.svg';
+import GrayFire from '@public/image/fire-alt.svg';
 
 import { META } from '@/constants/metaData';
 import { ROUTES } from '@/constants/routes';
@@ -27,7 +31,7 @@ import { useGetSimilarPick } from './apiHooks/useGetSimilarPick';
 import { useGetPickDetailData } from './apiHooks/usePickDetailData';
 import PickCommentSection from './components/PickCommentSection';
 import SimilarPick from './components/SimilarPick';
-import VoteCard from './components/VoteCard';
+import VoteCardV2 from './components/VoteCardV2';
 import usePickDetailHandlers from './handlers/usePickDetailHandlers';
 
 type NextPageWithMeta = NextPage & { meta?: MetaHeadProps };
@@ -87,50 +91,85 @@ const PickDetailPage: NextPageWithMeta = () => {
           ${isMobile ? 'px-[1.6rem]' : 'px-[20.4rem] pt-[6.4rem] pb-[12.2rem]'}
           `}
       >
-        <div className='border-b-[0.1rem] border-b-gray400 flex justify-between items-baseline pb-[1.6rem] pl-[1rem]'>
-          <div>
-            <h3 className='h3 font-bold mb-[0.8rem]'>{pickDetailData?.pickTitle}</h3>
+        <div>
+          <div
+            className={`border-b-[0.1rem] border-b-gray400 flex justify-between items-start ${isMobile ? 'pb-[1.6rem] mb-[2rem]' : 'py-[1.6rem] px-[3.2rem] bg-gray800 rounded-t-[1.2rem]'}`}
+          >
+            <div className='w-full'>
+              <div className={`flex items-baseline gap-[1.2rem] `}>
+                <h3 className={`font-bold mb-[0.8rem] flex w-full ${isMobile ? 'st2' : 'h3'}`}>
+                  {pickDetailData?.pickTitle}
+                </h3>
+                {!pickDetailData?.isAuthor && (
+                  <div className='mr-0'>
+                    <MoreButton
+                      moreButtonList={PICK_DETAIL_MORE_BUTTON_TYPE.map((type) => ({
+                        buttonType: type,
+                        moreButtonOnclick: handleVoteMoreButtonClick(type),
+                      }))}
+                    />
+                  </div>
+                )}
+              </div>
 
-            <div>
-              <span className='p2 text-gray100 font-bold'>
-                <NicknameWithMaskedEmail
-                  author={pickDetailData?.nickname ?? ''}
-                  maskedEmail={pickDetailData?.userId ?? ''}
-                  textSize='p2'
-                />
-              </span>
-              <span className='p2 text-gray200 ml-[2rem] mr-[1rem]'>{formatPickDate}</span>
-              {loginStatus === 'login' && !pickDetailData?.isAuthor && (
-                <span
-                  className='p2 text-gray200 cursor-pointer'
-                  onClick={() => handleBlameButtonClick()}
-                >
-                  신고
+              <div className={`${isMobile ? 'flex flex-col gap-[0.4rem]' : ''}`}>
+                <span className='p2 text-gray100 font-bold mr-[2rem] '>
+                  <NicknameWithMaskedEmail
+                    author={pickDetailData?.nickname ?? ''}
+                    maskedEmail={pickDetailData?.userId ?? ''}
+                    textSize='p2'
+                  />
                 </span>
+                <span>
+                  <span className='p2 text-gray200 mr-[1rem]'>{formatPickDate}</span>
+                  {loginStatus === 'login' && !pickDetailData?.isAuthor && (
+                    <span
+                      className='p2 text-gray200 cursor-pointer'
+                      onClick={() => handleBlameButtonClick()}
+                    >
+                      신고
+                    </span>
+                  )}
+                </span>
+              </div>
+              {!isMobile && (
+                <div className='flex mt-[1rem] gap-[2rem]'>
+                  <StatisticsItem
+                    icon={GrayFire}
+                    alt='투표 이미지'
+                    text='투표'
+                    count={1345}
+                    // count={pickData.voteTotalCount}
+                    textColor='text-gray100'
+                  />
+                  <StatisticsItem
+                    icon={GrayComment}
+                    alt='댓글 이미지'
+                    text='댓글'
+                    count={123}
+                    // count={pickData.commentTotalCount}
+                    textColor='text-gray100'
+                  />
+                </div>
               )}
             </div>
           </div>
 
-          {pickDetailData?.isAuthor && (
-            <div className='flex-shrink-0 ml-[2.1rem]'>
-              <MoreButton
-                moreButtonList={PICK_DETAIL_MORE_BUTTON_TYPE.map((type) => ({
-                  buttonType: type,
-                  moreButtonOnclick: handleVoteMoreButtonClick(type),
-                }))}
-              />
-            </div>
-          )}
+          <div
+            className={`flex ${isMobile ? 'flex-col gap-[4rem]' : 'gap-[2.4rem] p-[4rem] bg-gray800 rounded-b-[1.2rem]'}`}
+          >
+            <VoteCardV2
+              pickDetailOptionData={pickDetailData?.pickOptions.firstPickOption}
+              dataIsVoted={pickDetailData?.isVoted}
+              pickOrder='first'
+            />
+            <VoteCardV2
+              pickDetailOptionData={pickDetailData?.pickOptions.secondPickOption}
+              dataIsVoted={pickDetailData?.isVoted}
+              pickOrder='second'
+            />
+          </div>
         </div>
-
-        <VoteCard
-          pickDetailOptionData={pickDetailData?.pickOptions.firstPickOption}
-          dataIsVoted={pickDetailData?.isVoted}
-        />
-        <VoteCard
-          pickDetailOptionData={pickDetailData?.pickOptions.secondPickOption}
-          dataIsVoted={pickDetailData?.isVoted}
-        />
 
         <div className='py-[4.8rem]'>
           <h3 className='h3 mb-[2.4rem] font-bold'>나도 고민했는데! 다른 픽픽픽 💘</h3>
